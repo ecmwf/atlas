ed9a061b1dc1db6fd90844d2672451b177b14a60
Willem Deconinck <wdeconinck@me.com>
2013-11-04 17:18:32 +0000

    mpi_stubs added

--- src/io/joanna_module.F90
+++ src/io/joanna_module.F90
@@ -5,7 +5,7 @@ module joanna_module
   use parallel_module
   use datastruct_module, only: create_mesh, DataStructure_type, vector_field, scalar_field
   use split_globe_module, only : split_globe
-  use mpi
+  use mpi_stubs
   implicit none
 contains
   
