49d8897adef6b6e58f1ede9e65f5ed7af0bf13de
Tiago Quintino <tiago.quintino@ecmwf.int>
2014-03-06 15:12:18 +0000

    improve build system

--- src/tests/test_gmsh.cpp
+++ src/tests/test_gmsh.cpp
@@ -5,16 +5,19 @@
 #include "atlas/Partitioner.hpp"
 #include "atlas/MPL.hpp"
 
+#define DATADIR ATLAS_DATADIR
+
 using namespace atlas;
 int main(int argc, char *argv[])
 {
   MPL::init();
 
-  Mesh& mesh = Gmsh::read("data/meshes/T47.msh");
+  Mesh& mesh = Gmsh::read( std::string(DATADIR) + "/meshes/T47.msh");
 
-  build_periodic_boundaries(mesh);
-  build_edges(mesh);
-  build_dual_mesh(mesh);
+//  build_periodic_boundaries(mesh);
+//  build_edges(mesh);
+//  build_dual_mesh(mesh);
+
   Gmsh::write(mesh,"bla.msh");
   
   MPL::finalize();
