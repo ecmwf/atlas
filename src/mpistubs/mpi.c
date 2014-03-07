f0492d71c60966c5f66ab8a30c1b941df587c968
Willem Deconinck <willem.deconinck@ecmwf.int>
2014-03-07 09:25:46 +0000

    Fix mpistubs warning

--- src/mpistubs/mpi.c
+++ src/mpistubs/mpi.c
@@ -1,5 +1,14 @@
-#include "mpi.h"
+// (C) Copyright 1996-2014 ECMWF.
+//
+// This software is licensed under the terms of the Apache Licence Version 2.0
+// which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+// In applying this licence, ECMWF does not waive the privileges and immunities
+// granted to it by virtue of its status as an intergovernmental organisation nor
+// does it submit to any jurisdiction.
 
+
+#include <string.h>
+#include "mpi.h"
 
 #define MPI_INT_SIZE    sizeof(int)
 #define MPI_FLOAT_SIZE  sizeof(float)
