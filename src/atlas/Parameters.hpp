6445fb41be0e0c73001de43edaa87b5fcb8db521
Willem Deconinck <willem.deconinck@ecmwf.int>
2014-03-12 13:35:10 +0000

    Fix fortran numbering

--- src/atlas/Parameters.hpp
+++ src/atlas/Parameters.hpp
@@ -26,17 +26,13 @@ struct ElementRef {
     int e;
 };
 
-#if HAVE_FORTRAN_NUMBERING
-
+#ifdef ENABLE_FORTRAN_NUMBERING
 // So that we have the same local id in fortran by natural indexing
 #define C_IDX(index) index-1
 #define F_IDX(index) index+1
-
 #else
-
 #define C_IDX(index) index
 #define F_IDX(index) index
-
 #endif
 
 } // namespace atlas
