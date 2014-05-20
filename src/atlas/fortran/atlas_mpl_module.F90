7309dac1247b93f861f3d30f64930a34ed8e8f94
Willem Deconinck <willem.deconinck@ecmwf.int>
2014-05-20 16:34:38 +0000

    Fortran MPI support in atlas

---
+++ src/atlas/fortran/atlas_mpl_module.F90
@@ -0,0 +1,23 @@
+
+module atlas_mpl_module
+#include "atlas/atlas_mpi_fortran.h"
+
+contains
+
+  subroutine MPL_init()
+    integer :: ierr
+    logical :: initialized
+    call MPI_INITIALIZED(initialized, ierr)
+    if (.not. initialized) call MPI_INIT( ierr )
+    call MPI_COMM_RANK( MPI_COMM_WORLD, myproc, ierr )
+    call MPI_COMM_SIZE( MPI_COMM_WORLD, nproc,  ierr )
+  end subroutine MPL_init
+
+  subroutine MPL_finalize()
+    integer :: ierr
+    logical :: finalized
+    call MPI_FINALIZED(finalized, ierr)
+    if (.not. finalized) call MPI_FINALIZE (ierr)
+  end subroutine MPL_finalize
+
+end module atlas_mpl_module
