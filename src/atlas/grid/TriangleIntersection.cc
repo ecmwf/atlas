f4abeca7972f5e1d45a52ffabba7008c0f89746b
Tiago Quintino <tiago.quintino@ecmwf.int>
2014-09-22 08:15:53 +0100

    move triag intersection method into class

--- src/atlas/grid/TriangleIntersection.cc
+++ src/atlas/grid/TriangleIntersection.cc
@@ -10,9 +10,7 @@
 
 #include "eckit/eckit_config.h"
 
-#ifndef HAVE_EIGEN
-void atlas_grid_TriangeIntersection() {} // dummy symbol
-#else
+#ifdef HAVE_EIGEN
 
 #include "eckit/maths/Eigen.h"
 
@@ -25,26 +23,35 @@ using namespace atlas;
 
 namespace atlas {
 
-bool triag_intersection( const Triag& tg, const Ray& r, Isect& isect, const double slack )
+bool Triag::intersects( const Ray& r, Isect& isect, const double slack )
 {
-    Eigen::Vector3d edge1 = tg.v1 - tg.v0;
-    Eigen::Vector3d edge2 = tg.v2 - tg.v0;
+	Eigen::Vector3d edge1 = v1 - v0;
+	Eigen::Vector3d edge2 = v2 - v0;
     Eigen::Vector3d pvec  = r.dir.cross(edge2);
-    const double det = edge1.dot(pvec);
-    if( std::abs(det) < slack ) return false;
-    const double invDet = 1. / det;
-    Eigen::Vector3d tvec = r.orig - tg.v0;
+
+	const double det = edge1.dot(pvec);
+
+	if( std::abs(det) < slack )
+		return false;
+
+	const double invDet = 1. / det;
+	Eigen::Vector3d tvec = r.orig - v0;
     isect.u = tvec.dot(pvec) * invDet;
-    if( isect.u + slack < 0 || isect.u - slack > 1) return false;
-    Eigen::Vector3d qvec = tvec.cross(edge1);
+	if( isect.u + slack < 0 || isect.u - slack > 1)
+		return false;
+
+	Eigen::Vector3d qvec = tvec.cross(edge1);
     isect.v = r.dir.dot(qvec) * invDet;
-    if (isect.v + slack < 0 || isect.u + isect.v - slack > 1) return false;
+
+	if (isect.v + slack < 0 || isect.u + isect.v - slack > 1)
+		return false;
     isect.t = edge2.dot(qvec) * invDet;
-    return true;
+
+	return true;
 }
 
 //------------------------------------------------------------------------------------------------------
 
 } // namespace eckit
 
-#endif
+#endif // HAVE_EIGEN
