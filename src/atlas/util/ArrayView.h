08066839ad5668bc1a83707edf1930780ba34db4
Willem Deconinck <willem.deconinck@ecmwf.int>
2014-11-04 12:12:13 +0000

    ArrayView with eckit exceptions

--- src/atlas/util/ArrayView.h
+++ src/atlas/util/ArrayView.h
@@ -46,32 +46,34 @@
 #define ATLAS_ARRAYVIEW_BOUNDS_CHECKING
 
 #ifdef ATLAS_ARRAYVIEW_BOUNDS_CHECKING
-#include <stdexcept>
+#include <eckit/exception/Exceptions.h>
+
+
 #define CHECK_RANK(R)\
-  if(rank()!=R) {throw std::range_error("rank mismatch");}
+  if(rank()!=R) { std::ostringstream msg; msg << "ArrayView  rank mismatch: rank()="<<rank()<< " != " << R; throw eckit::OutOfRange(msg.str(),Here()); }
 #define CHECK_BOUNDS(idx) {\
   for( int d=0; d<rank(); ++d ) { \
-    if(idx[d]>=shape_[d]) {throw std::range_error("index out of bounds");}}}
+    if(idx[d]>=shape_[d]) {std::ostringstream msg; msg << "index " << d << " out of bounds: " << idx[d] << " >= " << shape_[d]; throw eckit::OutOfRange(msg.str(),Here()); } } }
 #define CHECK_BOUNDS_1(i)\
-	if(i>=shape_[0]) {std::ostringstream msg; msg << "index out of bounds: i=" << i << " >= " << shape_[0]; throw std::range_error(msg.str()); }
+	if(i>=shape_[0]) {std::ostringstream msg; msg << "ArrayView(i) index out of bounds: i=" << i << " >= " << shape_[0]; throw eckit::OutOfRange(msg.str(),Here()); }
 #define CHECK_BOUNDS_2(i,j)\
-  if(i>=shape_[0]) {throw std::range_error("index 'i' out of bounds");} \
-  if(j>=shape_[1]) {throw std::range_error("index 'j' out of bounds");}
+	if(i>=shape_[0]) {std::ostringstream msg; msg << "ArrayView(i,j) index out of bounds: i=" << i << " >= " << shape_[0]; throw eckit::OutOfRange(msg.str(),Here()); }\
+	if(j>=shape_[1]) {std::ostringstream msg; msg << "ArrayView(i,j) index out of bounds: j=" << j << " >= " << shape_[1]; throw eckit::OutOfRange(msg.str(),Here()); }
 #define CHECK_BOUNDS_3(i,j,k)\
-  if(i>=shape_[0]) {throw std::range_error("index 'i' out of bounds");} \
-  if(j>=shape_[1]) {throw std::range_error("index 'j' out of bounds");} \
-  if(k>=shape_[2]) {throw std::range_error("index 'k' out of bounds");}
+	if(i>=shape_[0]) {std::ostringstream msg; msg << "ArrayView(i,j,k) index out of bounds: i=" << i << " >= " << shape_[0]; throw eckit::OutOfRange(msg.str(),Here()); }\
+	if(j>=shape_[1]) {std::ostringstream msg; msg << "ArrayView(i,j,k) index out of bounds: j=" << j << " >= " << shape_[1]; throw eckit::OutOfRange(msg.str(),Here()); }\
+	if(k>=shape_[2]) {std::ostringstream msg; msg << "ArrayView(i,j,k) index out of bounds: k=" << k << " >= " << shape_[2]; throw eckit::OutOfRange(msg.str(),Here()); }
 #define CHECK_BOUNDS_4(i,j,k,l)\
-  if(i>=shape_[0]) {throw std::range_error("index 'i' out of bounds");} \
-  if(j>=shape_[1]) {throw std::range_error("index 'j' out of bounds");} \
-  if(k>=shape_[2]) {throw std::range_error("index 'k' out of bounds");} \
-  if(l>=shape_[3]) {throw std::range_error("index 'l' out of bounds");}
+	if(i>=shape_[0]) {std::ostringstream msg; msg << "ArrayView(i,j,k,l) index out of bounds: i=" << i << " >= " << shape_[0]; throw eckit::OutOfRange(msg.str(),Here()); }\
+	if(j>=shape_[1]) {std::ostringstream msg; msg << "ArrayView(i,j,k,l) index out of bounds: j=" << j << " >= " << shape_[1]; throw eckit::OutOfRange(msg.str(),Here()); }\
+	if(k>=shape_[2]) {std::ostringstream msg; msg << "ArrayView(i,j,k,l) index out of bounds: k=" << k << " >= " << shape_[2]; throw eckit::OutOfRange(msg.str(),Here()); }\
+	if(l>=shape_[3]) {std::ostringstream msg; msg << "ArrayView(i,j,k,l) index out of bounds: l=" << l << " >= " << shape_[3]; throw eckit::OutOfRange(msg.str(),Here()); }
 #define CHECK_BOUNDS_5(i,j,k,l,m)\
-  if(i>=shape_[0]) {throw std::range_error("index 'i' out of bounds");} \
-  if(j>=shape_[1]) {throw std::range_error("index 'j' out of bounds");} \
-  if(k>=shape_[2]) {throw std::range_error("index 'k' out of bounds");} \
-  if(l>=shape_[3]) {throw std::range_error("index 'l' out of bounds");} \
-  if(m>=shape_[4]) {throw std::range_error("index 'm' out of bounds");}
+	if(i>=shape_[0]) {std::ostringstream msg; msg << "ArrayView(i,j,k,l,m) index out of bounds: i=" << i << " >= " << shape_[0]; throw eckit::OutOfRange(msg.str(),Here()); }\
+	if(j>=shape_[1]) {std::ostringstream msg; msg << "ArrayView(i,j,k,l,m) index out of bounds: j=" << j << " >= " << shape_[1]; throw eckit::OutOfRange(msg.str(),Here()); }\
+	if(k>=shape_[2]) {std::ostringstream msg; msg << "ArrayView(i,j,k,l,m) index out of bounds: k=" << k << " >= " << shape_[2]; throw eckit::OutOfRange(msg.str(),Here()); }\
+	if(l>=shape_[3]) {std::ostringstream msg; msg << "ArrayView(i,j,k,l,m) index out of bounds: l=" << l << " >= " << shape_[3]; throw eckit::OutOfRange(msg.str(),Here()); }\
+	if(m>=shape_[4]) {std::ostringstream msg; msg << "ArrayView(i,j,k,l,m) index out of bounds: m=" << m << " >= " << shape_[4]; throw eckit::OutOfRange(msg.str(),Here()); }
 #else
 #define CHECK_RANK(R)
 #define CHECK_BOUNDS(i,max)
