c72c6e286aca9a74bf737e09bac3dc1822efe2db
Willem Deconinck <willem.deconinck@ecmwf.int>
2014-12-16 11:06:42 +0000

    OpenMP header

--- src/atlas/atlas.h
+++ src/atlas/atlas.h
@@ -1,7 +1,6 @@
 #ifndef atlas_atlas_h
 #define atlas_atlas_h
 
-#include <stdint.h>
 #include "atlas/atlas_config.h"
 
 namespace atlas {
---
+++ src/atlas/atlas_omp.h
@@ -0,0 +1,24 @@
+/*
+ * (C) Copyright 1996-2014 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#ifndef atlas_omp_h
+#define atlas_omp_h
+
+#include "atlas/atlas_config.h"
+
+#ifdef HAVE_OMP
+#include <omp.h>
+#else
+  #include "eckit/log/Log.h"
+  void omp_set_num_threads(int) { eckit::Log::warning() << "\nWARNING: OpenMP not available!\n" << std::endl; }
+  int omp_get_max_threads() { return 0; }
+#endif
+
+#endif
