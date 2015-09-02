ac5a33c91b6d1c9fdce17a5c71cbe2cb8f60d1aa
Willem Deconinck <willem.deconinck@ecmwf.int>
2015-09-02 08:48:35 +0100

    Fortran SpectralFunctionSpace in separate file

---
+++ src/atlas_f/atlas_module_SpectralFunctionSpace_i.f
@@ -0,0 +1,41 @@
+! (C) Copyright 2013-2015 ECMWF.
+
+
+!------------------------------------------------------------------------------
+TYPE, extends(object_type) :: atlas_SpectralFunctionSpace
+
+! Purpose :
+! -------
+!   *atlas_SpectralFunctionSpace* : Interpretes spectral fields
+
+! Methods :
+! -------
+
+! Author :
+! ------
+!   August-2015 Willem Deconinck     *ECMWF*
+
+!------------------------------------------------------------------------------
+contains
+
+  procedure, private :: create_field_name     => SpectralFunctionSpace__create_field_name
+  procedure, private :: create_field_name_lev => SpectralFunctionSpace__create_field_name_lev
+  generic, public :: create_field => &
+    & create_field_name, &
+    & create_field_name_lev
+
+  procedure, private :: create_glb_field_name     => SpectralFunctionSpace__create_glb_field_name
+  procedure, private :: create_glb_field_name_lev => SpectralFunctionSpace__create_glb_field_name_lev
+  generic, public :: create_global_field => &
+    & create_glb_field_name, &
+    & create_glb_field_name_lev
+
+END TYPE atlas_SpectralFunctionSpace
+
+interface atlas_SpectralFunctionSpace
+  module procedure atlas_SpectralFunctionSpace__name_truncation
+  module procedure atlas_SpectralFunctionSpace__name_trans
+end interface
+
+!------------------------------------------------------------------------------
+
