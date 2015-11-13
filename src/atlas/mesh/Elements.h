eefd06faadb894d94303b0deacc6c6dd6ef54045
Willem Deconinck <willem.deconinck@ecmwf.int>
2015-11-13 15:45:37 +0000

    ATLAS-56: Split Elements and HybridElements in different files

--- src/atlas/mesh/Elements.h
+++ src/atlas/mesh/Elements.h
@@ -20,17 +20,14 @@
 #include "eckit/memory/Owned.h"
 #include "eckit/memory/SharedPtr.h"
 #include "atlas/Connectivity.h"
+#include "atlas/mesh/HybridElements.h"
 
 namespace atlas { namespace mesh { class ElementType; } }
 
 namespace atlas {
 namespace mesh {
 
-// Classes defined in this file:
-class HybridElements;
-class Elements;
-
-// -------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------
 
 /// @brief Describe elements of a single type
 class Elements : public eckit::Owned {
@@ -85,145 +82,6 @@ private:
   size_t nb_nodes_;
   size_t nb_edges_;
 };
-
-// -------------------------------------------------------------------------------
-
-/// @brief HybridElements class that describes elements of different types
-class HybridElements : public eckit::Owned {
-friend class Elements;
-public:
-  typedef MultiBlockConnectivity Connectivity;
-
-public: // methods
-
-//-- Constructors
-
-  HybridElements();
-  virtual ~HybridElements();
-
-//-- Accessors
-
-  /// @brief Number of elements
-  size_t size() const;
-
-  /// @brief Number of nodes for given element
-  size_t nb_nodes(size_t elem_idx) const;
-
-  /// @brief Number of edges for given element
-  size_t nb_edges(size_t elem_idx) const;
-
-  /// @brief Element type index for given element
-  size_t type_idx(size_t elem_idx) const;
-
-  /// @brief Element type name for given element
-  const std::string& name(size_t elem_idx) const;
-
-  /// @brief Element to Node connectivity table
-  const HybridElements::Connectivity& node_connectivity() const;
-        HybridElements::Connectivity& node_connectivity();
-
-  /// @brief Number of types present in HybridElements
-  size_t nb_types() const;
-
-  /// @brief The element_type description for given type
-  const ElementType& element_type(size_t type_idx) const;
-
-  /// @brief Sub-elements convenience class for given type
-  /// This allows optimized access to connectivities and loops.
-  const Elements& elements(size_t type_idx) const;
-        Elements& elements(size_t type_idx);
-
-// -- Modifiers
-
-  /// @brief Add a new element type with given number of elements and node-connectivity
-  size_t add( const ElementType*, size_t nb_elements, const std::vector<idx_t> &node_connectivity );
-
-  /// @brief Add a new element type with given number of elements and node-connectivity
-  size_t add( const ElementType*, size_t nb_elements, const idx_t node_connectivity[] );
-
-  /// @brief Add a new element type with given number of elements and node-connectivity
-  size_t add( const ElementType*, size_t nb_elements, const idx_t node_connectivity[], bool fortran_array );
-
-  /// @brief Add a new element type from existing Elements.
-  /// Data will be copied.
-  size_t add( const Elements& );
-
-private:
-
-// -- Data
-  size_t size_;                          //!< total number of elements
-
-// -- Data: one value per type
-  std::vector<size_t> elements_size_;
-  std::vector<size_t> elements_begin_;
-  std::vector< eckit::SharedPtr<const ElementType> > element_types_;
-
-// -- Data: one value per element
-  std::vector<size_t> nodes_begin_;
-  std::vector<size_t> nb_nodes_;
-  std::vector<size_t> nb_edges_;
-  std::vector<size_t> type_idx_;
-
-// -- Data: one value per node per element
-  std::vector<idx_t> node_connectivity_array_;
-
-// -- Accessor helpers
-  eckit::SharedPtr< HybridElements::Connectivity > node_connectivity_;
-  std::vector< eckit::SharedPtr<Elements> > elements_;
-};
-
-// -----------------------------------------------------------------------------------------------------
-
-inline size_t HybridElements::size() const
-{
-  return size_;
-}
-
-inline size_t HybridElements::nb_types() const
-{
-  return element_types_.size();
-}
-
-inline const ElementType& HybridElements::element_type(size_t type_idx) const
-{
-  return *element_types_[type_idx].get();
-}
-
-inline const HybridElements::Connectivity& HybridElements::node_connectivity() const
-{
-  return *node_connectivity_.get();
-}
-
-inline HybridElements::Connectivity& HybridElements::node_connectivity()
-{
-  return *node_connectivity_.get();
-}
-
-inline const Elements& HybridElements::elements(size_t type_idx) const
-{
-  return *elements_[type_idx].get();
-}
-
-inline Elements& HybridElements::elements(size_t type_idx)
-{
-  return *elements_[type_idx].get();
-}
-
-inline size_t HybridElements::nb_nodes(size_t elem_idx) const
-{
-  return nb_nodes_[elem_idx];
-}
-
-inline size_t HybridElements::nb_edges(size_t elem_idx) const
-{
-  return nb_edges_[elem_idx];
-}
-
-inline size_t HybridElements::type_idx(size_t elem_idx) const
-{
-  return type_idx_[elem_idx];
-}
-
 
 // ------------------------------------------------------------------------------------------------------
 
---
+++ src/atlas/mesh/HybridElements.cc
@@ -0,0 +1,126 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#include "atlas/runtime/ErrorHandling.h"
+#include "atlas/mesh/ElementType.h"
+#include "atlas/mesh/HybridElements.h"
+#include "atlas/mesh/Elements.h"
+#include "atlas/atlas_defines.h"
+
+#ifdef ATLAS_HAVE_FORTRAN
+#define FORTRAN_BASE 1
+#define TO_FORTRAN +1
+#else
+#define FORTRAN_BASE 0
+#endif
+
+namespace atlas {
+namespace mesh {
+
+//------------------------------------------------------------------------------------------------------
+
+HybridElements::HybridElements() :
+  size_(0),
+  elements_size_(),
+  elements_begin_(1,0ul),
+  node_connectivity_array_(),
+  nodes_begin_(1,0ul),
+  nb_nodes_(),
+  nb_edges_(),
+  type_idx_()
+{
+}
+
+HybridElements::~HybridElements()
+{
+}
+
+size_t HybridElements::add( const ElementType* element_type, size_t nb_elements, const std::vector<idx_t> &connectivity )
+{
+  return add(element_type,nb_elements,connectivity.data());
+}
+
+size_t HybridElements::add( const ElementType* element_type, size_t nb_elements, const idx_t connectivity[] )
+{
+  return add(element_type,nb_elements,connectivity,false);
+}
+
+size_t HybridElements::add( const ElementType* element_type, size_t nb_elements, const idx_t connectivity[], bool fortran_array )
+{
+  eckit::SharedPtr<const ElementType> etype ( element_type );
+
+  size_t old_size=size();
+  size_t new_size = old_size+nb_elements;
+
+  size_t nb_nodes = etype->nb_nodes();
+  size_t nb_edges = etype->nb_edges();
+
+  size_t c(node_connectivity_array_.size());
+  node_connectivity_array_.resize(c+nb_elements*nb_nodes);
+
+  nb_nodes_.resize(new_size);
+  nb_edges_.resize(new_size);
+  type_idx_.resize(new_size);
+  nodes_begin_.resize(new_size+1);
+
+  for( size_t e=old_size; e<new_size; ++e ) {
+    nb_nodes_[e] = nb_nodes;
+    nb_edges_[e] = nb_edges;
+    type_idx_[e] = element_types_.size();
+    nodes_begin_[e+1] = nodes_begin_[e]+nb_nodes;
+  }
+
+  idx_t *conn = node_connectivity_array_.data();
+  idx_t add_base = fortran_array ? 0 : FORTRAN_BASE;
+  for(size_t j=0; c<node_connectivity_array_.size(); ++c, ++j) {
+    conn[c] = connectivity[j] + add_base ;
+  }
+
+  elements_begin_.push_back(new_size);
+  elements_size_.push_back(nb_elements);
+
+  element_types_.push_back( etype );
+  elements_.resize(element_types_.size());
+  for( size_t t=0; t<nb_types(); ++t ) {
+    elements_[t].reset( new Elements(*this,t) );
+  }
+  node_connectivity_.reset( new HybridElements::Connectivity(
+                                     node_connectivity_array_.data(),
+                                     size_,
+                                     nodes_begin_.data(),
+                                     nb_nodes_.data(),
+                                     element_types_.size(),
+                                     elements_begin_.data())
+                                   );
+  size_ = new_size;
+  return element_types_.size()-1;
+}
+
+size_t HybridElements::add( const Elements& elems )
+{
+  return add( &elems.element_type(), elems.size(), elems.hybrid_elements().node_connectivity_array_.data(), true );
+}
+
+
+const std::string& HybridElements::name(size_t elem_idx) const { return element_types_[type_idx_[elem_idx]]->name(); }
+
+//-----------------------------------------------------------------------------
+
+extern "C" {
+
+}
+
+//-----------------------------------------------------------------------------
+
+}  // namespace mesh
+}  // namespace atlas
+
+#undef FORTRAN_BASE
+
