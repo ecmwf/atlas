4a8efb3a4474e5569dc25a3cb8b6a1698a9380e5
Willem Deconinck <Willem.Deconinck@ecmwf.int>
2016-02-17 17:46:30 +0000

    Update Copyright years

--- src/tests/fctest_elements.F90
+++ src/tests/fctest_elements.F90
@@ -1,4 +1,4 @@
-! (C) Copyright 1996-2015 ECMWF.
+! (C) Copyright 1996-2016 ECMWF.
 ! This software is licensed under the terms of the Apache Licence Version 2.0
 ! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 ! In applying this licence, ECMWF does not waive the privileges and immunities
