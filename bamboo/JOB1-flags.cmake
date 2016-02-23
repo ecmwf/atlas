121023fb5f62e213464b04655aa4c9d89c7cc2da
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2016-02-23 09:29:09 +0000

    bamboo: enable free Fortran line length for GNU and CLANG jobs

--- bamboo/JOB1-flags.cmake
+++ bamboo/JOB1-flags.cmake
@@ -1,5 +1,5 @@
 # don't use trans library until it supports build without MPI
-set( ENABLE_MPI                 OFF CACHE BOOL "Disable MPI under Clang compilation" )
+set( ENABLE_MPI                 OFF CACHE BOOL "Disable MPI under GNU compilation" )
 set( ENABLE_TRANS               ON  CACHE BOOL "Enable TRANS" )
 set( ENABLE_ATLAS_TEST_GRIDSPEC ON  CACHE BOOL "Enable atlas_test_gridspec")
 set( ENABLE_BOUNDSCHECKING      ON  CACHE BOOL "Enable bounds checking")
