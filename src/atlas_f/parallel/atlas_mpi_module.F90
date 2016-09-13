e18f2c8e16d43ff49fd885054555ec8faff54c81
Willem Deconinck <willem.deconinck@ecmwf.int>
2016-09-13 16:55:22 +0100

    ECKIT-158

--- src/atlas_f/parallel/atlas_mpi_module.F90
+++ src/atlas_f/parallel/atlas_mpi_module.F90
@@ -1,59 +1,68 @@
 module atlas_mpi_module
 
+#include "atlas_f/internals/atlas_f_mpi.h"
+
+! Temporary until fckit will wrap all of eckit::mpi
+integer, save :: tmp_comm = MPI_COMM_WORLD
+
 public
 contains
 
   subroutine atlas_mpi_init()
     integer :: ierr
-!    if (.not. atlas_mpi_initialized() ) call MPI_INIT( ierr )
+    if (.not. atlas_mpi_initialized() ) call MPI_INIT( ierr )
   end subroutine atlas_mpi_init
 
   subroutine atlas_mpi_finalize()
     integer :: ierr
-!    if (.not. atlas_mpi_finalized()) call MPI_FINALIZE (ierr)
+    if (.not. atlas_mpi_finalized()) call MPI_FINALIZE (ierr)
   end subroutine atlas_mpi_finalize
 
   function atlas_mpi_initialized()
     integer :: ierr
     logical :: atlas_mpi_initialized
-!    call MPI_INITIALIZED(atlas_mpi_initialized, ierr)
+    call MPI_INITIALIZED(atlas_mpi_initialized, ierr)
   end function atlas_mpi_initialized
 
   function atlas_mpi_finalized()
     integer :: ierr
     logical :: atlas_mpi_finalized
-!    call MPI_FINALIZED(atlas_mpi_finalized, ierr)
+    call MPI_FINALIZED(atlas_mpi_finalized, ierr)
   end function atlas_mpi_finalized
 
   function atlas_mpi_comm()
+    use atlas_mpi_c_binding
     integer :: atlas_mpi_comm
-!    atlas_mpi_comm = atlas_mpi_comm_fortran_communicator()
+    atlas_mpi_comm = tmp_comm
+    !atlas_mpi_comm_fortran_communicator()
   end function atlas_mpi_comm
 
   subroutine atlas_mpi_set_comm(comm)
+    use atlas_mpi_c_binding
     integer :: comm
-!    call atlas_mpi_comm_attach_fortran_communicator(comm)
+    tmp_comm = comm
+    call atlas_mpi_comm_attach_fortran_communicator(comm)
   end subroutine atlas_mpi_set_comm
 
   subroutine atlas_mpi_barrier(comm)
     integer, optional :: comm
     integer :: ierr
-!    if( present(comm) ) then
-!      call MPI_Barrier ( comm, ierr )
-!    else
-!      call MPI_Barrier ( atlas_mpi_comm(), ierr )
-!    endif
+    if( present(comm) ) then
+      call MPI_Barrier ( comm, ierr )
+    else
+      call MPI_Barrier ( atlas_mpi_comm(), ierr )
+    endif
   end subroutine atlas_mpi_barrier
 
   function atlas_mpi_rank(comm)
     integer, optional :: comm
     integer :: ierr
     integer :: atlas_mpi_rank
-!    if( present(comm) ) then
-!      call MPI_COMM_RANK( comm, atlas_mpi_rank, ierr )
-!    else
-!      call MPI_COMM_RANK( atlas_mpi_comm(), atlas_mpi_rank, ierr )
-!    endif
+    if( present(comm) ) then
+      call MPI_COMM_RANK( comm, atlas_mpi_rank, ierr )
+    else
+      call MPI_COMM_RANK( atlas_mpi_comm(), atlas_mpi_rank, ierr )
+    endif
   end function atlas_mpi_rank
 
   function atlas_mpi_proc(comm)
@@ -61,23 +70,23 @@ contains
     integer :: ierr
     integer :: irank
     integer :: atlas_mpi_proc
-!    if( present(comm) ) then
-!      call MPI_COMM_RANK( comm, irank, ierr )
-!    else
-!      call MPI_COMM_RANK( atlas_mpi_comm(), irank, ierr )
-!    endif
-!    atlas_mpi_proc = irank + 1
+    if( present(comm) ) then
+      call MPI_COMM_RANK( comm, irank, ierr )
+    else
+      call MPI_COMM_RANK( atlas_mpi_comm(), irank, ierr )
+    endif
+    atlas_mpi_proc = irank + 1
   end function atlas_mpi_proc
 
   function atlas_mpi_size(comm)
     integer, optional :: comm
     integer :: ierr
     integer :: atlas_mpi_size
-!    if( present(comm) ) then
-!      call MPI_COMM_SIZE( comm, atlas_mpi_size,  ierr )
-!    else
-!      call MPI_COMM_SIZE( atlas_mpi_comm(), atlas_mpi_size,  ierr )
-!    endif
+    if( present(comm) ) then
+      call MPI_COMM_SIZE( comm, atlas_mpi_size,  ierr )
+    else
+      call MPI_COMM_SIZE( atlas_mpi_comm(), atlas_mpi_size,  ierr )
+    endif
   end function atlas_mpi_size
 
 end module atlas_mpi_module
