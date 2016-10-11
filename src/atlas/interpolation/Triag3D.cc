a2e769b5751258633566d20416996e6222d957e9
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2016-10-11 18:05:19 +0100

    Snap to edges/corners only if outside of triangle (with Tiago)

--- src/atlas/interpolation/Triag3D.cc
+++ src/atlas/interpolation/Triag3D.cc
@@ -16,13 +16,10 @@
 //----------------------------------------------------------------------------------------------------------------------
 
 
-static const double parametricEpsilon = 1e-7; ///< Epsilon used to compare weights and u,v's
-
-
 namespace atlas {
 namespace interpolation {
 
-Intersect Triag3D::intersects(const Ray& r, double epsilon) const {
+Intersect Triag3D::intersects(const Ray& r, double edgeEpsilon, double epsilon) const {
 
     Intersect isect;
 
@@ -32,42 +29,72 @@ Intersect Triag3D::intersects(const Ray& r, double epsilon) const {
 
     // ray is parallel to triangle (check?)
     const double det = edge1.dot(pvec);
-    if (fabs(det) < epsilon) return isect.success(false);
-
-    // pick an epsilon based on a small proportion of the smallest edge length
-    // (this scales linearly so it better compares with linear weights u,v,w)
-    const double edgeEpsilon = std::max(epsilon, parametricEpsilon * std::sqrt(std::min(edge1.squaredNorm(), edge2.squaredNorm())));
+    if (fabs(det) < epsilon) {
+        return isect.fail();
+    }
 
     const double invDet = 1. / det;
+
     Vector3D tvec = r.orig - v0;
-    isect.u = tvec.dot(pvec) * invDet;
-
-    if (fabs(isect.u) < edgeEpsilon) isect.u = 0;
-    else if (fabs(1-isect.u) < edgeEpsilon) isect.u = 1;
-    else if (isect.u < 0 || isect.u > 1) return isect.success(false);
-
     Vector3D qvec = tvec.cross(edge1);
+
+    isect.u = tvec.dot(pvec)  * invDet;
     isect.v = r.dir.dot(qvec) * invDet;
+    isect.t = edge2.dot(qvec) * invDet;
 
-    if (fabs(isect.v) < edgeEpsilon) isect.v = 0;
-    else if (fabs(1-isect.v) < edgeEpsilon) isect.v = 1;
-    else if (isect.v < 0 || isect.v > 1) return isect.success(false);
-
-    // check if projection point is near the diagonal of reference triangle
-    // if very close (but outside) to diagonal, equally correct u and v to the inside
     const double w = 1 - (isect.u + isect.v);
+
     if (w < 0) {
-        if (fabs(w) < edgeEpsilon) {
-            isect.u += 0.5 * w;
-            isect.v += 0.5 * w;
-        } else {
-            return isect.success(false);
+
+        // check if far outside of triangle, in respect to diagonal edge
+        if (w < -edgeEpsilon) {
+            return isect.fail();
+        }
+
+        // snap to diagonal edge
+        // Note: we may still be to the left of vertical edge or below the horizontal edge
+        isect.u += 0.5 * w;
+        isect.v += 0.5 * w;
+    }
+
+    if (isect.u < 0) {
+
+        // check if far outside of triangle, in respect to vertical edge
+        if ( (isect.u < -edgeEpsilon) ||
+             (isect.v < -edgeEpsilon) ||
+             (isect.v > 1 + edgeEpsilon) ) {
+            return isect.fail();
+        }
+
+        // snap to lower/upper left corners
+        isect.u = 0;
+        if (isect.v < 0) {
+            isect.v = 0;
+        } else if (isect.v > 1) {
+            isect.v = 1;
+        }
+
+    }
+
+    if (isect.v < 0) {
+
+        // check if far outside of triangle, in respect to horizontal edge
+        if( (isect.v < -edgeEpsilon) ||
+            (isect.u < -edgeEpsilon) ||
+            (isect.u > 1 + edgeEpsilon) ) {
+            return isect.fail();
+        }
+
+        // snap to lower left/right corners
+        isect.v = 0;
+        if (isect.u < 0) {
+            isect.u = 0;
+        } else if (isect.u > 1) {
+            isect.u = 1;
         }
     }
 
-    isect.t = edge2.dot(qvec) * invDet;
-
-    return isect.success(true);
+    return isect.success();
 }
 
 double Triag3D::area() const
