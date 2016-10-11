4094cec9f1259456644fbbe6013b4161770fc2a3
Willem Deconinck <willem.deconinck@ecmwf.int>
2016-10-11 10:14:38 +0100

    Fix sandbox compilation

--- src/sandbox/example_fortran/example_fortran.F90
+++ src/sandbox/example_fortran/example_fortran.F90
@@ -16,44 +16,19 @@ do j=1,2000
   var(j) = 1
 enddo
 
-call atlas_log%set_debug(10)
-
-
 call atlas_log%info("Atlas initialized")
 call atlas_log%info("version = ["//atlas_version()//"]")
 
 call atlas_init()
 
-call atlas_log%disconnect_stdout()
-call atlas_log%connect_fortran_unit(6)
-
-call atlas_log%channel_info %set_prefix("[%P] info  -- ")
-call atlas_log%channel_debug%set_prefix("[%P] debug -- ")
-call atlas_log%channel_stats%set_prefix("(S) -- ")
+call atlas_log%set_fortran_unit(6)
 
 #ifdef ATLAS_HAVE_OMP
 call atlas_log%info("OpenMP enabled")
 #endif
 
 call atlas_log%debug("Here is some debugging information")
-call atlas_log%stats("value = ...")
 
-call atlas_log%info("Section")
-call atlas_log%indent("|   ")
-call atlas_log%info("Subsection")
-call atlas_log%indent("|   ")
-call atlas_log%info("Subsubsection")
-call atlas_log%indent("|   ")
-call atlas_log%info("stuff")
-call atlas_log%debug("more stuff")
-call atlas_log%stats("lolo")
-call atlas_log%dedent()
-call atlas_log%info("Subsubsection")
-call atlas_log%dedent()
-call atlas_log%info("Subsection")
-call atlas_log%dedent()
-call atlas_log%info("Section")
-call atlas_log%debug("Atlas finalized",lvl=0,flush=.True.)
 write(6,'(A)') "exit"
 call atlas_finalize()
 end program
