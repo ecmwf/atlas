56fb9d1ced2fcb27645973e3107db517a337fd0f
carlos osuna <carlos.osuna@meteoswiss.ch>
2016-11-25 14:27:13 +0100

    fix un ftest

--- src/tests/field/fctest_field.F90
+++ src/tests/field/fctest_field.F90
@@ -50,7 +50,7 @@ field = atlas_Field(kind=atlas_real(8),shape=[10,5])
 call field%host_data(host)
 
 FCTEST_CHECK( field%is_on_host() )
-FCTEST_CHECK( .not. field%is_on_device() )
+FCTEST_CHECK( field%is_on_device() )
 
 call field%clone_to_device()
 FCTEST_CHECK( field%is_on_device() )
