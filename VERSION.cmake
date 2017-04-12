0285f42217522afc86c0be9dce0ee69d23513359
Willem Deconinck <willem.deconinck@ecmwf.int>
2017-04-12 17:19:34 +0100

    No longer use VERSION.tex

--- VERSION.cmake
+++ VERSION.cmake
@@ -6,6 +6,5 @@
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
-file ( STRINGS "VERSION.tex" ATLASVER )
-set  ( ${PROJECT_NAME}_VERSION_STR  ${ATLASVER} )
+set  ( ${PROJECT_NAME}_VERSION_STR  "0.10.0" )
 
