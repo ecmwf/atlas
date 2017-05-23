a4ffe4044d4c6340cd7fbab7cebda9369d9bd050
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2017-05-23 19:53:54 +0100

    MIR-189: calculate cell centroids only using "real" nodes

--- src/atlas/mesh/actions/BuildCellCentres.cc
+++ src/atlas/mesh/actions/BuildCellCentres.cc
@@ -8,13 +8,13 @@
  * does it submit to any jurisdiction.
  */
 
+#include <limits>
 #include "atlas/mesh/Mesh.h"
 #include "atlas/mesh/Nodes.h"
 #include "atlas/mesh/HybridElements.h"
 #include "atlas/mesh/actions/BuildCellCentres.h"
 #include "atlas/field/Field.h"
 #include "atlas/util/CoordinateEnums.h"
-#include "atlas/array/ArrayView.h"
 #include "atlas/array/MakeView.h"
 
 namespace atlas {
@@ -25,33 +25,47 @@ namespace actions {
 
 void BuildCellCentres::operator()( Mesh& mesh ) const
 {
-    if( !mesh.cells().has_field("centre") ) {
+    if (!mesh.cells().has_field("centre")) {
 
-        mesh::Nodes& nodes     = mesh.nodes();
-        array::ArrayView<double,2> coords = array::make_view<double,2>( nodes.field("xyz") );
+        mesh::Nodes& nodes = mesh.nodes();
+        array::ArrayView<double, 2> coords = array::make_view<double, 2>( nodes.field("xyz") );
+
+        size_t firstVirtualPoint = std::numeric_limits<size_t>::max();
+        if (nodes.metadata().has("NbRealPts")) {
+            firstVirtualPoint = nodes.metadata().get<size_t>("NbRealPts");
+        }
 
         size_t nb_cells = mesh.cells().size();
-        array::ArrayView<double,2> centroids = array::make_view<double,2>( 
-          mesh.cells().add(
-           Field("centre", array::make_datatype<double>(), array::make_shape(nb_cells,3))) );
+        array::ArrayView<double, 2> centroids = array::make_view<double, 2>(
+                    mesh.cells().add(
+                        Field("centre", array::make_datatype<double>(), array::make_shape(nb_cells,3))) );
         const mesh::HybridElements::Connectivity& cell_node_connectivity = mesh.cells().node_connectivity();
 
         for (size_t e=0; e<nb_cells; ++e)
         {
-            centroids(e,XX) = 0.;
-            centroids(e,YY) = 0.;
-            centroids(e,ZZ) = 0.;
+            centroids(e, XX) = 0.;
+            centroids(e, YY) = 0.;
+            centroids(e, ZZ) = 0.;
+            size_t nb_real_nodes = 0;
+
             const size_t nb_nodes_per_elem = cell_node_connectivity.cols(e);
-            const double average_coefficient = 1./static_cast<double>(nb_nodes_per_elem);
             for (size_t n=0; n<nb_nodes_per_elem; ++n)
             {
-                centroids(e,XX) += coords( cell_node_connectivity(e,n), XX );
-                centroids(e,YY) += coords( cell_node_connectivity(e,n), YY );
-                centroids(e,ZZ) += coords( cell_node_connectivity(e,n), ZZ );
+                const size_t i = size_t(cell_node_connectivity(e, n));
+                if (i < firstVirtualPoint) {
+                    ++nb_real_nodes;
+                    centroids(e, XX) += coords(i, XX);
+                    centroids(e, YY) += coords(i, YY);
+                    centroids(e, ZZ) += coords(i, ZZ);
+                }
             }
-            centroids(e,XX) *= average_coefficient;
-            centroids(e,YY) *= average_coefficient;
-            centroids(e,ZZ) *= average_coefficient;
+
+            if (nb_real_nodes > 1) {
+                const double average_coefficient = 1./static_cast<double>(nb_real_nodes);
+                centroids(e, XX) *= average_coefficient;
+                centroids(e, YY) *= average_coefficient;
+                centroids(e, ZZ) *= average_coefficient;
+            }
         }
     }
 }
