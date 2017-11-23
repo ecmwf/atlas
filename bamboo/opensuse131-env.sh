d1c0dd8e241f2b17a1b2d8147d98546d48b85520
Willem Deconinck <willem.deconinck@ecmwf.int>
2017-11-23 10:24:06 +0000

    Update bamboo's cmake version when using fckit

--- bamboo/opensuse131-env.sh
+++ bamboo/opensuse131-env.sh
@@ -4,3 +4,12 @@
 # CXX=mpicxx
 # FC=mpif90
 ulimit -s unlimited
+
+# Initialise module environment if it is not
+if [[ ! $(command -v module > /dev/null 2>&1) ]]; then
+  . /usr/local/apps/module/init/bash
+fi
+
+# Minimum required cmake (3.1) when using fckit only
+module unload cmake
+module load cmake/3.7.1
