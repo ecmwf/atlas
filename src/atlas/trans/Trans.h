ecmwf/atlas

commit 911b19e5dbd26ad367ed51460ddd91c090a2e566

ATLAS-134 Caching for Trans with unit tests

src/atlas/trans/Trans.h
@@ -13,6 +13,10 @@
 #include "eckit/config/Configuration.h"
 #include "eckit/memory/Owned.h"
 #include "eckit/memory/SharedPtr.h"
+
+#include "eckit/io/Buffer.h"
+#include "eckit/io/DataHandle.h"
+
 #include "atlas/util/Config.h"
 
 //-----------------------------------------------------------------------------
@@ -34,17 +38,58 @@ namespace trans {
 
 class TransCacheEntry {
 public:
-  virtual operator bool() const = 0;
+  operator bool() const { return size() != 0; }
   virtual size_t size() const = 0;
   virtual const void* data() const = 0;
-  virtual std::string hash() const = 0;
 };
 
-class TransCache {
+class EmptyCacheEntry : public TransCacheEntry {
 public:
-  virtual const TransCacheEntry& legendre() const = 0;
-  virtual const TransCacheEntry& fft()      const = 0;
+  virtual size_t size() const override { return 0; }
+  virtual const void* data() const override { return nullptr; }
 };
+
+class TransCacheFileEntry : public TransCacheEntry {
+  eckit::Buffer buffer_;
+public:
+  TransCacheFileEntry( const eckit::PathName& path ) :
+    buffer_(path.size()) {
+    std::unique_ptr<eckit::DataHandle> dh( path.fileHandle() );
+    dh->openForRead();
+    dh->read(buffer_.data(),buffer_.size());
+    dh->close();
+  }
+  virtual size_t size() const override { return buffer_.size(); }
+  virtual const void* data() const override { return buffer_.data(); }
+};
+
+class Cache {
+public:
+  Cache() = default;
+  Cache( const Cache& other ) = default;
+protected:
+  Cache( const std::shared_ptr<TransCacheEntry>& legendre ) :
+    legendre_(legendre) {
+  }
+  Cache( const std::shared_ptr<TransCacheEntry>& legendre, const std::shared_ptr<TransCacheEntry>& fft ) :
+    legendre_(legendre),
+    fft_(fft) {
+  }
+public:
+  const TransCacheEntry& legendre() const { return *legendre_; }
+  const TransCacheEntry& fft()      const { return *fft_; }
+private:
+  std::shared_ptr<TransCacheEntry> legendre_{ new EmptyCacheEntry() };
+  std::shared_ptr<TransCacheEntry> fft_{ new EmptyCacheEntry() } ;
+};
+
+class LegendreCache : public Cache {
+public:
+  LegendreCache( const eckit::PathName& path ) :
+    Cache( std::shared_ptr<TransCacheEntry>( new TransCacheFileEntry( path ) ) ) {
+  }
+};
+
 
 class TransImpl : public eckit::Owned {
 
@@ -53,6 +98,8 @@ public:
     virtual ~TransImpl() = 0;
 
     virtual int truncation() const = 0;
+
+    virtual size_t spectralCoefficients() const = 0;
 
     virtual const Grid& grid() const = 0;
 
@@ -137,6 +184,7 @@ public:
      */
     virtual void dirtrans( const int nb_fields, const double wind_fields[], double vorticity_spectra[], double divergence_spectra[],
                            const eckit::Configuration& = util::NoConfig() ) const = 0;
+
 };
 
 // ------------------------------------------------------------------
@@ -153,8 +201,8 @@ public:
     static TransImpl* build( const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& = util::Config() );
     static TransImpl* build( const Grid&, int truncation, const eckit::Configuration& = util::Config() );
 
-    static TransImpl* build( const TransCache&, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& = util::Config() );
-    static TransImpl* build( const TransCache&, const Grid&, int truncation, const eckit::Configuration& = util::Config() );
+    static TransImpl* build( const Cache&, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& = util::Config() );
+    static TransImpl* build( const Cache&, const Grid&, int truncation, const eckit::Configuration& = util::Config() );
 
     /*!
      * \brief list all registered trans implementations
@@ -168,8 +216,8 @@ private:
     std::string name_;
     virtual TransImpl* make( const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& ) { return nullptr; }
     virtual TransImpl* make( const Grid& gp, int truncation, const eckit::Configuration& ) { return nullptr; }
-    virtual TransImpl* make( const TransCache&, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& ) { return nullptr; }
-    virtual TransImpl* make( const TransCache&, const Grid& gp, int truncation, const eckit::Configuration& ) { return nullptr; }
+    virtual TransImpl* make( const Cache&, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& ) { return nullptr; }
+    virtual TransImpl* make( const Cache&, const Grid& gp, int truncation, const eckit::Configuration& ) { return nullptr; }
 
 protected:
 
@@ -185,7 +233,7 @@ class TransBuilderFunctionSpace : public TransFactory {
   virtual TransImpl* make( const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& config ) {
         return new T(gp,sp,config);
   }
-  virtual TransImpl* make( const TransCache& cache, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& config ) {
+  virtual TransImpl* make( const Cache& cache, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& config ) {
         return new T(cache,gp,sp,config);
   }
 public:
@@ -197,7 +245,7 @@ class TransBuilderGrid : public TransFactory {
   virtual TransImpl* make( const Grid& grid, int truncation, const eckit::Configuration& config ) {
         return new T(grid,truncation,config);
   }
-  virtual TransImpl* make( const TransCache& cache, const Grid& grid, int truncation, const eckit::Configuration& config ) {
+  virtual TransImpl* make( const Cache& cache, const Grid& grid, int truncation, const eckit::Configuration& config ) {
         return new T(cache,grid,truncation,config);
   }
 public:
@@ -225,14 +273,15 @@ public:
     Trans( const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& = util::NoConfig() );
     Trans( const Grid&, int truncation, const eckit::Configuration& = util::NoConfig() );
 
-    Trans( const TransCache&, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& = util::NoConfig() );
-    Trans( const TransCache&, const Grid&, int truncation, const eckit::Configuration& = util::NoConfig() );
+    Trans( const Cache&, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& = util::NoConfig() );
+    Trans( const Cache&, const Grid&, int truncation, const eckit::Configuration& = util::NoConfig() );
 
     void hash(eckit::Hash&) const;
     const Implementation* get() const { return impl_.get(); }
     operator bool() const { return impl_.owners(); }
 
     int truncation() const;
+    size_t spectralCoefficients() const;
     const Grid& grid() const;
 
     void dirtrans( const Field& gpfield,

src/atlas/CMakeLists.txt
@@ -55,6 +55,7 @@ list( APPEND atlas_grid_srcs
 
 option.h
 option/Options.h
+option/Options.cc
 
 
 projection.h
@@ -322,12 +323,12 @@ trans/Trans.cc
 )
 if( ATLAS_HAVE_TRANS )
 list( APPEND atlas_numerics_srcs
-  trans/detail/TransIFS.h
-  trans/detail/TransIFS.cc
-  trans/detail/ifs/TransIFSNodeColumns.h
-  trans/detail/ifs/TransIFSNodeColumns.cc
-  trans/detail/ifs/TransIFSStructuredColumns.h
-  trans/detail/ifs/TransIFSStructuredColumns.cc
+  trans/ifs/TransIFS.h
+  trans/ifs/TransIFS.cc
+  trans/ifs/TransIFSNodeColumns.h
+  trans/ifs/TransIFSNodeColumns.cc
+  trans/ifs/TransIFSStructuredColumns.h
+  trans/ifs/TransIFSStructuredColumns.cc
 )
 endif()
 

src/atlas/functionspace/Spectral.cc
@@ -23,7 +23,7 @@
 #include "atlas/trans/Trans.h"
 
 #ifdef ATLAS_HAVE_TRANS
-#include "atlas/trans/detail/TransIFS.h"
+#include "atlas/trans/ifs/TransIFS.h"
 namespace {
 void trans_check(const int code, const char* msg, const eckit::CodeLocation& location) {
   if(code != TRANS_SUCCESS) {

src/atlas/functionspace/StructuredColumns.h
@@ -36,12 +36,6 @@ namespace field {
 }
 
 namespace atlas {
-namespace trans {
-    class TransIFS;
-}
-}
-
-namespace atlas {
 namespace functionspace {
 
 // -------------------------------------------------------------------

src/atlas/grid/detail/partitioner/TransPartitioner.cc
@@ -12,7 +12,7 @@
 #include "atlas/grid/Grid.h"
 #include "atlas/grid/detail/partitioner/TransPartitioner.h"
 #include "atlas/grid/detail/partitioner/EqualRegionsPartitioner.h"
-#include "atlas/trans/detail/TransIFS.h"
+#include "atlas/trans/ifs/TransIFS.h"
 #include "atlas/parallel/mpi/mpi.h"
 #include "atlas/array.h"
 #include "atlas/runtime/Trace.h"

src/atlas/option/Options.cc
@@ -1,0 +1,99 @@
+/*
+ * (C) Copyright 1996-2017 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#include "atlas/option/Options.h"
+
+// ----------------------------------------------------------------------------
+
+namespace atlas {
+namespace option {
+
+vorticity_divergence_fields::vorticity_divergence_fields(bool v) { set("vorticity_divergence_fields",v); }
+
+wind_EW_derivatives::wind_EW_derivatives(bool v) { set("wind_EW_derivatives",v); }
+
+scalar_derivatives::scalar_derivatives(bool v) { set("scalar_derivatives",v); }
+
+radius::radius(double _radius)
+{
+  set("radius",_radius);
+}
+
+radius::radius(const std::string &key)
+{
+  if( key == "Earth" ) {
+    set("radius",util::Earth::radiusInMeters());
+  } else {
+    NOTIMP;
+  }
+}
+
+type::type(const std::string &_type)
+{
+  set("type",_type);
+}
+
+halo::halo(size_t size)
+{
+  set("halo",size);
+}
+
+datatype::datatype(array::DataType::kind_t kind)
+{
+  set("datatype",kind);
+}
+
+datatype::datatype(const std::string &str )
+{
+  set("datatype",array::DataType::str_to_kind(str));
+}
+
+datatype::datatype(array::DataType dtype)
+{
+  set("datatype",dtype.kind());
+}
+
+name::name(const std::string &_name)
+{
+  set("name",_name);
+}
+
+global::global(size_t _owner)
+{
+  set("global",true);
+  set("owner",_owner);
+}
+
+levels::levels(size_t _levels)
+{
+  set("levels",_levels);
+}
+
+variables::variables(size_t _variables)
+{
+  set("variables",_variables);
+}
+
+flt::flt(bool flt) { set("flt",flt); }
+
+static const std::map<FFT,std::string> FFT_to_string = { {FFT992,"FFT992"},{FFTW,"FFTW"} };
+//static const std::map<std::string,FFT> string_to_FFT = { {"FFT992",FFT992},{"FFTW",FFTW} };
+
+fft::fft( FFT fft ) { set("fft",FFT_to_string.at(fft)); }
+fft::fft( const std::string& fft ) { set("fft",fft); }
+split_latitudes::split_latitudes(bool split_latitudes) { set("split_latitudes",split_latitudes); }
+write_legendre::write_legendre( const eckit::PathName& filepath ) { set("write_legendre",filepath); }
+read_legendre::read_legendre( const eckit::PathName& filepath ) { set("read_legendre",filepath); }
+
+
+// ----------------------------------------------------------------------------
+
+} // namespace option
+} // namespace atlas

src/atlas/option/Options.h
@@ -21,109 +21,140 @@ namespace option {
 
 // ----------------------------------------------------------------------------
 
-class type : public util::Config
-{
+class type : public util::Config {
 public:
-  type( const std::string& _type )
-  {
-    set("type",_type);
-  }
+  type( const std::string& );
 };
-
-class global : public util::Config
-{
-public:
-  global(size_t _owner=0)
-  {
-    set("global",true);
-    set("owner",_owner);
-  }
-};
-
-class levels : public util::Config
-{
-public:
-  levels(size_t _levels)
-  {
-    set("levels",_levels);
-  }
-};
-
-class variables : public util::Config
-{
-public:
-  variables(size_t _variables)
-  {
-    set("variables",_variables);
-  }
-};
-
-class name : public util::Config
-{
-public:
-  name(const std::string& _name)
-  {
-    set("name",_name);
-  }
-};
-
-template< typename T >
-class datatypeT : public util::Config
-{
-public:
-  datatypeT()
-  {
-    set("datatype",array::DataType::kind<T>());
-  }
-};
-
-
-class datatype : public util::Config
-{
-public:
-  datatype(array::DataType::kind_t kind)
-  {
-    set("datatype",kind);
-  }
-  datatype(const std::string& str)
-  {
-    set("datatype",array::DataType::str_to_kind(str));
-  }
-  datatype(array::DataType dtype)
-  {
-    set("datatype",dtype.kind());
-  }
-};
-
-class halo : public util::Config
-{
-public:
-  halo(size_t size)
-  {
-    set("halo",size);
-  }
-};
-
-class radius : public util::Config
-{
-public:
-  radius(double _radius)
-  {
-    set("radius",_radius);
-  }
-  radius(const std::string& key = "Earth")
-  {
-    if( key == "Earth" ) {
-      set("radius",util::Earth::radiusInMeters());
-    } else {
-      NOTIMP;
-    }
-  }
-
-};
-
 
 // ----------------------------------------------------------------------------
+
+class global : public util::Config {
+public:
+  global( size_t owner = 0 );
+};
+
+// ----------------------------------------------------------------------------
+
+class levels : public util::Config {
+public:
+  levels( size_t );
+};
+
+// ----------------------------------------------------------------------------
+
+class variables : public util::Config {
+public:
+  variables( size_t );
+};
+
+// ----------------------------------------------------------------------------
+
+class name : public util::Config {
+public:
+  name( const std::string& );
+};
+
+// ----------------------------------------------------------------------------
+
+template< typename T >
+class datatypeT : public util::Config {
+public:
+  datatypeT();
+};
+
+// ----------------------------------------------------------------------------
+
+class datatype : public util::Config {
+public:
+  datatype( array::DataType::kind_t );
+  datatype( const std::string& );
+  datatype( array::DataType );
+};
+
+// ----------------------------------------------------------------------------
+
+class halo : public util::Config {
+public:
+  halo(size_t size);
+};
+
+// ----------------------------------------------------------------------------
+
+class radius : public util::Config {
+public:
+  radius( double );
+  radius( const std::string& = "Earth" );
+};
+
+// ----------------------------------------------------------------------------
+
+class scalar_derivatives : public util::Config {
+public:
+  scalar_derivatives( bool );
+};
+
+// ----------------------------------------------------------------------------
+
+class wind_EW_derivatives : public util::Config {
+public:
+  wind_EW_derivatives( bool );
+};
+
+// ----------------------------------------------------------------------------
+
+class vorticity_divergence_fields : public util::Config {
+public:
+  vorticity_divergence_fields( bool );
+};
+
+// ----------------------------------------------------------------------------
+
+class flt : public util::Config {
+public:
+  flt( bool );
+};
+
+// ----------------------------------------------------------------------------
+
+enum FFT { FFT992=1, FFTW=2 };
+
+class fft : public util::Config {
+public:
+  fft( FFT );
+  fft( const std::string& );
+};
+
+// ----------------------------------------------------------------------------
+
+class split_latitudes : public util::Config {
+public:
+  split_latitudes( bool );
+};
+
+// ----------------------------------------------------------------------------
+
+class write_legendre : public util::Config {
+public:
+  write_legendre( const eckit::PathName& );
+};
+
+// ----------------------------------------------------------------------------
+
+class read_legendre : public util::Config {
+public:
+  read_legendre( const eckit::PathName& );
+};
+
+// ----------------------------------------------------------------------------
+// Definitions
+// ----------------------------------------------------------------------------
+
+template<typename T>
+datatypeT<T>::datatypeT() {
+  set("datatype",array::DataType::kind<T>());
+}
+
+
 
 } // namespace option
 } // namespace atlas

src/atlas/trans/Trans.cc
@@ -19,8 +19,8 @@
 #include "atlas/grid/Grid.h"
 
 #ifdef ATLAS_HAVE_TRANS
-#include "atlas/trans/detail/ifs/TransIFSNodeColumns.h"
-#include "atlas/trans/detail/ifs/TransIFSStructuredColumns.h"
+#include "atlas/trans/ifs/TransIFSNodeColumns.h"
+#include "atlas/trans/ifs/TransIFSStructuredColumns.h"
 #define TRANS_DEFAULT "ifs"
 #else
 #define TRANS_DEFAULT "local"
@@ -136,7 +136,7 @@ Trans::Implementation *TransFactory::build( const FunctionSpace& gp, const Funct
     return (*j).second->make(gp,sp,config);
 }
 
-Trans::Implementation *TransFactory::build( const TransCache& cache, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& config ) {
+Trans::Implementation *TransFactory::build( const Cache& cache, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& config ) {
 
     pthread_once(&once, init);
 
@@ -195,7 +195,7 @@ Trans::Implementation *TransFactory::build( const Grid& grid, int truncation, co
     return (*j).second->make(grid,truncation,config);
 }
 
-Trans::Implementation *TransFactory::build( const TransCache& cache, const Grid& grid, int truncation, const eckit::Configuration& config ) {
+Trans::Implementation *TransFactory::build( const Cache& cache, const Grid& grid, int truncation, const eckit::Configuration& config ) {
 
     pthread_once(&once, init);
 
@@ -241,11 +241,11 @@ Trans::Trans( const Grid& grid, int truncation, const eckit::Configuration& conf
   impl_( TransFactory::build(grid,truncation,config) ) {
 }
 
-Trans::Trans( const TransCache& cache, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& config ) :
+Trans::Trans( const Cache& cache, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& config ) :
   impl_( TransFactory::build(cache,gp,sp,config) ) {
 }
 
-Trans::Trans( const TransCache& cache, const Grid& grid, int truncation, const eckit::Configuration& config ) :
+Trans::Trans( const Cache& cache, const Grid& grid, int truncation, const eckit::Configuration& config ) :
   impl_( TransFactory::build(cache,grid,truncation,config) ) {
 }
 

commit 75cc3d5467f3635a98d26d1a8df5081e97d4152d

Fixes for Cray broken since 911b19e5

src/atlas/trans/Trans.h
@@ -10,10 +10,11 @@
 
 #pragma once
 
+#include <memory>
+
 #include "eckit/config/Configuration.h"
 #include "eckit/memory/Owned.h"
 #include "eckit/memory/SharedPtr.h"
-
 #include "eckit/io/Buffer.h"
 #include "eckit/io/DataHandle.h"
 
@@ -65,11 +66,15 @@ public:
 
 class Cache {
 public:
-  Cache() = default;
+  Cache() :
+    legendre_( new EmptyCacheEntry() ),
+    fft_( new EmptyCacheEntry() ) {
+  }
   Cache( const Cache& other ) = default;
 protected:
   Cache( const std::shared_ptr<TransCacheEntry>& legendre ) :
-    legendre_(legendre) {
+    legendre_(legendre),
+    fft_( new EmptyCacheEntry() ) {
   }
   Cache( const std::shared_ptr<TransCacheEntry>& legendre, const std::shared_ptr<TransCacheEntry>& fft ) :
     legendre_(legendre),
@@ -79,8 +84,8 @@ public:
   const TransCacheEntry& legendre() const { return *legendre_; }
   const TransCacheEntry& fft()      const { return *fft_; }
 private:
-  std::shared_ptr<TransCacheEntry> legendre_{ new EmptyCacheEntry() };
-  std::shared_ptr<TransCacheEntry> fft_{ new EmptyCacheEntry() } ;
+  std::shared_ptr<TransCacheEntry> legendre_;
+  std::shared_ptr<TransCacheEntry> fft_;
 };
 
 class LegendreCache : public Cache {
@@ -236,6 +241,12 @@ class TransBuilderFunctionSpace : public TransFactory {
   virtual TransImpl* make( const Cache& cache, const FunctionSpace& gp, const FunctionSpace& sp, const eckit::Configuration& config ) {
         return new T(cache,gp,sp,config);
   }
+  virtual TransImpl* make( const Grid&, int, const eckit::Configuration& ) {
+        throw eckit::SeriousBug("This function should not be called",Here());
+  }
+  virtual TransImpl* make( const Cache&, const Grid&, int, const eckit::Configuration& ) {
+        throw eckit::SeriousBug("This function should not be called",Here());
+  }
 public:
     TransBuilderFunctionSpace(const std::string& name) : TransFactory(name) {}
 };
@@ -247,6 +258,12 @@ class TransBuilderGrid : public TransFactory {
   }
   virtual TransImpl* make( const Cache& cache, const Grid& grid, int truncation, const eckit::Configuration& config ) {
         return new T(cache,grid,truncation,config);
+  }
+  virtual TransImpl* make( const FunctionSpace&, const FunctionSpace&, const eckit::Configuration& ) {
+        throw eckit::SeriousBug("This function should not be called",Here());
+  }
+  virtual TransImpl* make( const Cache&, const FunctionSpace&, const FunctionSpace&, const eckit::Configuration& ) {
+        throw eckit::SeriousBug("This function should not be called",Here());
   }
 public:
     TransBuilderGrid(const std::string& name) : TransFactory(name) {}