61c60858bd539c5867d90e3dd3e34002417d0cbf
Willem Deconinck <willem.deconinck@ecmwf.int>
2018-05-10 16:09:22 +0100

    Fix version suffix

--- VERSION.cmake
+++ VERSION.cmake
@@ -6,5 +6,5 @@
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
-set  ( ${PROJECT_NAME}_VERSION_STR  "0.14.0-opt-translocal" )
+set  ( ${PROJECT_NAME}_VERSION_STR  "0.14.0-develop" )
 
