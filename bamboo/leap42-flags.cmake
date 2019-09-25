078d8cff1799a7ab443be196e61497db237c20a3
Willem Deconinck <willem.deconinck@ecmwf.int>
2019-09-25 09:07:09 +0100

    bamboo: leap42-gcc with proj enabled

--- bamboo/leap42-flags.cmake
+++ bamboo/leap42-flags.cmake
@@ -2,3 +2,4 @@
 set( ENABLE_TRANS               ON  CACHE BOOL "Enable TRANS" )
 set( ENABLE_BOUNDSCHECKING      ON  CACHE BOOL "Enable bounds checking")
 set( ENABLE_TESSELATION         OFF CACHE BOOL "Disable CGAL" ) # cgal is old in leap42
+set( ENABLE_PROJ                ON  CACHE BOOL "Enable proj" )
