389a6cf95e19c3b76ad1ee2d7ca728c46e27a162
Willem Deconinck <willem.deconinck@ecmwf.int>
2019-09-25 11:04:46 +0100

    bamboo: leap42-gcc with proj enabled

--- bamboo/GCC-flags.cmake
+++ bamboo/GCC-flags.cmake
@@ -1,5 +1,5 @@
-# don't use trans library until it supports build without MPI
+set( ENABLE_MPI                 OFF CACHE BOOL "Disable MPI under Intel compilation" )
 set( ENABLE_TRANS               ON  CACHE BOOL "Enable TRANS" )
 set( ENABLE_BOUNDSCHECKING      ON  CACHE BOOL "Enable bounds checking")
-set( ENABLE_TESSELATION         OFF CACHE BOOL "Disable CGAL" ) # cgal is old in leap42
 set( ENABLE_PROJ                ON  CACHE BOOL "Enable proj" )
+set( ENABLE_TESSELATION         OFF CACHE BOOL "Disable CGAL" ) # cgal is old in leap42
