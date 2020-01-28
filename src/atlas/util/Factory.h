ef0d7e39852ab5fbe5d9836051a4ad4499a8af14
Willem Deconinck <willem.deconinck@ecmwf.int>
2020-01-28 14:26:41 +0000

    ATLAS-262 Fix self registration from external library

--- src/atlas/util/Factory.h
+++ src/atlas/util/Factory.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #include <map>
+#include <memory>
 #include <mutex>
 #include <string>
 #include <vector>
@@ -27,14 +28,15 @@ class FactoryBase;
 class FactoryRegistry {
 protected:
     FactoryRegistry( const std::string& factory );
-    ~FactoryRegistry();
+    virtual ~FactoryRegistry();
 
 private:
     mutable std::mutex mutex_;
-    std::map<std::string, FactoryBase*> factories_;
     std::string factory_;
+    std::map<std::string, FactoryBase*> factories_;
 
 public:
+    const std::string& factory() const { return factory_; }
     std::vector<std::string> keys() const;
     void list( std::ostream& ) const;
     bool has( const std::string& builder ) const;
@@ -46,24 +48,35 @@ public:
 template <typename T>
 struct FactoryRegistryT : public FactoryRegistry {
 public:
-    static FactoryRegistryT<T>& instance() {
-        static FactoryRegistryT<T> env( T::className() );
+    static std::shared_ptr<FactoryRegistryT<T>> instance() {
+        struct make_shared_enabler : public FactoryRegistryT<T> {
+            make_shared_enabler( const std::string& factory ) : FactoryRegistryT<T>( factory ) {}
+        };
+
+        static auto env = std::make_shared<make_shared_enabler>( T::className() );
         return env;
     }
 
-private:
+protected:
     FactoryRegistryT( const std::string& factory ) : FactoryRegistry( factory ) {}
+    virtual ~FactoryRegistryT() {}
 };
 
 class FactoryBase {
 private:
     FactoryRegistry& registry_;
     std::string builder_;
+    std::shared_ptr<FactoryRegistry> attached_registry_;
 
 protected:
     FactoryBase( FactoryRegistry&, const std::string& builder );
     virtual ~FactoryBase();
+    void attach_registry( const std::shared_ptr<FactoryRegistry>& registry ) { attached_registry_ = registry; }
     friend class FactoryRegistry;
+
+public:
+    const std::string& factoryBuilder() const { return builder_; }
+    const std::string& factoryName() const { return registry_.factory(); }
 };
 
 template <typename T>
@@ -74,11 +87,15 @@ public:
     static bool has( const std::string& builder ) { return registry().has( builder ); }
     static T* get( const std::string& builder ) { return dynamic_cast<T*>( registry().get( builder ) ); }
 
-    Factory( const std::string& builder = "" ) : FactoryBase( registry(), builder ) {}
+    Factory( const std::string& builder = "" ) : FactoryBase( registry(), builder ) {
+        if ( not builder.empty() ) {
+            attach_registry( FactoryRegistryT<T>::instance() );
+        }
+    }
 
 protected:
-    virtual ~Factory() = default;
-    static FactoryRegistryT<T>& registry() { return FactoryRegistryT<T>::instance(); }
+    virtual ~Factory(){};
+    static FactoryRegistry& registry() { return *FactoryRegistryT<T>::instance().get(); }
 };
 
 //----------------------------------------------------------------------------------------------------------------------
