525a71ef20e288d599cd53dedea8afab0e5cb3ae
Willem Deconinck <willem.deconinck@ecmwf.int>
2020-03-18 22:03:39 +0000

    Add test for PointCloud::createField

--- src/tests/functionspace/test_pointcloud.cc
+++ src/tests/functionspace/test_pointcloud.cc
@@ -10,6 +10,7 @@
 
 #include "atlas/array.h"
 #include "atlas/functionspace/PointCloud.h"
+#include "atlas/option.h"
 
 #include "tests/AtlasTestEnvironment.h"
 
@@ -33,6 +34,36 @@ CASE( "test_functionspace_PointCloud" ) {
     points.dump( Log::info() );
     Log::info() << std::endl;
 }
+
+//-----------------------------------------------------------------------------
+
+CASE( "test_createField" ) {
+    FunctionSpace p1;
+    {
+        Field points( "points", array::make_datatype<double>(), array::make_shape( 10, 2 ) );
+        auto xy = array::make_view<double, 2>( points );
+        xy.assign( {00., 0., 10., 0., 20., 0., 30., 0., 40., 0., 50., 0., 60., 0., 70., 0., 80., 0., 90., 0.} );
+        p1 = functionspace::PointCloud( points );
+    }
+
+    Field f1 = p1.createField<double>( option::name( "f1" ) | option::levels( 3 ) );
+    EXPECT_EQ( f1.levels(), 3 );
+    EXPECT_EQ( f1.shape( 0 ), 10 );
+    EXPECT_EQ( f1.shape( 1 ), 3 );
+
+    FunctionSpace p2;
+    {
+        Field points( "points", array::make_datatype<double>(), array::make_shape( 4, 2 ) );
+        auto xy = array::make_view<double, 2>( points );
+        xy.assign( {20., 0., 40., 0., 70., 0., 90., 0.} );
+        p2 = functionspace::PointCloud( points );
+    }
+    Field f2 = p2.createField( f1, util::NoConfig() );
+    EXPECT_EQ( f2.levels(), 3 );
+    EXPECT_EQ( f2.shape( 0 ), 4 );
+    EXPECT_EQ( f2.shape( 1 ), 3 );
+}
+
 
 //-----------------------------------------------------------------------------
 
