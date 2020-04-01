16aecaac21d0cf8bb54e071d7a5b1ddf5eb84b7d
Willem Deconinck <willem.deconinck@ecmwf.int>
2020-04-01 17:48:00 +0100

    ATLAS-270 Add Point2 comparators

--- src/atlas/util/Point.h
+++ src/atlas/util/Point.h
@@ -29,6 +29,13 @@ namespace atlas {
 using Point2 = eckit::geometry::Point2;
 using Point3 = eckit::geometry::Point3;
 
+inline bool operator==( const Point2& p1, const Point2& p2 ) {
+    return eckit::geometry::points_equal( p1, p2 );
+}
+inline bool operator!=( const Point2& p1, const Point2& p2 ) {
+    return !eckit::geometry::points_equal( p1, p2 );
+}
+
 /// @brief  Point in arbitrary XY-coordinate system
 class PointXY : public eckit::geometry::Point2 {
     using array_t = std::array<double, 2>;
