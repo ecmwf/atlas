7f53b428536d7a6be9da2e637c4bd9d71fea46d0
Willem Deconinck <willem.deconinck@ecmwf.int>
2020-04-14 23:04:27 +0100

    Fix find_package(LATEX) by removing REQUIRED keyword

--- cmake/features/DOCS.cmake
+++ cmake/features/DOCS.cmake
@@ -1,7 +1,7 @@
 ################################################################################
 # documentation
 if( ENABLE_DOCS )
-  find_package(LATEX REQUIRED COMPONENTS PDFLATEX BIBTEX OPTIONAL_COMPONENTS MAKEINDEX HTLATEX)
+  find_package(LATEX COMPONENTS PDFLATEX BIBTEX OPTIONAL_COMPONENTS MAKEINDEX HTLATEX)
 endif()
 ecbuild_add_option( FEATURE DOCS
                     DESCRIPTION "Atlas documentation"
