e8ef21493af1903a5653558fa1fa1620451ae4ca
MarekWlasak <marekandjenny@gmail.com>
2020-04-22 09:32:44 +0100

    add copyright

--- src/tests/parallel/test_haloexchange_adjoint.cc
+++ src/tests/parallel/test_haloexchange_adjoint.cc
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2013 ECMWF.
+ * (C) British Crown Cpyright 2020, Met Office
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
