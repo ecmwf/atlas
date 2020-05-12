7d4bffd7bfa927d329dba9388f5f4a28d08f7a25
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2020-05-12 20:53:02 +0000

    Add test to demonstrate that the creation of a 40000x20000 lat/lon grid with longitudes starting at -180 is cheap, both in terms of memory and time.

--- src/tests/grid/test_largegrid.cc
+++ src/tests/grid/test_largegrid.cc
@@ -1,26 +1,19 @@
 #include <algorithm>
 #include <vector>
 
-#include "atlas/array.h"
-#include "atlas/functionspace.h"
+#include <time.h>
+#include <sys/resource.h>
+
 #include "atlas/grid.h"
 #include "atlas/library/Library.h"
-#include "atlas/parallel/mpi/mpi.h"
 #include "atlas/util/Config.h"
-#include "atlas/util/UnitSphere.h"
-#include "eckit/types/FloatCompare.h"
 #include "tests/AtlasTestEnvironment.h"
-
-using namespace atlas::util;
-using namespace atlas::grid;
-using namespace atlas;
-
 
 namespace atlas {
 namespace test {
 CASE( "largeGrid" ) {
 
-
+  time_t t0 = time (NULL);
 
   atlas::StructuredGrid grid (atlas::util::Config ("nx", 40000) | atlas::util::Config ("ny", 20000) 
                             | atlas::util::Config ("type", "regular_lonlat") 
@@ -28,11 +21,12 @@ CASE( "largeGrid" ) {
                                                  | atlas::util::Config ("units", "degrees")
                                                  | atlas::util::Config ("west", -180.)));
  
-                       
-  auto lonlat = grid.lonlat (0, 0);
+  EXPECT ((time (NULL)) - t0 < 10);
 
-  printf (" %12.2f, %12.2f\n", lonlat.lon (), lonlat.lat ());
+  struct rusage usage; 
 
+  EXPECT (getrusage (RUSAGE_SELF, &usage) == 0);
+  EXPECT (usage.ru_maxrss < 30000); // 30Mb
 
 }
 }  // namespace test
