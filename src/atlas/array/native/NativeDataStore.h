a3497bb2025aab5c3265cda406d09818d12a1db9
Willem Deconinck <willem.deconinck@ecmwf.int>
2020-06-24 17:21:45 +0100

    ATLAS-302 Use posix_memalign for NativeDataStore

--- src/atlas/array/native/NativeDataStore.h
+++ src/atlas/array/native/NativeDataStore.h
@@ -10,11 +10,15 @@
 
 #pragma once
 
-
 #include <algorithm>  // std::fill
+#include <cstdlib>    // posix_memalign
 #include <limits>     // std::numeric_limits<T>::signaling_NaN
+#include <sstream>
+
 #include "atlas/array/ArrayUtil.h"
 #include "atlas/library/config.h"
+#include "atlas/runtime/Exception.h"
+#include "eckit/log/Bytes.h"
 
 //------------------------------------------------------------------------------
 
@@ -45,9 +49,12 @@ void initialise( Value[], size_t ) {}
 template <typename Value>
 class DataStore : public ArrayDataStore {
 public:
-    DataStore( size_t size ) : data_store_( new Value[size] ), size_( size ) { initialise( data_store_, size_ ); }
+    DataStore( size_t size ) : size_( size ) {
+        alloc_aligned( data_store_, size_ );
+        initialise( data_store_, size_ );
+    }
 
-    virtual ~DataStore() override { delete[] data_store_; }
+    virtual ~DataStore() override { free_aligned( data_store_ ); }
 
     virtual void updateDevice() const override {}
 
@@ -72,6 +79,26 @@ public:
     virtual void* voidDeviceData() override { return static_cast<void*>( data_store_ ); }
 
 private:
+    [[noreturn]] void throw_AllocationFailed( size_t bytes, const eckit::CodeLocation& loc ) {
+        std::ostringstream ss;
+        ss << "AllocationFailed: Could not allocate " << eckit::Bytes( bytes );
+        throw_Exception( ss.str(), loc );
+    }
+
+    void alloc_aligned( Value*& ptr, size_t n ) {
+        const size_t alignment = 64 * sizeof( Value );
+        size_t bytes           = sizeof( Value ) * n;
+        int err                = posix_memalign( (void**)&ptr, alignment, bytes );
+        if ( err ) {
+            throw_AllocationFailed( bytes, Here() );
+        }
+    }
+
+    void free_aligned( Value*& ptr ) {
+        free( ptr );
+        ptr = nullptr;
+    }
+
     Value* data_store_;
     size_t size_;
 };
