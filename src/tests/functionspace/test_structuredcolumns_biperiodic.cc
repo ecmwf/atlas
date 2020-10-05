3940c995ece0413c165025619a4fc4a4da27a4c6
Willem Deconinck <willem.deconinck@ecmwf.int>
2020-10-05 12:26:00 +0100

    Formatting

--- src/tests/functionspace/test_structuredcolumns_biperiodic.cc
+++ src/tests/functionspace/test_structuredcolumns_biperiodic.cc
@@ -37,75 +37,75 @@ namespace test {
 
 //-----------------------------------------------------------------------------
 
-CASE( "biperiodic_latlon") {
-
-  auto & comm = atlas::mpi::comm ();
-  const int myproc = comm.rank ();
-
-
-  using namespace atlas::util;
-  using namespace atlas;
-  using namespace atlas::grid;
-
-  const int Nx = 80, Ny = 80; 
-  const double xmin = +20, xmax = +60, ymin = +20, ymax = +60;
-
-  std::vector<Spacing> spacings (Ny);
-
-  for (int i = 0; i < Ny; i++)
-    spacings[i] = Spacing (Config ("type", "linear") | Config ("N", Nx) 
-                         | Config ("start", xmin) | Config ("end", xmax));
-
-  StructuredGrid::XSpace xspace (spacings);
-  StructuredGrid::YSpace yspace (Config ("type", "linear") | Config ("N", Ny) | Config ("start", ymin) | Config ("end", ymax));
-  Projection proj (Config ("type", "lonlat"));
-
-  atlas::StructuredGrid grid (xspace, yspace, proj, Domain ());
-
-  atlas::grid::Distribution dist (grid, atlas::util::Config ("type", "checkerboard"));
-  atlas::functionspace::StructuredColumns fs (grid, dist, atlas::util::Config ("halo", 3) 
-                                            | atlas::util::Config ("periodic_x", true)
-                                            | atlas::util::Config ("periodic_y", true));
+CASE( "biperiodic_latlon" ) {
+    auto& comm       = atlas::mpi::comm();
+    const int myproc = comm.rank();
 
 
-  auto f = atlas::Field ("f",
-                         atlas::array::DataType::kind<double> (),
-                         atlas::array::make_shape (fs.size ()));
+    using namespace atlas::util;
+    using namespace atlas;
+    using namespace atlas::grid;
 
-  auto v = atlas::array::make_view<double,1> (f);
+    const int Nx = 80, Ny = 80;
+    const double xmin = +20, xmax = +60, ymin = +20, ymax = +60;
 
-  for (int i = 0; i < f.size (); i++)
-    v (i) = static_cast<double> (myproc);
+    std::vector<Spacing> spacings( Ny );
 
-  fs.haloExchange (f);
- 
-  auto clamp = [] (int i, int n)
-  {
-    while (i < 0)
-      i += n;
-    while (i >= n)
-      i -= n;
-    return i;
-  };
- 
-  auto gv = atlas::array::make_view<atlas::gidx_t, 1>( fs.global_index() );
-  auto pv = atlas::array::make_view<atlas::idx_t, 1>( fs.partition() );
+    for ( int i = 0; i < Ny; i++ ) {
+        spacings[i] =
+            Spacing( Config( "type", "linear" ) | Config( "N", Nx ) | Config( "start", xmin ) | Config( "end", xmax ) );
+    }
+    StructuredGrid::XSpace xspace( spacings );
+    StructuredGrid::YSpace yspace( Config( "type", "linear" ) | Config( "N", Ny ) | Config( "start", ymin ) |
+                                   Config( "end", ymax ) );
+    Projection proj( Config( "type", "lonlat" ) );
 
-  for (int j = fs.j_begin_halo (); j < fs.j_end_halo (); j++)
-  for (int i = fs.i_begin_halo (j); i < fs.i_end_halo (j); i++)
-    {
-      int k = fs.index (i, j);
-      int jj = clamp (j, grid.ny ());
-      int ii = clamp (i, grid.nx (jj));
+    atlas::StructuredGrid grid( xspace, yspace, proj, Domain() );
 
-      int g = grid.index (ii, jj);
-      int p = dist.partition (g);
+    atlas::grid::Distribution dist( grid, atlas::util::Config( "type", "checkerboard" ) );
+    atlas::functionspace::StructuredColumns fs( grid, dist,
+                                                atlas::util::Config( "halo", 3 ) |
+                                                    atlas::util::Config( "periodic_x", true ) |
+                                                    atlas::util::Config( "periodic_y", true ) );
 
-      EXPECT_EQ (v (k), p);
-      EXPECT_EQ (p, pv (k));
-      EXPECT_EQ (gv (k)-1, g);
+
+    auto f = atlas::Field( "f", atlas::array::DataType::kind<double>(), atlas::array::make_shape( fs.size() ) );
+
+    auto v = atlas::array::make_view<double, 1>( f );
+
+    for ( int i = 0; i < f.size(); i++ ) {
+        v( i ) = static_cast<double>( myproc );
     }
 
+    fs.haloExchange( f );
+
+    auto clamp = []( int i, int n ) {
+        while ( i < 0 ) {
+            i += n;
+        }
+        while ( i >= n ) {
+            i -= n;
+        }
+        return i;
+    };
+
+    auto gv = atlas::array::make_view<atlas::gidx_t, 1>( fs.global_index() );
+    auto pv = atlas::array::make_view<atlas::idx_t, 1>( fs.partition() );
+
+    for ( int j = fs.j_begin_halo(); j < fs.j_end_halo(); j++ ) {
+        for ( int i = fs.i_begin_halo( j ); i < fs.i_end_halo( j ); i++ ) {
+            int k  = fs.index( i, j );
+            int jj = clamp( j, grid.ny() );
+            int ii = clamp( i, grid.nx( jj ) );
+
+            int g = grid.index( ii, jj );
+            int p = dist.partition( g );
+
+            EXPECT_EQ( v( k ), p );
+            EXPECT_EQ( p, pv( k ) );
+            EXPECT_EQ( gv( k ) - 1, g );
+        }
+    }
 }
 
 //-----------------------------------------------------------------------------
