8864627a6e59b8248bb70bbd0bbc9b109a79a07a
Willem Deconinck <willem.deconinck@ecmwf.int>
2020-11-07 00:48:18 +0000

    Linking with proposed new trans project containing transi

--- cmake/features/TRANS.cmake
+++ cmake/features/TRANS.cmake
@@ -1,6 +1,18 @@
 ### trans ...
 
-ecbuild_find_package( NAME transi VERSION 0.4.4 QUIET )
+if( ENABLE_TRANS OR NOT DEFINED ENABLE_TRANS )
+    find_package( trans 47.2 COMPONENTS transi double QUIET )
+    if( TARGET transi_dp )
+        set( transi_FOUND TRUE )
+        if( NOT TARGET transi )
+            set_target_properties( transi_dp PROPERTIES IMPORTED_GLOBAL TRUE) # required for aliasing imports
+            add_library( transi ALIAS transi_dp )
+        endif()
+    else()
+        find_package( transi 0.8 QUIET )
+    endif()
+endif()
+
 ecbuild_add_option( FEATURE TRANS
-                    DESCRIPTION "Support for spectral transforms"
+                    DESCRIPTION "Support for IFS spectral transforms"
                     CONDITION transi_FOUND )
