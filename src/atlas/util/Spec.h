848a271b9aa7969ed826c90468f254f3b8fccb47
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2021-02-05 17:57:50 +0000

    ATLAS-301 atlas::util::Spec, atlas::util::SpecRegistry

--- src/atlas/util/Spec.h
+++ src/atlas/util/Spec.h
@@ -31,31 +31,17 @@ using Spec = Config;
 template <typename T>
 struct SpecRegistry {
     struct Enregister {
-        Enregister( const std::string& id, const eckit::PathName& path ) { enregister( id, path ); }
         Enregister( const std::string& id, Spec&& spec ) { enregister( id, std::move( spec ) ); }
     };
 
-    static void enregister( const std::string& id, const eckit::PathName& path ) {
-        ATLAS_ASSERT_MSG( instance().m_.find( id ) == instance().m_.end(), duplicate( id ) );
-        ATLAS_ASSERT_MSG( instance().p_.emplace( id, path ).second, duplicate( id ) );
-    }
-
     static void enregister( const std::string& id, Spec&& spec ) {
-        ATLAS_ASSERT_MSG( instance().m_.emplace( id, spec ).second, duplicate( id ) );
+        ATLAS_ASSERT_MSG( instance().m_.emplace( id, std::move( spec ) ).second,
+                          "SpecFactory: duplicate '" + id + "'" );
     }
 
     static Spec lookup( const std::string& id ) {
-        auto& p = instance().p_;
-        auto i  = p.find( id );
-        if ( i != p.end() ) {
-            enregister( id, std::move( Spec( i->second ) ) );
-            p.erase( i );
-            return lookup( id );
-        }
-
-        auto& m = instance().m_;
-        auto j  = m.find( id );
-        if ( j != m.end() ) {
+        auto j = instance().m_.find( id );
+        if ( j != instance().m_.end() ) {
             return j->second;
         }
 
@@ -77,10 +63,7 @@ private:
         return factory;
     }
 
-    static std::string duplicate( const std::string& id ) { return "SpecFactory: duplicate '" + id + "'"; }
-
     std::map<std::string, const Spec> m_;
-    std::map<std::string, const eckit::PathName> p_;
 };
 
 }  // namespace util
