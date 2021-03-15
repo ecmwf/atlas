e652fa770d0cd130f7f293120c9ee3c7d5075c97
Oliver Lomax <oliver.lomax@metoffice.gov.uk>
2021-03-15 19:08:30 +0000

    Branch ready for pull request.

--- src/atlas/repartition/detail/StructuredColumnsToStructuredColumns.h
+++ src/atlas/repartition/detail/StructuredColumnsToStructuredColumns.h
@@ -11,7 +11,6 @@
 
 #include "atlas/functionspace/StructuredColumns.h"
 #include "atlas/repartition/detail/RepartitionImpl.h"
-#include "atlas/repartition/mpi/GraphComm.h"
 
 namespace atlas {
 
@@ -35,12 +34,10 @@ namespace atlas {
       class FuncSpaceRange;
 
       // Type aliases.
-      struct FuncSpaceRange;
+      class FuncSpaceRange;
       using idxPair = std::pair<idx_t, idx_t>;
       using idxPairVector = std::vector<idxPair>;
       using FuncSpaceRangeVector = std::vector<FuncSpaceRange>;
-      using BlockData = std::pair<std::vector<int>, std::vector<int>>;
-      using BlockDataVector = std::vector<BlockData>;
 
       using functionspace::detail::StructuredColumns;
 
@@ -48,9 +45,7 @@ namespace atlas {
       ///           StructuredColumns.
       ///
       /// \details  Class to map two function spaces with the same grid but
-      ///           different partitioners. Creates an MPI Graph communicator
-      ///           to perform an efficient, scalable Neighbor_Alltoallw
-      ///           communication.
+      ///           different partitioners.
       class StructuredColumnsToStructuredColumns : public RepartitionImpl {
 
       public:
@@ -58,10 +53,8 @@ namespace atlas {
         /// \brief    Constructs and initialises the repartitioner.
         ///
         /// \details  Performs MPI_Allgatherv to determine the (i, j, k) ranges
-        ///           of each source and target function space on each PE. Then
-        ///           calculates range intersections to determine what needs to
-        ///           be sent where via an MPI_Alltoallw. The grids of source
-        ///           and target function space must match.
+        ///           of each source and target function space on each PE.
+        ///           The grids of source and target function space must match.
         ///
         /// \param[in]  sourceFunctionSpace  Function space of source fields.
         /// \param[in]  targetFunctionSpace  Function space of target fields.
@@ -72,7 +65,7 @@ namespace atlas {
         /// \brief    Repartitions source field to target field.
         ///
         /// \details  Transfers source field to target field via an
-        ///           MPI_Neighbor_Alltoallw. Function space of source field
+        ///           MPI_Neighbor_Alltoallv. Function space of source field
         ///           must match sourceFunctionSpace supplied to the
         ///           constructor.Same applies to target field.
         ///
@@ -101,47 +94,43 @@ namespace atlas {
         const StructuredColumns* sourceStructuredColumnsPtr_{};
         const StructuredColumns* targetStructuredColumnsPtr_{};
 
-        // MPI distributed graph communicator.
-        mpi::GraphComm graphComm_;
+        // Vectors of index range intersection objects.
+        FuncSpaceRangeVector sendIntersections_{};
+        FuncSpaceRangeVector recvIntersections_{};
 
-        // Block lenghts and displacements for MPI indexed datatype.
-        BlockDataVector sendBlockDataVector_{};
-        BlockDataVector recvBlockDataVector_{};
+        // Counts and displacements for MPI communications.
+        std::vector<int> sendCounts_{};
+        std::vector<int> sendDisplacements_{};
+        std::vector<int> recvCounts_{};
+        std::vector<int> recvDisplacements_{};
 
       };
 
-      // Helper class for function space intersections.
+      /// \brief    Helper class for function space intersections.
       class FuncSpaceRange {
 
       public:
 
-        // Default Constructor.
+        /// \brief    Default Constructor.
         FuncSpaceRange() = default;
 
-        // Constructor.
+        /// \brief    Constructor.
         FuncSpaceRange(const StructuredColumns* const structuredColumnsPtr);
 
-        // Get index ranges from all PEs.
+        /// \brief    Get index ranges from all PEs.
         FuncSpaceRangeVector getFuncSpaceRanges() const;
 
-        // Count number of elements.
+        /// \brief    Count number of elements.
         idx_t getElemCount() const;
 
-        // Intersection operator.
+        /// \brief    Intersection operator.
         FuncSpaceRange operator&(const FuncSpaceRange& indexRange) const;
 
-        // Get block lengths and displacements for index range.
-        BlockData
-          getBlockData(
-          const StructuredColumns* const structuredColumnsPtr) const;
-
-        // Return copy of rank.
-        int getRank() const {return rank_;}
+        /// \brief    Iterate over all indices and do something with functor.
+        template <typename functorType>
+        void forEach(functorType& functor) const;
 
       private:
-
-        // MPI rank of range.
-        int rank_{};
 
         // Begin and end of j range.
         idxPair jBeginEnd_{};
