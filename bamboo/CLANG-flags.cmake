b6c913c51989a664ed8c1ab80220a92cbcc928c9
Willem Deconinck <willem.deconinck@ecmwf.int>
2021-06-19 10:44:26 +0100

    Disable OpenMP for C++ for clang bamboo build

--- bamboo/CLANG-flags.cmake
+++ bamboo/CLANG-flags.cmake
@@ -1,3 +1,4 @@
 set( ENABLE_TRANS               ON  CACHE BOOL "Enable TRANS" )
 set( ENABLE_BOUNDSCHECKING      ON  CACHE BOOL "Enable bounds checking")
 set( ENABLE_TESSELATION         OFF CACHE BOOL "Disable CGAL" ) # cgal is old in leap42
+set( ENABLE_OMP_CXX             OFF CACHE BOOL "Disable OpenMP for C++" ) # because of problems with clang OpenMP
