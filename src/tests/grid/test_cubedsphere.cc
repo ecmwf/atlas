9d5e2ca3e0cd34940d4245cd9d9b43e4cc844386
svahl991 <vahl@ucar.edu>
2021-08-09 22:50:03 +0000

    Add cubed sphere cell-centers grid

--- src/tests/grid/test_cubedsphere.cc
+++ src/tests/grid/test_cubedsphere.cc
@@ -115,7 +115,7 @@ CASE( "cubedsphere_grid_mesh_field_test" ) {
     // I expect this will be replaced by some more aggressive tests.
 
     // Set grid.
-    const auto grid = atlas::Grid( "CS-EA-2" );
+    const auto grid = atlas::Grid( "CS-EA-L-2" );
 
     atlas::Log::info() << grid->type() << std::endl;
     atlas::Log::info() << grid.size() << std::endl;
