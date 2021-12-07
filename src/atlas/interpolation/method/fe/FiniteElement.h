175032ee9d38144779d6df904ae130eeee329426
twsearle <14909402+twsearle@users.noreply.github.com>
2021-12-07 09:09:30 +0000

    fix: max_fraction_elems_to_try name.

--- src/atlas/interpolation/method/fe/FiniteElement.h
+++ src/atlas/interpolation/method/fe/FiniteElement.h
@@ -29,7 +29,7 @@ namespace method {
 class FiniteElement : public Method {
 public:
     FiniteElement(const Config& config): Method(config) {
-        if (config.has("maxFractionElemsToTry") ) { config.get("max_fraction_elems_to_try", maxFractionElemsToTry_); }
+        if (config.has("max_fraction_elems_to_try") ) { config.get("max_fraction_elems_to_try", maxFractionElemsToTry_); }
     }
 
     virtual ~FiniteElement() override {}
