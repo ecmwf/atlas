0a9e45addf12143363555f88bf8ab36b3ffc6592
Willem Deconinck <willem.deconinck@ecmwf.int>
2022-04-04 15:56:40 +0100

    Use util::Factory for MultiFieldCreatorFactory

--- src/atlas/field/MultiField.cc
+++ src/atlas/field/MultiField.cc
@@ -8,8 +8,6 @@
  * nor does it submit to any jurisdiction.
  */
 
-// file deepcode ignore CppMemoryLeak: static pointers for global registry are OK and will be cleaned up at end
-
 #include "MultiField.h"
 
 #include <iomanip>
@@ -30,31 +28,20 @@ namespace atlas {
 namespace field {
 
 namespace {
-
-static eckit::Mutex* local_mutex                           = nullptr;
-static std::map<std::string, MultiFieldCreatorFactory*>* m = nullptr;
-static pthread_once_t once                                 = PTHREAD_ONCE_INIT;
-
-static void init() {
-    local_mutex = new eckit::Mutex();
-    m           = new std::map<std::string, MultiFieldCreatorFactory*>();
-}
-
-template <typename T>
-void load_builder() {
-    MultiFieldCreatorBuilder<T>("tmp");
-}
-
 void force_link() {
-    static struct Link { Link() = default; } link;
-    [](const Link&) {}(link);  // disable unused warnings
+    static struct Link {
+        Link() {
+            ;
+            // For static linking add here something like
+            // MultiFieldCreatorBuilder<T>();
+        }
+    } link;
 }
-
 }  // namespace
 
-void MultiField::initialize(const std::string& generator, const eckit::Parametrisation& params) {
-    std::unique_ptr<MultiFieldCreator> FieldPool_generator(MultiFieldCreatorFactory::build(generator, params));
-    FieldPool_generator->generate(*this, params);
+void MultiField::initialize(const std::string& creator, const eckit::Parametrisation& params) {
+    std::unique_ptr<MultiFieldCreator> MultiField_creator(MultiFieldCreatorFactory::build(creator, params));
+    MultiField_creator->generate(*this, params);
 }
 
 array::Array& MultiField::allocate(array::DataType datatype, array::ArraySpec&& spec) {
@@ -66,8 +53,8 @@ array::Array& MultiField::allocate(array::DataType datatype, array::ArraySpec&& 
 
 MultiField::MultiField() = default;
 
-MultiField::MultiField(const std::string& generator, const eckit::Parametrisation& params) {
-    initialize(generator, params);
+MultiField::MultiField(const std::string& creator, const eckit::Parametrisation& params) {
+    initialize(creator, params);
 }
 
 const util::Metadata& MultiField::metadata() const {
@@ -96,111 +83,12 @@ MultiFieldCreator::MultiFieldCreator(const eckit::Parametrisation&) {}
 
 MultiFieldCreator::~MultiFieldCreator() = default;
 
-MultiFieldCreator* MultiFieldCreatorFactory::build(const std::string& name, const eckit::Parametrisation& param) {
-    pthread_once(&once, init);
-
-    eckit::AutoLock<eckit::Mutex> lock(local_mutex);
-
+MultiFieldCreator* MultiFieldCreatorFactory::build(const std::string& builder, const eckit::Parametrisation& config) {
     force_link();
-
-    std::map<std::string, MultiFieldCreatorFactory*>::const_iterator j = m->find(name);
-
-    Log::debug() << "Looking for MultiFieldCreatorFactory [" << name << "]" << std::endl;
-
-    if (j == m->end()) {
-        Log::error() << "No MultiFieldCreatorFactory for [" << name << "]" << std::endl;
-        Log::error() << "FieldPoolFactories are:" << std::endl;
-        for (j = m->begin(); j != m->end(); ++j) {
-            Log::error() << "   " << (*j).first << std::endl;
-        }
-        throw_Exception(std::string("No MultiFieldCreatorFactory called ") + name, Here());
-    }
-
-    return (*j).second->make(param);
+    auto factory = get(builder);
+    return factory->make(config);
 }
 
-void MultiFieldCreatorFactory::list(std::ostream& out) {
-    pthread_once(&once, init);
-
-    eckit::AutoLock<eckit::Mutex> lock(local_mutex);
-
-    force_link();
-
-    const char* sep = "";
-    for (std::map<std::string, MultiFieldCreatorFactory*>::const_iterator j = m->begin(); j != m->end(); ++j) {
-        out << sep << (*j).first;
-        sep = ", ";
-    }
-}
-
-bool MultiFieldCreatorFactory::has(const std::string& name) {
-    pthread_once(&once, init);
-
-    eckit::AutoLock<eckit::Mutex> lock(local_mutex);
-
-    force_link();
-
-    return (m->find(name) != m->end());
-}
-
-MultiFieldCreatorFactory::MultiFieldCreatorFactory(const std::string& name): name_(name) {
-    pthread_once(&once, init);
-
-    eckit::AutoLock<eckit::Mutex> lock(local_mutex);
-
-    ATLAS_ASSERT(m->find(name) == m->end());
-    (*m)[name] = this;
-}
-
-MultiFieldCreatorFactory::~MultiFieldCreatorFactory() {
-    eckit::AutoLock<eckit::Mutex> lock(local_mutex);
-    m->erase(name_);
-}
-
-//-----------------------------------------------------------------------------
-// C wrapper interfaces to C++ routines
-
-extern "C" {
-
-MultiField* atlas__FieldPool__new() {
-    return new MultiField;
-}
-
-void atlas__FieldPool__initialize(MultiField* This, const char* generator, const eckit::Parametrisation* params) {
-    ATLAS_ASSERT(This != nullptr, "Reason: Use of uninitialised atlas_FieldPool");
-    This->initialize(std::string(generator), *params);
-}
-
-void atlas__FieldPool__delete(MultiField* This) {
-    ATLAS_ASSERT(This != nullptr, "Reason: Use of uninitialised atlas_FieldPool");
-    delete This;
-}
-
-int atlas__FieldPool__has(MultiField* This, const char* name) {
-    ATLAS_ASSERT(This != nullptr, "Reason: Use of uninitialised atlas_FieldPool");
-    return This->has(name);
-}
-
-FieldImpl* atlas__FieldPool__field_by_name(MultiField* This, const char* name) {
-    ATLAS_ASSERT(This != nullptr, "Reason: Use of uninitialised atlas_FieldPool");
-    return This->field(std::string(name)).get();
-}
-
-FieldImpl* atlas__FieldPool__field_by_index(MultiField* This, idx_t index) {
-    ATLAS_ASSERT(This != nullptr, "Reason: Use of uninitialised atlas_FieldPool");
-    return This->field(index).get();
-}
-
-idx_t atlas__FieldPool__size(const MultiField* This) {
-    ATLAS_ASSERT(This != nullptr, "Reason: Use of uninitialised atlas_FieldPool");
-    return This->size();
-}
-
-util::Metadata* atlas__FieldPool__metadata(MultiField* This) {
-    ATLAS_ASSERT(This != nullptr, "Reason: Use of uninitialised atlas_FieldPool");
-    return &This->metadata();
-}
-}
 //-----------------------------------------------------------------------------
 
 }  // namespace field
