591f5b6631eb434c3bc8fc896274ed3fd9ceeb93
Willem Deconinck <willem.deconinck@ecmwf.int>
2022-04-19 12:51:52 +0200

    ATLAS-354 Compatibility with proj >= 8
    From proj 8 onwards the forward declared Proj types are no longer backwards compatible.
    The backwards compatible solution is to not use the forward declarations at all.

--- src/atlas/projection/detail/ProjProjection.h
+++ src/atlas/projection/detail/ProjProjection.h
@@ -16,36 +16,16 @@
 #include "atlas/projection/detail/ProjectionImpl.h"
 #include "atlas/util/Config.h"
 
-
-extern "C" {
-using PJ = struct PJconsts;
-PJ* proj_destroy(PJ*);
-using PJ_CONTEXT = struct projCtx_t;
-PJ_CONTEXT* proj_context_destroy(PJ_CONTEXT*);
-}
-
-
 namespace atlas {
 namespace projection {
 namespace detail {
 
+// Forward declaration of Proj, defined in ProjProjection.cc
+struct Proj;
 
 class ProjProjection final : public ProjectionImpl {
 public:
-    // -- Types
-    // None
-
-    // -- Exceptions
-    // None
-
-    // Constructors
-
     ProjProjection(const eckit::Parametrisation&);
-
-    // Destructor
-    // None
-
-    // -- Methods
 
     static std::string static_type() { return "proj"; }
 
@@ -67,98 +47,17 @@ public:
     std::string units() const override;
     void hash(eckit::Hash&) const override;
 
-    // -- Class members
-    // None
-
-    // -- Class methods
-    // None
-
-    // -- Members
-    // None
-
-    // -- Methods
-    // None
-
-    // -- Overridden methods
-    // None
-
-    // -- Class members
-    // None
-
-    // -- Class methods
-    // None
-
-    // -- Friends
-    // None
-
-public:
-    // -- Types
-
-    struct pj_t : std::unique_ptr<PJ, decltype(&proj_destroy)> {
-        using t = std::unique_ptr<PJ, decltype(&proj_destroy)>;
-        explicit pj_t(PJ* ptr): t(ptr, &proj_destroy) {}
-        operator PJ*() const { return t::get(); }
-    };
-
-    struct ctx_t : std::unique_ptr<PJ_CONTEXT, decltype(&proj_context_destroy)> {
-        using t = std::unique_ptr<PJ_CONTEXT, decltype(&proj_context_destroy)>;
-        explicit ctx_t(PJ_CONTEXT* ptr): t(ptr, &proj_context_destroy) {}
-        operator PJ_CONTEXT*() const { return t::get(); }
-    };
-
-    // -- Contructors
-    // None
-
-    // -- Destructor
-    // None
-
-    // -- Convertors
-    // None
-
-    // -- Operators
-    // None
-
-    // -- Methods
-    // None
-
-    // -- Overridden methods
-    // None
-
-    // -- Class members
-    // None
-
-    // -- Class methods
-    // None
-
-    // -- Members
 private:
     Normalise normalise_;
-    std::string proj_;
+    std::string proj_string_;
     std::string source_;
     std::string geocentric_;
     bool source_encoded_;
     bool geocentric_encoded_;
 
-    pj_t sourceToTarget_;
-    pj_t sourceToGeocentric_;
-    ctx_t context_;
+    std::unique_ptr<Proj> proj_;
 
     Spec extraSpec_;
-
-    // -- Methods
-    // None
-
-    // -- Overridden methods
-    // None
-
-    // -- Class members
-    // None
-
-    // -- Class methods
-    // None
-
-    // -- Friends
-    // None (how sad)
 };
 
 
