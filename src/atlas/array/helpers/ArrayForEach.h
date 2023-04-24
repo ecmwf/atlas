f7a8f7721dd9c85c475486ed2b40c993a5d493ed
odlomax <oliver.lomax@metoffice.gov.uk>
2023-04-24 18:51:30 +0100

    Ready for PR.

---
+++ src/atlas/array/helpers/ArrayForEach.h
@@ -0,0 +1,209 @@
+/*
+ * (C) Crown Copyright 2023 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#pragma once
+
+#include <array>
+#include <tuple>
+
+#include "atlas/array/ArrayView.h"
+#include "atlas/array/Range.h"
+#include "atlas/array/helpers/ArraySlicer.h"
+#include "atlas/parallel/omp/omp.h"
+#include "atlas/util/Config.h"
+
+#include "eckit/exception/Exceptions.h"
+
+namespace atlas {
+namespace array {
+namespace helpers {
+
+namespace detail {
+
+enum class ExecutionPolicy {
+  parallel,
+  serial
+};
+
+template <typename... Ts, typename T>
+constexpr auto tuplePushBack(const std::tuple<Ts...>& tuple, T value) {
+  return std::tuple_cat(tuple, std::make_tuple(value));
+}
+
+template <ExecutionPolicy Policy, typename Functor>
+void forEach(idx_t idxMax, const Functor& functor) {
+
+  if constexpr(Policy == ExecutionPolicy::parallel) {
+      atlas_omp_parallel_for(auto idx = idx_t{}; idx < idxMax; ++idx) {
+        functor(idx);
+      }
+    }
+  else {
+    for (auto idx = idx_t{}; idx < idxMax; ++idx) {
+      functor(idx);
+    }
+  }
+}
+
+template <size_t ViewIdx = 0, typename... ArrayViews, typename... SlicerArgs>
+auto makeSlices(std::tuple<ArrayViews...>& arrayViews,
+                const std::tuple<SlicerArgs...>& slicerArgs) {
+
+  // Loop over all views in tuple and make slices.
+  if constexpr(ViewIdx < std::tuple_size_v<std::tuple<ArrayViews...>>) {
+
+      auto& view = std::get<ViewIdx>(arrayViews);
+
+      // "Lambdafy" slicer apply method to work with std::apply.
+      const auto slicer = [&view](const auto&... args) {
+        return view.slice(args...);
+      };
+
+      // Fill out the remaining slicerArgs with Range::all().
+      constexpr auto dim = std::tuple_size_v<std::tuple<SlicerArgs...>>;
+      constexpr auto rank = view.rank();
+      auto argPadding = std::array<decltype(Range::all()), rank - dim>();
+      argPadding.fill(Range::all());
+      const auto paddedArgs = std::tuple_cat(slicerArgs, argPadding);
+
+      // Recurse until all views are sliced.
+      return std::tuple_cat(std::make_tuple(std::apply(slicer, paddedArgs)),
+                            makeSlices<ViewIdx + 1>(arrayViews, slicerArgs));
+    }
+  else {
+
+    // No more ArrayViews. Finish recursion.
+    return std::make_tuple();
+  }
+}
+
+template <ExecutionPolicy Policy, int Dim, int... ItrDims>
+struct ArrayForEachImpl;
+
+template <ExecutionPolicy Policy, int Dim, int ItrDim, int... ItrDims>
+struct ArrayForEachImpl<Policy, Dim, ItrDim, ItrDims...> {
+  template <typename... ArrayViews, typename LoopFunctor, typename GhostFunctor,
+            typename... SlicerArgs, typename... GhostArgs>
+  static void apply(std::tuple<ArrayViews...>& arrayViews,
+                    const LoopFunctor& loopFunctor,
+                    const GhostFunctor& ghostFunctor,
+                    const std::tuple<SlicerArgs...>& slicerArgs,
+                    const std::tuple<GhostArgs...>& ghostArgs) {
+
+    using namespace detail;
+
+    // Iterate over this dimension.
+    if constexpr(Dim == ItrDim) {
+
+        // Get size of iteration dimenion from first view argument.
+        const auto idxMax = std::get<0>(arrayViews).shape(ItrDim);
+
+        forEach<Policy>(idxMax, [&](idx_t idx) {
+
+          // Always set Policy to serial after one loop.
+          ArrayForEachImpl<ExecutionPolicy::serial, Dim + 1,
+                           ItrDims...>::apply(arrayViews, loopFunctor,
+                                              ghostFunctor,
+                                              tuplePushBack(slicerArgs, idx),
+                                              tuplePushBack(ghostArgs, idx));
+        });
+      }
+
+    // Add a RangeAll to arguments.
+    else {
+      ArrayForEachImpl<Policy, Dim + 1, ItrDim, ItrDims...>::apply(
+          arrayViews, loopFunctor, ghostFunctor,
+          tuplePushBack(slicerArgs, Range::all()), ghostArgs);
+    }
+  }
+};
+
+template <ExecutionPolicy Policy, int Dim>
+struct ArrayForEachImpl<Policy, Dim> {
+  template <typename... ArrayViews, typename LoopFunctor, typename GhostFunctor,
+            typename... SlicerArgs, typename... GhostArgs>
+  static void apply(std::tuple<ArrayViews...>& arrayViews,
+                    const LoopFunctor& loopFunctor,
+                    const GhostFunctor& ghostFunctor,
+                    const std::tuple<SlicerArgs...>& slicerArgs,
+                    const std::tuple<GhostArgs...>& ghostArgs) {
+
+    // Skip iteration if ghostFunctor evaluates to true.
+    if (std::apply(ghostFunctor, ghostArgs)) {
+      return;
+    }
+
+    auto slices = makeSlices(arrayViews, slicerArgs);
+    std::apply(loopFunctor, slices);
+  }
+};
+}  // namespace detail
+
+
+/// brief  Array "For-Each" helper struct.
+///
+/// detail Iterates over dimensions given in  ItrDims. Slices over full range
+/// of other dimensions.
+/// Note: ItrDims must be given in ascending numerical order. TODO: Static
+/// checking for this.
+template <int... ItrDims>
+struct ArrayForEach {
+  /// brief   Apply "For-Each" method.
+  ///
+  /// detials Vists all elements indexed by ItrDims and creates a slice from
+  ///         each ArrayView in arrayViews. Slices are sent to loopFunctor
+  ///         which is executed with the signature f(slice1, slice2,...).
+  ///         Iterations are skipped when ghostFunctor evaluates to "true"
+  ///         and is executed with signature g(idx_i, idx_j,...), where the idxs
+  ///         are indices of ItrDims.
+  ///         When the config contains "execution policy" = "parallel" (default)
+  ///         the first loop is executed using OpenMP. The remaining loops are
+  ///         executed in serial. When "execution policy" = "serial", all loops
+  ///         are executed in serial.
+  ///         Note: The lowest ArrayView.rank() must be greater than or equal
+  ///         to the highest dim in ItrDims. TODO: static checking for this.
+  template <typename... ArrayViews, typename LoopFunctor, typename GhostFunctor>
+  static void apply(const std::tuple<ArrayViews...>& arrayViews,
+                    const LoopFunctor& loopFunctor,
+                    const GhostFunctor& ghostFunctor,
+                    const util::Config& conf = util::Config()) {
+
+    using namespace detail;
+
+    // Make a copy of views to simplify constness and forwarding.
+    auto arrayViewsCopy = arrayViews;
+
+    const auto executionPolicy = conf.getString("execution policy", "parallel");
+
+    if (executionPolicy == "parallel") {
+      ArrayForEachImpl<ExecutionPolicy::parallel, 0, ItrDims...>::apply(
+          arrayViewsCopy, loopFunctor, ghostFunctor, std::make_tuple(),
+          std::make_tuple());
+    } else if (executionPolicy == "serial") {
+      ArrayForEachImpl<ExecutionPolicy::serial, 0, ItrDims...>::apply(
+          arrayViewsCopy, loopFunctor, ghostFunctor, std::make_tuple(),
+          std::make_tuple());
+    } else {
+      throw eckit::BadParameter("Unknown execution policy: " + executionPolicy,
+                                Here());
+    }
+  }
+
+  /// brief   Apply "For-Each" method.
+  ///
+  /// details Apply "For-Each" without a ghostFunctor.
+  template <typename... ArrayViews, typename LoopFunctor>
+  static void apply(const std::tuple<ArrayViews...>& arrayViews,
+                    const LoopFunctor& loopFunctor,
+                    const util::Config& conf = util::Config()) {
+    apply(arrayViews, loopFunctor, [](auto args...) { return 0; }, conf);
+  }
+};
+
+}  // namespace helpers
+}  // namespace array
+}  // namespace atlas
---
+++ src/tests/array/test_array_foreach.cc
@@ -0,0 +1,263 @@
+/*
+ * (C) Crown Copyright 2023 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include <type_traits>
+
+#include "atlas/array.h"
+#include "atlas/array/MakeView.h"
+#include "atlas/array/helpers/ArrayForEach.h"
+#include "atlas/array/helpers/ArraySlicer.h"
+#include "atlas/util/Config.h"
+
+#include "tests/AtlasTestEnvironment.h"
+
+using namespace atlas::array;
+using namespace atlas::array::helpers;
+
+namespace atlas {
+namespace test {
+
+auto serialConf() { return util::Config("execution policy", "serial"); }
+
+CASE("test_array_foreach_1_view") {
+
+  const auto arr = ArrayT<double>(2, 3);
+  const auto view = make_view<double, 2>(arr);
+
+  // Test slice shapes.
+
+  const auto loopFunctorDim0 = [](auto& slice) {
+    EXPECT_EQUAL(slice.rank(), 1);
+    EXPECT_EQUAL(slice.shape(0), 3);
+  };
+  ArrayForEach<0>::apply(std::make_tuple(view), loopFunctorDim0);
+
+  const auto loopFunctorDim1 = [](auto& slice) {
+    EXPECT_EQUAL(slice.rank(), 1);
+    EXPECT_EQUAL(slice.shape(0), 2);
+  };
+  ArrayForEach<1>::apply(std::make_tuple(view), loopFunctorDim1);
+
+  // Test that slice resolves to double.
+
+  const auto loopFunctorDimAll = [](auto& slice) {
+    static_assert(std::is_convertible_v<decltype(slice), const double&>);
+  };
+  ArrayForEach<0, 1>::apply(std::make_tuple(view), loopFunctorDimAll);
+
+  // Test ghost functionality.
+
+  auto ghost = ArrayT<int>(2);
+  auto ghostView = make_view<int, 1>(ghost);
+  ghostView.assign({0, 1});
+
+  auto count = int {};
+  const auto countGhosts = [&count](auto&...) { ++count; };
+  ArrayForEach<0>::apply(std::make_tuple(view), countGhosts, ghostView,
+                         serialConf());
+  EXPECT_EQ(count, 1);
+
+  count = 0;
+  const auto ghostWrap = [&ghostView](idx_t idx, auto&...) {
+    // Wrap ghostView to use correct number of indices.
+    return ghostView(idx);
+  };
+  ArrayForEach<0, 1>::apply(std::make_tuple(view), countGhosts, ghostWrap,
+                            serialConf());
+  EXPECT_EQ(count, 3);
+}
+
+CASE("test_array_foreach_2_views") {
+
+  const auto arr1 = ArrayT<double>(2, 3);
+  const auto view1 = make_view<double, 2>(arr1);
+
+  const auto arr2 = ArrayT<double>(2, 3, 4);
+  const auto view2 = make_view<double, 3>(arr2);
+
+  // Test slice shapes.
+
+  const auto loopFunctorDim0 = [](auto& slice1, auto& slice2) {
+    EXPECT_EQUAL(slice1.rank(), 1);
+    EXPECT_EQUAL(slice1.shape(0), 3);
+
+    EXPECT_EQUAL(slice2.rank(), 2);
+    EXPECT_EQUAL(slice2.shape(0), 3);
+    EXPECT_EQUAL(slice2.shape(1), 4);
+  };
+  ArrayForEach<0>::apply(std::make_tuple(view1, view2), loopFunctorDim0);
+
+  const auto loopFunctorDim1 = [](auto& slice1, auto& slice2) {
+    EXPECT_EQUAL(slice1.rank(), 1);
+    EXPECT_EQUAL(slice1.shape(0), 2);
+
+    EXPECT_EQUAL(slice2.rank(), 2);
+    EXPECT_EQUAL(slice2.shape(0), 2);
+    EXPECT_EQUAL(slice2.shape(1), 4);
+  };
+  ArrayForEach<1>::apply(std::make_tuple(view1, view2), loopFunctorDim1);
+
+  // Test that slice resolves to double.
+
+  const auto loopFunctorDimAll = [](auto& slice2) {
+    static_assert(std::is_convertible_v<decltype(slice2), const double&>);
+  };
+  ArrayForEach<0, 1, 2>::apply(std::make_tuple(view2), loopFunctorDimAll);
+
+  // Test ghost functionality.
+
+  auto ghost = ArrayT<int>(2);
+  auto ghostView = make_view<int, 1>(ghost);
+  ghostView.assign({0, 1});
+
+  auto count = int {};
+  const auto countGhosts = [&count](auto&...) { ++count; };
+  ArrayForEach<0>::apply(std::make_tuple(view2), countGhosts, ghostView,
+                         serialConf());
+  EXPECT_EQ(count, 1);
+
+  count = 0;
+  const auto ghostWrap = [&ghostView](idx_t idx, auto&...) {
+    // Wrap ghostView to use correct number of indices.
+    return ghostView(idx);
+  };
+  ArrayForEach<0, 1>::apply(std::make_tuple(view2), countGhosts, ghostWrap,
+                            serialConf());
+  EXPECT_EQ(count, 3);
+
+  count = 0;
+  ArrayForEach<0, 1, 2>::apply(std::make_tuple(view2), countGhosts, ghostWrap,
+                               serialConf());
+  EXPECT_EQ(count, 12);
+}
+
+CASE("test_array_foreach_3_views") {
+
+  const auto arr1 = ArrayT<double>(2, 3);
+  const auto view1 = make_view<double, 2>(arr1);
+
+  const auto arr2 = ArrayT<double>(2, 3, 4);
+  const auto view2 = make_view<double, 3>(arr2);
+
+  const auto arr3 = ArrayT<double>(2, 3, 4, 5);
+  const auto view3 = make_view<double, 4>(arr3);
+
+  // Test slice shapes.
+
+  const auto loopFunctorDim0 = [](auto& slice1, auto& slice2, auto& slice3) {
+    EXPECT_EQUAL(slice1.rank(), 1);
+    EXPECT_EQUAL(slice1.shape(0), 3);
+
+    EXPECT_EQUAL(slice2.rank(), 2);
+    EXPECT_EQUAL(slice2.shape(0), 3);
+    EXPECT_EQUAL(slice2.shape(1), 4);
+
+    EXPECT_EQUAL(slice3.rank(), 3);
+    EXPECT_EQUAL(slice3.shape(0), 3);
+    EXPECT_EQUAL(slice3.shape(1), 4);
+    EXPECT_EQUAL(slice3.shape(2), 5);
+  };
+  ArrayForEach<0>::apply(std::make_tuple(view1, view2, view3), loopFunctorDim0);
+
+  const auto loopFunctorDim1 = [](auto& slice1, auto& slice2, auto& slice3) {
+    EXPECT_EQUAL(slice1.rank(), 1);
+    EXPECT_EQUAL(slice1.shape(0), 2);
+
+    EXPECT_EQUAL(slice2.rank(), 2);
+    EXPECT_EQUAL(slice2.shape(0), 2);
+    EXPECT_EQUAL(slice2.shape(1), 4);
+
+    EXPECT_EQUAL(slice3.rank(), 3);
+    EXPECT_EQUAL(slice3.shape(0), 2);
+    EXPECT_EQUAL(slice3.shape(1), 4);
+    EXPECT_EQUAL(slice3.shape(2), 5);
+  };
+  ArrayForEach<1>::apply(std::make_tuple(view1, view2, view3), loopFunctorDim1);
+
+  // Test that slice resolves to double.
+
+  const auto loopFunctorDimAll = [](auto& slice3) {
+    static_assert(std::is_convertible_v<decltype(slice3), const double&>);
+  };
+  ArrayForEach<0, 1, 2, 3>::apply(std::make_tuple(view3), loopFunctorDimAll);
+
+  // Test ghost functionality.
+
+  auto ghost = ArrayT<int>(2);
+  auto ghostView = make_view<int, 1>(ghost);
+  ghostView.assign({0, 1});
+
+  auto count = int {};
+  const auto countGhosts = [&count](auto&...) { ++count; };
+  ArrayForEach<0>::apply(std::make_tuple(view3), countGhosts, ghostView,
+                         serialConf());
+  EXPECT_EQ(count, 1);
+
+  count = 0;
+  const auto ghostWrap = [&ghostView](idx_t idx, auto&...) {
+    // Wrap ghostView to use correct number of indices.
+    return ghostView(idx);
+  };
+  ArrayForEach<0, 1>::apply(std::make_tuple(view3), countGhosts, ghostWrap,
+                            serialConf());
+  EXPECT_EQ(count, 3);
+
+  count = 0;
+  ArrayForEach<0, 1, 2>::apply(std::make_tuple(view3), countGhosts, ghostWrap,
+                               serialConf());
+  EXPECT_EQ(count, 12);
+
+  count = 0;
+  ArrayForEach<0, 1, 2, 3>::apply(std::make_tuple(view3), countGhosts,
+                                  ghostWrap, serialConf());
+  EXPECT_EQ(count, 60);
+}
+
+CASE("test_array_foreach_data_integrity") {
+
+  auto arr1 = ArrayT<double>(200, 3);
+  auto view1 = make_view<double, 2>(arr1);
+
+  auto arr2 = ArrayT<double>(200, 3, 4);
+  auto view2 = make_view<double, 3>(arr2);
+
+  for (auto idx = size_t{}; idx < arr1.size(); ++idx) {
+    static_cast<double*>(arr1.data())[idx] = idx;
+  }
+
+  for (auto idx = size_t{}; idx < arr2.size(); ++idx) {
+    static_cast<double*>(arr2.data())[idx] = idx;
+  }
+
+  const auto scaleDataDim0 = [](auto& slice1, auto& slice2) {
+
+    static_assert(std::is_convertible_v<decltype(slice1), double&>);
+    slice1 *= 2.;
+
+    const auto scaleDataDim1 = [](auto& slice) {
+
+      static_assert(std::is_convertible_v<decltype(slice), double&>);
+      slice *= 3.;
+    };
+    ArrayForEach<0>::apply(std::make_tuple(slice2), scaleDataDim1,
+                           serialConf());
+  };
+  ArrayForEach<0, 1>::apply(std::make_tuple(view1, view2), scaleDataDim0);
+
+  for (auto idx = size_t{}; idx < arr1.size(); ++idx) {
+    EXPECT_EQ(static_cast<double*>(arr1.data())[idx], 2. * idx);
+  }
+
+  for (auto idx = size_t{}; idx < arr2.size(); ++idx) {
+    EXPECT_EQ(static_cast<double*>(arr2.data())[idx], 3. * idx);
+  }
+}
+
+}  // namespace test
+}  // namespace atlas
+
+int main(int argc, char** argv) { return atlas::test::run(argc, argv); }
