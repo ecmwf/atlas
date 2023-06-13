920be8010b543ed89bb713b27022caff03a474b5
Slavko Brdar <slavko.brdar@ecmwf.int>
2023-06-13 14:56:46 +0200

    add error-of-accuracy sandbox tool to compare interpolation methods

---
+++ src/sandbox/interpolation/atlas-eoa-computation.cc
@@ -0,0 +1,345 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+#include <cmath>
+#include <fstream>
+#include <map>
+#include <unordered_map>
+
+#include "eckit/geometry/Sphere.h"
+#include "eckit/log/Bytes.h"
+#include "eckit/log/JSON.h"
+#include "eckit/types/FloatCompare.h"
+
+#include "atlas/array.h"
+#include "atlas/array/MakeView.h"
+#include "atlas/field.h"
+#include "atlas/grid.h"
+#include "atlas/grid/Spacing.h"
+#include "atlas/grid/detail/spacing/CustomSpacing.h"
+#include "atlas/interpolation/Interpolation.h"
+#include "atlas/interpolation/method/unstructured/ConservativeSphericalPolygonInterpolation.h"
+#include "atlas/mesh.h"
+#include "atlas/mesh/Mesh.h"
+#include "atlas/mesh/actions/Build2DCellCentres.h"
+#include "atlas/meshgenerator.h"
+#include "atlas/option.h"
+#include "atlas/output/Gmsh.h"
+#include "atlas/runtime/AtlasTool.h"
+#include "atlas/util/Config.h"
+#include "atlas/util/function/MDPI_functions.h"
+#include "atlas/util/function/SolidBodyRotation.h"
+#include "atlas/util/function/SphericalHarmonic.h"
+#include "atlas/util/function/VortexRollup.h"
+
+#include "tests/AtlasTestEnvironment.h"
+
+namespace atlas {
+
+using CSPInterpolation = interpolation::method::ConservativeSphericalPolygonInterpolation;
+
+class AtlasEOAComputation : public AtlasTool {
+    int execute(const AtlasTool::Args& args) override;
+    std::string briefDescription() override { return "Computation of experimental order of accuracy for Atlas' interpolation methods"; }
+    std::string usage() override {
+        return name() +
+               " [--source.grid=gridname] "
+               "[--target.grid=gridname] [OPTION]... [--help]";
+    }
+
+    int numberOfPositionalArguments() override { return -1; }
+    int minimumPositionalArguments() override { return 0; }
+
+public:
+    AtlasEOAComputation(int argc, char* argv[]): AtlasTool(argc, argv) {
+        add_option(new Separator("Interpolation options"));
+        add_option(new SimpleOption<std::string>("type","Type of interpolation"));
+        add_option(new SimpleOption<std::string>("interpolation.structured","Is the interpolation for structured grids"));
+        
+        add_option(new Separator("Initial data"));
+        add_option(new SimpleOption<std::string>(
+            "init", "Setup initial source field [ constant, spherical_harmonic, vortex_rollup (default), solid_body_rotation_wind_magnitude, MDPI_sinusoid, MDPI_harmonic, MDPI_vortex, MDPI_gulfstream ]"));
+        add_option(new SimpleOption<double>("solid_body_rotation.angle", "Angle of solid body rotation (default = 0.)"));
+        add_option(new SimpleOption<double>("vortex_rollup.t", "Value that controls vortex rollup (default = 0.5)"));
+        add_option(new SimpleOption<double>("constant.value", "Value that is assigned in case init==constant)"));
+        add_option(new SimpleOption<long>("spherical_harmonic.n", "total wave number 'n' of a spherical harmonic"));
+        add_option(new SimpleOption<long>("spherical_harmonic.m", "zonal wave number 'm' of a spherical harmonic"));
+    
+        add_option(new Separator("Input options"));
+        //add_option(new SimpleOption<std::string>("input.meshgenerator.pole_elements", "default = pentagons"));
+        //add_option(new SimpleOption<bool>("input.meshed",  "Use function spaces based on mesh, also required for gmsh output"));
+        //add_option(new SimpleOption<eckit::PathName>("input.gmsh.file", "Input gmsh file. If not provided, no gmsh output will be performed"));
+        //add_option(new SimpleOption<std::string>("input.gmsh.coordinates", "Mesh coordinates: [xy, lonlat, xyz]"));
+
+        add_option(new Separator("Output options"));
+        //add_option(new SimpleOption<bool>("output.cell_centred", "Overwrite defaults"));
+        //add_option(new SimpleOption<std::string>("output.meshgenerator.pole_elements", "default = pentagons"));
+        //add_option(new SimpleOption<bool>("output.meshed", "Use function spaces based on mesh, also required for gmsh output"));
+        //add_option(new SimpleOption<eckit::PathName>("output.gmsh.file", "Output gmsh file. If not provided, no gmsh output will be performed"));
+        //add_option(new SimpleOption<std::string>("output.gmsh.coordinates", "Mesh coordinates: [xy, lonlat, xyz]"));
+       
+        add_option(new Separator("Advanced / Debugging"));
+        //add_option(new SimpleOption<bool>("double_remap", "default: true for target data on nodes"));
+    }
+
+    struct Timers {
+        using StopWatch = atlas::runtime::trace::StopWatch;
+        StopWatch target_setup;
+        StopWatch source_setup;
+        StopWatch initial_condition;
+        StopWatch interpolation_setup;
+        StopWatch interpolation_execute;
+    } timers;
+};
+
+std::function<double(const PointLonLat&)> get_init(const AtlasTool::Args& args) {
+    std::string init;
+    args.get("init", init = "vortex_rollup");
+    if (init == "vortex_rollup") {
+        double t;
+        args.get("vortex_rollup.t", t = 1.);
+        return [t](const PointLonLat& p) { return util::function::vortex_rollup(p.lon(), p.lat(), t); };
+    }
+    else if (init == "spherical_harmonic") {
+        int n = 2;
+        int m = 2;
+        args.get("spherical_harmonic.n", n);
+        args.get("spherical_harmonic.m", m);
+
+        bool caching = true;  // true -> warning not thread-safe
+        util::function::SphericalHarmonic Y(n, m, caching);
+        return [Y](const PointLonLat& p) { return Y(p.lon(), p.lat()); };
+    }
+    else if (init == "constant") {
+        double value;
+        args.get("constant.value", value = 1.);
+        return [value](const PointLonLat&) { return value; };
+    }
+    else if (init == "solid_body_rotation_wind_magnitude") {
+        double beta;
+        args.get("solid_body_rotation.angle", beta = 0.);
+        util::function::SolidBodyRotation sbr(beta);
+        return [sbr](const PointLonLat& p) { return sbr.windMagnitude(p.lon(), p.lat()); };
+    }
+    else if (init == "MDPI_sinusoid") {
+        auto sbr = util::function::MDPI_sinusoid;
+        return [sbr](const PointLonLat& p) { return sbr(p.lon(), p.lat()); };
+    }
+    else if (init == "MDPI_harmonic") {
+        auto sbr = util::function::MDPI_harmonic;
+        return [sbr](const PointLonLat& p) { return sbr(p.lon(), p.lat()); };
+    }
+    else if (init == "MDPI_vortex") {
+        auto sbr = util::function::MDPI_vortex;
+        return [sbr](const PointLonLat& p) { return sbr(p.lon(), p.lat()); };
+    }
+    else if (init == "MDPI_gulfstream") {
+        auto sbr = util::function::MDPI_gulfstream;
+        return [sbr](const PointLonLat& p) { return sbr(p.lon(), p.lat()); };
+    }
+    else {
+        if (args.has("init")) {
+            Log::error() << "Bad value for \"init\": \"" << init << "\" not recognised." << std::endl;
+            ATLAS_NOTIMPLEMENTED;
+        }
+    }
+    ATLAS_THROW_EXCEPTION("Should not be here");
+}
+
+FunctionSpace create_functionspace(Mesh& mesh, int halo, std::string type, bool struct_interpolation) {
+    if (type.empty()) {
+        type = "NodeColumns";
+        if (mesh.grid().type() == "healpix" || mesh.grid().type() == "cubedsphere") {
+            type = "CellColumns";
+        }
+    }
+    if (struct_interpolation) {
+        type = "StructuredColumns";
+    }
+    if (type == "CellColumns") {
+        if (!mesh.cells().has_field("lonlat")) {
+            mesh::actions::Build2DCellCentres{"lonlat"}(mesh);
+        }
+        return functionspace::CellColumns(mesh, option::halo(halo));
+    }
+    else if (type == "NodeColumns") {
+        return functionspace::NodeColumns(mesh, option::halo(std::max(1,halo)));
+    }
+    else {
+        return functionspace::StructuredColumns(mesh.grid(), option::halo(halo));
+    }
+    ATLAS_THROW_EXCEPTION("FunctionSpace " << type << " is not recognized.");
+}
+
+
+double compute_analytical_error(const Field target, std::function<double(const PointLonLat&)> func, Mesh src_mesh, Mesh tgt_mesh, double& err_remap_linf){
+    auto tgt_vals             = array::make_view<double, 1>(target);
+    const auto tgt_cell_halo  = array::make_view<int, 1>(tgt_mesh.cells().halo());
+    const auto tgt_node_ghost = array::make_view<int, 1>(tgt_mesh.nodes().ghost());
+    const auto tgt_node_halo  = array::make_view<int, 1>(tgt_mesh.nodes().halo());
+
+    // to get tgt_points and tgt_areas
+    // note: no need for polygon intersections here!!!
+    auto src_fs = create_functionspace(src_mesh, 2, "NodeColumns", false);
+    auto tgt_fs = create_functionspace(tgt_mesh, 1, "NodeColumns", false);
+    auto interpolation = CSPInterpolation();
+    interpolation.do_setup(src_fs, tgt_fs);
+
+    double err_remap_l2 = 0;
+    err_remap_linf = -1.;
+    size_t ncells = std::min<size_t>(tgt_vals.size(), tgt_mesh.cells().size());
+    size_t nnodes = std::min<size_t>(tgt_vals.size(), tgt_mesh.nodes().size());
+    for (idx_t tpt = 0; tpt < interpolation.tgt_npoints(); ++tpt) {
+        //if (tgt_cell_halo(tpt)) { // in case of cell data
+        if (tgt_node_ghost(tpt) or tgt_node_halo(tpt)) { // for node data
+            continue;
+        }
+        auto p = interpolation.tgt_points(tpt);
+        PointLonLat pll;
+        eckit::geometry::Sphere::convertCartesianToSpherical(1., p, pll);
+        double err_l = std::abs(tgt_vals(tpt) - func(pll));
+        err_remap_l2 += err_l * err_l * interpolation.tgt_areas(tpt);
+        err_remap_linf = std::max(err_remap_linf, err_l);
+    }
+    return err_remap_l2;
+}
+
+int AtlasEOAComputation::execute(const AtlasTool::Args& args) {
+    ATLAS_ASSERT(atlas::mpi::size() == 1);
+
+    auto src_grid = StructuredGrid(args.getString("source.grid", "O16"));
+    auto tgt_grid = StructuredGrid(args.getString("target.grid", "O128"));
+
+    timers.target_setup.start();
+    auto tgt_mesh = Mesh{tgt_grid, grid::Partitioner(args.getString("target.partitioner", "regular_bands"))};
+    auto tgt_functionspace =
+        create_functionspace(tgt_mesh, args.getLong("target.halo", 0), args.getString("target.functionspace", ""), args.getBool("interpolation.structured", 0));
+    auto tgt_field = tgt_functionspace.createField<double>();
+    timers.target_setup.stop();
+
+    timers.source_setup.start();
+    auto src_meshgenerator =
+        MeshGenerator{src_grid.meshgenerator() | option::halo(2) | util::Config("pole_elements", "")};
+    auto src_partitioner = grid::MatchingPartitioner{tgt_mesh, util::Config("partitioner",args.getString("source.partitioner", "spherical-polygon"))};
+    auto src_mesh        = src_meshgenerator.generate(src_grid, src_partitioner);
+    auto src_functionspace =
+        create_functionspace(src_mesh, args.getLong("source.halo", 2), args.getString("source.functionspace", ""), args.getBool("interpolation.structured", 0));
+    auto src_field = src_functionspace.createField<double>();
+    timers.source_setup.stop();
+
+    {
+        ATLAS_TRACE("Initial condition");
+        timers.initial_condition.start();
+        const auto lonlat = array::make_view<double, 2>(src_functionspace.lonlat());
+        auto src_view     = array::make_view<double, 1>(src_field);
+        auto f            = get_init(args);
+        for (idx_t n = 0; n < lonlat.shape(0); ++n) {
+            src_view(n) = f(PointLonLat{lonlat(n, LON), lonlat(n, LAT)});
+        }
+        src_field.set_dirty(true);
+        timers.initial_condition.start();
+    }
+
+    timers.interpolation_setup.start();
+    auto interpolation =
+        Interpolation(args, src_functionspace, tgt_functionspace);
+    Log::info() << interpolation << std::endl;
+    timers.interpolation_setup.stop();
+
+
+    timers.interpolation_execute.start();
+    auto metadata = interpolation.execute(src_field, tgt_field);
+    timers.interpolation_execute.stop();
+
+    double err_remap_linf;
+    double err_remap_l2 = compute_analytical_error(tgt_field, get_init(args), src_mesh, tgt_mesh, err_remap_linf);
+    std::cout << "L2 error: " << err_remap_l2 << std::endl;
+    std::cout << "L_inf error: " << err_remap_linf << std::endl;
+
+    // API not yet acceptable
+    Field src_conservation_field;
+    {
+        using Statistics = interpolation::method::ConservativeSphericalPolygonInterpolation::Statistics;
+        Statistics stats(metadata);
+        //stats.accuracy(interpolation, tgt_field, get_init(args), &metadata);
+        //src_conservation_field = stats.diff(interpolation, src_field, tgt_field);
+        //src_conservation_field.set_dirty(true);
+        //src_conservation_field.haloExchange();
+    }
+
+    Log::info() << "interpolation metadata: \n";
+    {
+        eckit::JSON json(Log::info(), eckit::JSON::Formatting::indent(2));
+        json << metadata;
+    }
+    Log::info() << std::endl;
+
+    if (args.getBool("output-gmsh", false)) {
+        if (args.getBool("gmsh.ghost", false)) {
+            ATLAS_TRACE("halo exchange target");
+            tgt_field.haloExchange();
+        }
+        util::Config config(args.getSubConfiguration("gmsh"));
+        output::Gmsh{"src_mesh.msh", config}.write(src_mesh);
+        output::Gmsh{"src_field.msh", config}.write(src_field);
+        output::Gmsh{"tgt_mesh.msh", config}.write(tgt_mesh);
+        output::Gmsh{"tgt_field.msh", config}.write(tgt_field);
+        if (src_conservation_field) {
+            output::Gmsh{"src_conservation_field.msh", config}.write(src_conservation_field);
+        }
+    }
+
+    if (args.getBool("output-json", false)) {
+        util::Config output;
+        output.set("setup.source.grid", args.getString("source.grid"));
+        output.set("setup.target.grid", args.getString("target.grid"));
+        output.set("setup.source.functionspace", src_functionspace.type());
+        output.set("setup.target.functionspace", tgt_functionspace.type());
+        output.set("setup.source.halo", args.getLong("source.halo", 2));
+        output.set("setup.target.halo", args.getLong("target.halo", 0));
+        output.set("setup.interpolation.order", args.getInt("order", 1));
+        output.set("setup.interpolation.normalise_intersections", args.getBool("normalise_intersections", false));
+        output.set("setup.interpolation.validate", args.getBool("validate", false));
+        output.set("setup.interpolation.matrix_free", args.getBool("matrix-free", false));
+        output.set("setup.init", args.getString("init", "vortex_rollup"));
+
+        output.set("runtime.mpi", mpi::size());
+        output.set("runtime.omp", atlas_omp_get_max_threads());
+        output.set("atlas.build_type", ATLAS_BUILD_TYPE);
+
+        output.set("timings.target.setup", timers.target_setup.elapsed());
+        output.set("timings.source.setup", timers.source_setup.elapsed());
+        output.set("timings.initial_condition", timers.initial_condition.elapsed());
+        output.set("timings.interpolation.setup", timers.interpolation_setup.elapsed());
+        output.set("timings.interpolation.execute", timers.interpolation_execute.elapsed());
+
+        output.set("interpolation", metadata);
+
+        eckit::PathName json_filepath(args.getString("json.file", "out.json"));
+        std::ostringstream ss;
+        eckit::JSON json(ss, eckit::JSON::Formatting::indent(4));
+        json << output;
+
+        eckit::FileStream file(json_filepath, "w");
+        std::string str = ss.str();
+        file.write(str.data(), str.size());
+        file.close();
+    }
+
+    return success();
+}
+
+}  // namespace atlas
+
+
+int main(int argc, char* argv[]) {
+    atlas::AtlasEOAComputation tool(argc, argv);
+    return tool.start();
+}
