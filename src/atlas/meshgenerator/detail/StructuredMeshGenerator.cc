419dae84d9ae56cd534d60d1cde9b47077b92fae
Willem Deconinck <willem.deconinck@ecmwf.int>
2023-07-06 12:55:44 +0200

    Fix global indexing for meshes from structured grids with pole patch

--- src/atlas/meshgenerator/detail/StructuredMeshGenerator.cc
+++ src/atlas/meshgenerator/detail/StructuredMeshGenerator.cc
@@ -1273,8 +1273,8 @@ void StructuredMeshGenerator::generate_mesh(const StructuredGrid& rg, const grid
         nodes[1] = tmp;
     };
 
-    bool regular_cells_glb_idx = atlas::RegularLonLatGrid(rg);
-    if( options.getBool("triangulate") || y_numbering > 0) {
+    bool regular_cells_glb_idx = rg.regular();
+    if (options.getBool("triangulate") || y_numbering > 0 || rg.y().front() != 90. || rg.y().back() != -90.) {
         regular_cells_glb_idx = false;
     }
 
