b953afb834277f9fc7b9a7491fc5f2a0e601b3c8
Willem Deconinck <willem.deconinck@ecmwf.int>
2024-04-08 12:45:17 +0000

    Update HaloExchangeCUDA to use function ArrayView::shape(idx)

--- src/atlas/parallel/HaloExchangeCUDA.cu
+++ src/atlas/parallel/HaloExchangeCUDA.cu
@@ -23,8 +23,8 @@ struct get_buffer_index{
     ATLAS_HOST_DEVICE
     static idx_t apply(const array::ArrayView<DATA_TYPE, RANK>& field,
                            const idx_t node_cnt, const idx_t var1_idx) {
-        return field.data_view().template length<RANK-1>() * field.data_view().template length<RANK-2>() * node_cnt +
-                field.data_view().template length<RANK-1>() * var1_idx;
+        return field.shape(RANK-1) * field.shape(RANK-2) * node_cnt +
+                field.shape(RANK-1) * var1_idx;
     }
 };
 
@@ -34,7 +34,7 @@ struct get_buffer_index<ParallelDim, 2>{
     ATLAS_HOST_DEVICE
     static idx_t apply(const array::ArrayView<DATA_TYPE, 2>& field,
                            const idx_t node_cnt, const idx_t var1_idx) {
-        return field.data_view().template length<1>() * node_cnt + var1_idx;
+        return field.shape(1) * node_cnt + var1_idx;
     }
 };
 
@@ -60,7 +60,7 @@ __global__ void pack_kernel(const int sendcnt,  const int* sendmap_ptr, const id
     const idx_t node_cnt = blockIdx.x*blockDim.x + threadIdx.x;
     const idx_t var1_idx = blockIdx.y*blockDim.y + threadIdx.y;
 
-    if(node_cnt >= sendcnt || var1_idx >= field.data_view().template length<1>() ) return;
+    if(node_cnt >= sendcnt || var1_idx >= field.shape(1) ) return;
 
     idx_t buff_idx = get_buffer_index<ParallelDim, RANK>::apply(field, node_cnt, var1_idx);
     const idx_t node_idx = sendmap[node_cnt];
@@ -92,7 +92,7 @@ __global__ void unpack_kernel(const int recvcnt, const int* recvmap_ptr, const i
     const idx_t node_cnt = blockIdx.x*blockDim.x + threadIdx.x;
     const idx_t var1_idx = blockIdx.y*blockDim.y + threadIdx.y;
 
-    if(node_cnt >= recvcnt || var1_idx >= field.data_view().template length<1>() ) return;
+    if(node_cnt >= recvcnt || var1_idx >= field.shape(1) ) return;
 
     const idx_t node_idx = recvmap[node_cnt];
 
@@ -124,7 +124,8 @@ struct get_n_cuda_blocks
 {
   template<typename DATA_TYPE>
   static unsigned int apply(const array::ArrayView<DATA_TYPE, RANK>& hfield, const unsigned int block_size_y) {
-      return (hfield.data_view().template length<get_first_non_parallel_dim<ParallelDim, RANK, 0>::apply()>()+block_size_y-1)/block_size_y;
+      constexpr auto dim = get_first_non_parallel_dim<ParallelDim, RANK, 0>::apply();
+      return (hfield.shape(dim)+block_size_y-1)/block_size_y;
   }
 };
 
