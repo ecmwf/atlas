b7c37d1c5e912adbf8ab8dc06baaa00862abc82d
Slavko Brdar <slavko.brdar@ecmwf.int>
2024-04-22 16:40:50 +0000

    add FielSet cummulative gpu operations: allocate_device, update_device ...

--- src/tests/field/fctest_field_gpu.F90
+++ src/tests/field/fctest_field_gpu.F90
@@ -107,5 +107,191 @@ END_TEST
 
 ! -----------------------------------------------------------------------------
 
+TEST( test_fieldset_cummulative_gpu_calls_all_fields )
+implicit none
+type(atlas_FieldSet) :: fset
+type(atlas_Field) :: field
+real(4), pointer :: fview(:,:)
+
+print *, "test_fieldset_cummulative_gpu_calls_all_fields"
+fset = atlas_FieldSet()
+call fset%add(atlas_Field(name="f1", kind=atlas_real(4), shape=[1,2]))
+call fset%add(atlas_Field(name="f2", kind=atlas_real(4), shape=[2,2]))
+call fset%add(atlas_Field(name="f3", kind=atlas_real(4), shape=[2,1]))
+
+print *, "... by idx"
+field = fset%field(2)
+call field%data(fview)
+fview(:,:) = 1.
+fview(2,1) = 2.
+
+call fset%set_host_needs_update(.false.)
+FCTEST_CHECK_EQUAL(field%device_allocated(), .false.)
+call fset%allocate_device()
+FCTEST_CHECK_EQUAL(field%device_allocated(), .true.)
+call fset%update_device()
+
+!$acc data present(fview)
+!$acc kernels
+fview(2,1) = 5.
+!$acc end kernels
+!$acc end data
+
+FCTEST_CHECK_EQUAL( fview(2,1), 2. )
+call fset%update_host()
+FCTEST_CHECK_EQUAL( fview(2,1), 5. )
+call fset%deallocate_device()
+FCTEST_CHECK_EQUAL(field%device_allocated(), .false.)
+
+print *, "... by name"
+field = fset%field("f3")
+call field%data(fview)
+fview(:,:) = 3.
+fview(2,1) = 4.
+
+call fset%allocate_device()
+call fset%update_device()
+
+!$acc data present(fview)
+!$acc kernels
+fview(2,1) = 7.
+!$acc end kernels
+!$acc end data
+
+FCTEST_CHECK_EQUAL( fview(2,1), 4. )
+call fset%update_host()
+FCTEST_CHECK_EQUAL( fview(2,1), 7. )
+
+call fset%final()
+END_TEST
+
+! -----------------------------------------------------------------------------
+
+TEST( test_fieldset_cummulative_gpu_calls_subset_field)
+implicit none
+type(atlas_FieldSet) :: fset
+type(atlas_Field) :: field
+real(4), pointer :: fview(:,:)
+print *, "fieldset_cummulative_gpu_calls subset fields"
+
+fset = atlas_FieldSet()
+call fset%add(atlas_Field(name="f1", kind=atlas_real(4), shape=[1,2]))
+call fset%add(atlas_Field(name="f2", kind=atlas_real(4), shape=[2,2]))
+call fset%add(atlas_Field(name="f3", kind=atlas_real(4), shape=[2,1]))
+
+print *, "... by idx"
+field = fset%field(2)
+call field%data(fview)
+fview(:,:) = 1.
+fview(2,1) = 2.
+
+call fset%set_host_needs_update((/2, 3/), .false.)
+call fset%allocate_device((/ 2, 3 /)) ! device allocate only for field 2 and 3
+call fset%sync_host_device((/ 2, 3 /)) ! device-memcpy for field 2 and 3
+
+!$acc data present(fview)
+!$acc kernels
+fview(2,1) = 5.
+!$acc end kernels
+!$acc end data
+
+call fset%set_host_needs_update((/ 2 /))
+
+FCTEST_CHECK_EQUAL( fview(2,1), 2. )
+call fset%sync_host_device()
+FCTEST_CHECK_EQUAL( fview(2,1), 5. )
+call fset%deallocate_device()
+
+print *, "... by name"
+field = fset%field("f3")
+call field%data(fview)
+fview(:,:) = 3.
+fview(2,1) = 4.
+
+call fset%allocate_device((/ "f2", "f3" /))
+call fset%sync_host_device((/ "f2", "f3" /))
+
+!$acc data present(fview)
+!$acc kernels
+fview(2,1) = 7.
+!$acc end kernels
+!$acc end data
+
+call fset%set_host_needs_update((/ "f3" /))
+
+FCTEST_CHECK_EQUAL( fview(2,1), 4. )
+call fset%sync_host_device()
+FCTEST_CHECK_EQUAL( fview(2,1), 7. )
+
+call fset%final()
+END_TEST
+
+! -----------------------------------------------------------------------------
+
+TEST( test_fieldset_cummulative_gpu_calls_not_all_field_with_sync_host_device)
+implicit none
+type(atlas_FieldSet) :: fset
+type(atlas_Field) :: field
+real(4), pointer :: fview(:,:)
+
+print *, "test_fieldset_cummulative_gpu_calls_with_sync_host_device"
+fset = atlas_FieldSet()
+call fset%add(atlas_Field(name="f1", kind=atlas_real(4), shape=[1,2]))
+call fset%add(atlas_Field(name="f2", kind=atlas_real(4), shape=[2,2]))
+call fset%add(atlas_Field(name="f3", kind=atlas_real(4), shape=[2,1]))
+
+print *, "... by idx"
+field = fset%field(2)
+call field%data(fview)
+fview(:,:) = 1.
+fview(2,1) = 2.
+
+call fset%set_host_needs_update(.false.)
+call fset%allocate_device((/ 2, 3 /)) ! device allocate only for field 2 and 3
+call fset%set_device_needs_update((/ 2 /))
+call fset%sync_host_device((/ 2 /)) ! device-memcpy for field 2 and 3
+
+!$acc data present(fview)
+!$acc kernels
+fview(2,1) = 5.
+!$acc end kernels
+!$acc end data
+
+call fset%set_host_needs_update((/ 2 /))
+
+FCTEST_CHECK_EQUAL( fview(2,1), 2. )
+call fset%sync_host_device()
+FCTEST_CHECK_EQUAL( fview(2,1), 5. )
+
+call fset%deallocate_device()
+
+print *, "... by name"
+field = fset%field("f2")
+call field%data(fview)
+fview(:,:) = 3.
+fview(2,1) = 4.
+
+call fset%set_host_needs_update(.false.)
+call fset%allocate_device((/ "f2", "f3" /)) ! device allocate only for field 2 and 3
+call fset%set_device_needs_update((/ "f2" /)) ! set fields for sync_host_device
+call fset%sync_host_device((/ "f2" /)) ! device-memcpy for field 'f2'
+
+!$acc data present(fview)
+!$acc kernels
+fview(2,1) = 7.
+!$acc end kernels
+!$acc end data
+
+call fset%set_host_needs_update((/ "f2" /))
+
+FCTEST_CHECK_EQUAL( fview(2,1), 4. )
+call fset%sync_host_device()
+FCTEST_CHECK_EQUAL( fview(2,1), 7. )
+
+call fset%final()
+END_TEST
+
+! -----------------------------------------------------------------------------
+
 END_TESTSUITE
 
