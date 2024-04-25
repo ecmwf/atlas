e59fe9c0308f3b020e74749454f01c599be75aaf
Willem Deconinck <willem.deconinck@ecmwf.int>
2024-04-25 09:36:00 +0000

    Use standard CMake find_package(OpenACC) instead of handcrafted flags

--- cmake/features/ACC.cmake
+++ cmake/features/ACC.cmake
@@ -2,28 +2,25 @@
 
 if( atlas_HAVE_ATLAS_FIELD )
 
-set( ATLAS_ACC_CAPABLE FALSE )
-if( HAVE_CUDA )
-  if( CMAKE_Fortran_COMPILER_ID MATCHES "PGI|NVHPC" )
-    set( ATLAS_ACC_CAPABLE TRUE )
-  endif()
-endif()
-
-ecbuild_add_option( FEATURE ACC
-                    DESCRIPTION  "OpenACC capable data structures"
-                    CONDITION ATLAS_ACC_CAPABLE )
-
-if( atlas_HAVE_ACC )
-  if( CMAKE_Fortran_COMPILER_ID MATCHES "PGI|NVHPC" )
-      #set( ACC_Fortran_FLAGS -acc -ta=tesla,nordc )
-    set( ACC_Fortran_FLAGS "-acc=gpu;-gpu=gvmode,lineinfo,fastmath,rdc" )
-    set( ACC_C_FLAGS ${ACC_Fortran_FLAGS} )
-    find_program( ACC_C_COMPILER NAMES pgcc HINTS ${PGI_DIR} ${NVPHC_DIR} ENV PGI_DIR NVHPC_DIR PATH_SUFFIXES bin )
-    if( NOT ACC_C_COMPILER )
-      ecbuild_error( "Could not find OpenACC capable C compiler" )
+  set( ATLAS_FIND_OPENACC ${HAVE_CUDA} )
+  if (DEFINED ATLAS_ENABLE_ACC)
+    if(NOT ATLAS_ENABLE_ACC)
+      set( ATLAS_FIND_OPENACC OFF )
+    endif()
+  elseif (DEFINED ENABLE_ACC)
+    if (NOT ENABLE_ACC)
+      set( ATLAS_FIND_OPENACC OFF )
     endif()
   endif()
-endif()
+
+  if (ATLAS_FIND_OPENACC)
+    enable_language(C)  # So that find_package(OpenACC) looks C components
+    find_package(OpenACC)
+  endif()
+
+  ecbuild_add_option( FEATURE ACC
+                      DESCRIPTION  "OpenACC capable data structures"
+                      CONDITION HAVE_CUDA AND OpenACC_C_FOUND )
 
 else()
   set( HAVE_ACC 0 )
--- cmake/project_summary.cmake
+++ cmake/project_summary.cmake
@@ -42,9 +42,9 @@ endif()
 if( atlas_HAVE_ACC )
 
     ecbuild_info( "ACC" )
-    ecbuild_info( "    ACC_C_COMPILER     : [${ACC_C_COMPILER}]" )
-    ecbuild_info( "    ACC_C_FLAGS        : [${ACC_C_FLAGS}]" )
-    ecbuild_info( "    ACC_Fortran_FLAGS  : [${ACC_Fortran_FLAGS}]" )
+    ecbuild_info( "    OpenACC_C_FLAGS       : [${OpenACC_C_FLAGS}]" )
+    ecbuild_info( "    OpenACC_CXX_FLAGS     : [${OpenACC_CXX_FLAGS}]" )
+    ecbuild_info( "    OpenACC_Fortran_FLAGS : [${OpenACC_Fortran_FLAGS}]" )
 
 endif()
 
