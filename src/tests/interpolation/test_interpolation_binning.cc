fb363059b045ffe663fedbc75f371dfb32475cb8
Lorenzo Milazzo <lorenzo.milazzo@metoffice.gov.uk>
2024-06-07 15:16:42 +0100

    update 07.06.24-03

--- src/tests/interpolation/test_interpolation_binning.cc
+++ src/tests/interpolation/test_interpolation_binning.cc
@@ -124,8 +124,7 @@ CASE("rigridding from high to low resolution; grid type: CS-LFR") {
 
 
   const auto scheme = util::Config("type", "binning") |
-                      util::Config("ancillary_scheme", option::type("cubedsphere-bilinear")) |
-                      util::Config("grid_type", "CS-LFR");
+                      util::Config("ancillary_scheme", option::type("cubedsphere-bilinear"));
 
   Interpolation regrid_high2low(scheme, csfs_s, csfs_t);
 
@@ -180,8 +179,7 @@ CASE("rigridding from high to low resolution; grid type: O") {
 
 
   const auto scheme = util::Config("type", "binning") |
-                      util::Config("ancillary_scheme", option::type("structured-bilinear")) |
-                      util::Config("grid_type", "O");
+                      util::Config("ancillary_scheme", option::type("structured-bilinear"));
 
   Interpolation regrid_high2low(scheme, ncfs_s, ncfs_t);
 
