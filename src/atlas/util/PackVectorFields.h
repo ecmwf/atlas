9e2a75e564d4776d0c80678ed337e5702bdcf576
odlomax <oliver.lomax@metoffice.gov.uk>
2024-08-12 11:57:36 +0100

    Added Copyright.

--- src/atlas/util/PackVectorFields.h
+++ src/atlas/util/PackVectorFields.h
@@ -1,3 +1,10 @@
+/*
+ * (C) Crown Copyright 2024 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
 #pragma once
 
 #include "atlas/field.h"
