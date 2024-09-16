6fe2801218d8ca4f50fe8582e031362a52dca4fc
Slavko Brdar <slavko.brdar@ecmwf.int>
2024-09-16 15:09:37 +0200

    add problematic multifield-array test with config

--- src/tests/field/test_multifield_ifs.cc
+++ src/tests/field/test_multifield_ifs.cc
@@ -18,7 +18,6 @@
 #include "atlas/array/MakeView.h"
 #include "atlas/field/Field.h"
 #include "atlas/field/MultiField.h"
-//#include "atlas/field/MultiFieldCreatorIFS.h"
 #include "atlas/field/MultiFieldCreatorArray.h"
 #include "atlas/field/detail/MultiFieldImpl.h"
 #include "atlas/runtime/Exception.h"
@@ -26,7 +25,6 @@
 
 #include "tests/AtlasTestEnvironment.h"
 
-using namespace atlas::field;
 using namespace atlas::field;
 
 namespace atlas {
@@ -38,7 +36,10 @@ const std::vector<int> make_shape(const std::initializer_list<int>& list) {
 
 CASE("multifield_generator") {
     EXPECT(MultiFieldCreatorFactory::has("MultiFieldCreatorIFS"));
-    std::unique_ptr<MultiFieldCreator> MultiFieldCreator(MultiFieldCreatorFactory::build("MultiFieldCreatorIFS"));
+    std::unique_ptr<MultiFieldCreator> MultiFieldCreatorIFS(MultiFieldCreatorFactory::build("MultiFieldCreatorIFS"));
+
+    EXPECT(MultiFieldCreatorFactory::has("MultiFieldCreatorArray"));
+    std::unique_ptr<MultiFieldCreator> MultiFieldCreatorArray(MultiFieldCreatorFactory::build("MultiFieldCreatorArray"));
 }
 
 
@@ -56,11 +57,11 @@ CASE("multifield_ifs_create") {
         p.set("nlev", nlev);
         p.set("datatype", array::make_datatype<Value>().str());
         p.set("fields", {
-                            util::Config("name", "temperature"),     //
-                            util::Config("name", "pressure"),        //
-                            util::Config("name", "density"),         //
-                            util::Config("name", "clv")("nvar", 5),  //
-                            util::Config("name", "wind_u")           //
+                            util::Config("name", "temperature"),
+                            util::Config("name", "pressure"),
+                            util::Config("name", "density"),
+                            util::Config("name", "clv")("nvar", 5),  // 'clv' with 5 subvariables
+                            util::Config("name", "wind_u")
                         });
         return p.json();
     };
@@ -173,7 +174,22 @@ CASE("multifield_array_create") {
     const int nblks = (ngptot + nproma - 1) / nproma;
     const std::vector<std::string> var_names = {"temperature", "pressure", "density", "clv", "wind_u"};
 
-    SECTION("test_3d") {
+    auto json = [&]() -> std::string {
+        util::Config p;
+        p.set("type", "MultiFieldCreatorArray");
+        p.set("shape", {nblks, -1, nlev, nproma});
+        p.set("datatype", array::make_datatype<Value>().str());
+        p.set("fields", {
+                            util::Config("name", "temperature"),     //
+                            util::Config("name", "pressure"),        //
+                            util::Config("name", "density"),         //
+                            util::Config("name", "clv")("nvar", 5),  //
+                            util::Config("name", "wind_u")           //
+                        });
+        return p.json();
+    };
+
+    SECTION("test_MultiFieldArray_noconfig_3d") {
         int nlev = 3;
         const std::vector<int> vshape = make_shape({nblks, -1, nlev, nproma});
         MultiField multifield(array::make_datatype<Value>(), vshape, var_names);
@@ -234,7 +250,7 @@ CASE("multifield_array_create") {
         EXPECT_EQ(field_v(1,2,3), 4);
     }
 
-    SECTION("test_2d") {
+    SECTION("test_MultiFieldArray_noconfig_2d") {
         const std::vector<int> vshape = make_shape({nblks, -1, nproma});
         MultiField multifield(array::make_datatype<Value>(), vshape, var_names);
 
@@ -289,6 +305,101 @@ CASE("multifield_array_create") {
         EXPECT(fieldset.has("temperature"));
         EXPECT(fieldset.has("wind_u"));
         EXPECT_EQ(field_v(1,3), 4);
+    }
+
+    SECTION("Print configuration") {
+        Log::info() << "json = " << json() << std::endl;
+    }
+
+    SECTION("test_MultiFieldArray_config") {
+        MultiField multifield{eckit::YAMLConfiguration{json()}};
+
+        const auto nblk = multifield.array().shape(0);
+        const auto nvar = multifield.array().shape(1);
+        const auto nfld = multifield.size();
+        EXPECT_EQ(nfld, 9);
+        EXPECT_EQ(nvar, 9);
+
+        EXPECT_EQ(multifield.size(), 9);
+        EXPECT(multifield.has("temperature"));
+        EXPECT(multifield.has("pressure"));
+        EXPECT(multifield.has("density"));
+        EXPECT(multifield.has("clv_0"));
+        EXPECT(multifield.has("wind_u"));
+
+        Log::info() << multifield.field("temperature") << std::endl;
+        Log::info() << multifield.field("pressure") << std::endl;
+        Log::info() << multifield.field("density") << std::endl;
+        Log::info() << multifield.field("clv_0") << std::endl;
+        Log::info() << multifield.field("wind_u") << std::endl;
+
+        auto temp   = array::make_view<Value, 3>(multifield.field("temperature"));
+        auto pres   = array::make_view<Value, 3>(multifield.field("pressure"));
+        auto dens   = array::make_view<Value, 3>(multifield.field("density"));
+        auto clv    = array::make_view<Value, 3>(multifield.field("clv_0"));  // note rank 4
+        auto wind_u = array::make_view<Value, 3>(multifield.field("wind_u"));
+
+        EXPECT_EQ(multifield[0].name(), "temperature");
+        EXPECT_EQ(multifield[1].name(), "pressure");
+        EXPECT_EQ(multifield[2].name(), "density");
+        EXPECT_EQ(multifield[3].name(), "clv_0");
+        EXPECT_EQ(multifield[8].name(), "wind_u");
+
+        auto block_stride  = multifield.array().stride(0);
+        auto field_stride  = nproma * nlev;
+        auto level_stride  = nproma;
+        auto nproma_stride = 1;
+
+        temp(1, 2, 3)      = 4;
+        pres(5, 6, 7)      = 8;
+        dens(9, 10, 11)    = 12;
+        clv(13, 14, 15) = 16;
+        wind_u(17, 18, 3)  = 19;
+
+        EXPECT_EQ(temp.stride(0), block_stride);
+        EXPECT_EQ(temp.stride(1), level_stride);
+        EXPECT_EQ(temp.stride(2), nproma_stride);
+        EXPECT_EQ(temp.size(), nblk * nlev * nproma);
+
+        EXPECT_EQ(clv.stride(0), block_stride);
+        EXPECT_EQ(clv.stride(1), level_stride);
+        EXPECT_EQ(clv.stride(2), nproma_stride);
+
+        EXPECT_EQ(clv.size(), nblk * nlev * nproma);
+
+
+        // Advanced usage, to access underlying array. This should only be used
+        // in a driver and not be exposed to algorithms.
+        {
+            auto multiarray = array::make_view<Value, 4>(multifield);
+            EXPECT_EQ(multiarray.stride(0), block_stride);
+            EXPECT_EQ(multiarray.stride(1), field_stride);
+            EXPECT_EQ(multiarray.stride(2), level_stride);
+            EXPECT_EQ(multiarray.stride(3), nproma_stride);
+
+            EXPECT_EQ(multiarray(1, 0, 2, 3), 4.);
+            EXPECT_EQ(multiarray(5, 1, 6, 7), 8.);
+            EXPECT_EQ(multiarray(9, 2, 10, 11), 12.);
+            EXPECT_EQ(multiarray(13, 3, 14, 15), 16.);
+            EXPECT_EQ(multiarray(17, 8, 18, 3), 19.);
+
+            EXPECT_EQ(multiarray.size(), nblk * nvar * nlev * nproma);
+        }
+
+        // access FieldSet through MultiField
+        auto fieldset = multifield->fieldset();
+        auto field_v = array::make_view<Value,3>(fieldset.field("temperature"));
+        EXPECT_EQ(fieldset.size(), 9);
+        EXPECT(fieldset.has("temperature"));
+        EXPECT(fieldset.has("wind_u"));
+        EXPECT_EQ(field_v(1,2,3), 4);
+    }
+
+    SECTION("test registry") {
+        {
+            Field field = MultiField {eckit::YAMLConfiguration{json()}}.field("temperature");
+            auto temp = array::make_view<Value, 3>(field);
+        }
     }
 }
 
