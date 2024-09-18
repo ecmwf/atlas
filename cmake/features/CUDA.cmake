4155412d489fd2bf273115fab7d5e67773163225
Willem Deconinck <willem.deconinck@ecmwf.int>
2024-09-18 15:27:35 +0300

    Add support for HIP with cmake

--- cmake/features/CUDA.cmake
+++ cmake/features/CUDA.cmake
@@ -1,15 +1,37 @@
 
-ecbuild_add_option( FEATURE CUDA
-                    DESCRIPTION "Enable CUDA support"
-                    DEFAULT OFF
-                  )
+# ecbuild_add_option( FEATURE CUDA
+#                     DESCRIPTION "Enable CUDA support"
+#                     DEFAULT OFF
+#                   )
+# ecbuild_add_option( FEATURE HIP
+#                     DESCRIPTION "Enable CUDA support"
+#                     DEFAULT OFF
+#                   )
 
-if( HAVE_CUDA )
+set( atlas_HAVE_CUDA 0 )
+set( atlas_HAVE_HIP  0 )
+set( atlas_HAVE_GPU  0 )
 
-    enable_language( CUDA )
-    ecbuild_info( "CUDA language enabled" )
-
-    find_package( CUDAToolkit REQUIRED )
-
+if( hic_HAVE_CUDA )
+  enable_language( CUDA )
+  ecbuild_info( "CUDA language enabled" )
+  find_package(CUDAToolkit REQUIRED)
+  set( atlas_HAVE_CUDA 1 )
+  set( atlas_HAVE_GPU  1 )
+elseif( hic_HAVE_HIP )
+  enable_language( HIP )
+  ecbuild_info( "HIP language enabled" )
+  find_package(hip CONFIG REQUIRED)
+  set( atlas_HAVE_HIP 1 )
+  set( atlas_HAVE_GPU 1 )
 endif()
 
+set( HAVE_CUDA ${atlas_HAVE_CUDA} )
+set( HAVE_HIP  ${atlas_HAVE_HIP} )
+set( HAVE_GPU  ${atlas_HAVE_GPU} )
+
+if( HAVE_GPU )
+  ecbuild_info("GPU support enabled")
+else()
+  ecbuild_info("GPU support not enabled")
+endif()
