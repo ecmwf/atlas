89455dcfc7d9f33ad1667b76e3d6d13240c9bbff
Willem Deconinck <willem.deconinck@ecmwf.int>
2024-10-15 10:50:41 +0300

    Fix warnings on lumi

--- hic/src/hic/hic_runtime.h
+++ hic/src/hic/hic_runtime.h
@@ -23,17 +23,11 @@
   #include <cuda_runtime.h>
 #elif HIC_BACKEND_HIP
   #define HIC_BACKEND hip
-//#if defined(DEPRECATED)
-//  #define DEFINED_OUTERSCOPE DEPRECATED
-//  #undef DEPRECATED
-//  #endif
+
+  #pragma push_macro("DEPRECATED")
+  #undef DEPRECATED
   #include <hip/hip_runtime.h>
-//  #if defined(DEPRECATED)
-//  #undef DEPRECATED
-//  #endif
-//  #if defined(DEFINED_OUTERSCOPE)
-//  #define DEPRECATED DEFINED_OUTERSCOPE
-//  #endif
+  #pragma pop_macro("DEPRECATED")
 
 #if HIP_VERSION_MAJOR < 6
   enum hicMemoryType {
