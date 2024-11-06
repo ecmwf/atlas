7ad63e812ebef0799c0e0c9ff3b83c84cd3809b2
Willem Deconinck <willem.deconinck@ecmwf.int>
2024-11-06 10:16:57 +0000

    Nicer output on TraceMemoryResource

--- pluto/src/pluto/memory_resource/TraceMemoryResource.cc
+++ pluto/src/pluto/memory_resource/TraceMemoryResource.cc
@@ -11,15 +11,41 @@
 #include "TraceMemoryResource.h"
 
 #include <limits>
+#include <sstream>
+#include <iomanip>
 
 namespace pluto {
+
+static std::string bytes_to_string(std::size_t bytes) {
+    constexpr double KB = 1024.;
+    constexpr double MB = 1024.*KB;
+    constexpr double GB = 1024.*MB;
+    double b = static_cast<double>(bytes);
+
+    std::stringstream ss;
+    ss << std::setprecision(2) << std::fixed;
+    if (b >= GB) {
+        ss << b/GB << "G";
+    }
+    else if (b >= MB) {
+        ss << b/MB << "M";
+    }
+    else if (b >= KB) {
+        ss << b/KB << "K";
+    }
+    else {
+        ss << b << "B";
+    }
+    return ss.str();
+}
+
 
 int TraceMemoryResource::nest = 0;
 
 void* TraceMemoryResource::do_allocate(std::size_t bytes, std::size_t alignment) {
     nest++;
     if (TraceOptions::instance().enabled) {
-        *TraceOptions::instance().out << std::string(4*nest,' ') << "[" << name_ << " (alloc)] " << bytes << " bytes with alignment " << alignment << std::endl;
+        *TraceOptions::instance().out << std::string(4*nest,' ') << "[" << name_ << " (alloc)] " << bytes << " bytes (" << bytes_to_string(bytes) << ") with alignment " << alignment << std::endl;
     }
     auto ptr = mr_->allocate(bytes, alignment);
     nest--;
@@ -34,7 +60,7 @@ void TraceMemoryResource::do_deallocate(void* p, std::size_t bytes, std::size_t 
             *TraceOptions::instance().out << std::string(4*nest,' ') << "[" << name_ << " (dealloc)] " << p << std::endl;
         }
         else {
-            *TraceOptions::instance().out << std::string(4*nest,' ') << "[" << name_ << " (dealloc)] " << p << " : " << bytes << " bytes with alignment " << alignment << std::endl;
+            *TraceOptions::instance().out << std::string(4*nest,' ') << "[" << name_ << " (dealloc)] " << p << " : " << bytes << " bytes (" << bytes_to_string(bytes) << ") with alignment " << alignment << std::endl;
         }
     }
     mr_->deallocate(p, bytes, alignment);
