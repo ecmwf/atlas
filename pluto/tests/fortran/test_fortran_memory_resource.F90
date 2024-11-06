7591f18f339527929f4bd525d264af20f32556e5
Willem Deconinck <willem.deconinck@ecmwf.int>
2024-11-06 10:23:54 +0000

    Extend Fortran and add tests

--- pluto/tests/fortran/test_fortran_memory_resource.F90
+++ pluto/tests/fortran/test_fortran_memory_resource.F90
@@ -1,46 +1,35 @@
-program pluto_test_pluto_f
 
-use pluto_module, only : pluto, pluto_memory_resource, pluto_allocator
-use iso_c_binding
+program pluto_test_fortran_memory_resource
+
+use pluto_module, only : pluto, pluto_memory_resource
+use, intrinsic :: iso_c_binding, only: c_ptr, c_sizeof, c_float
 
 implicit none
 
-integer, parameter :: wp = c_float
+type(pluto_memory_resource) :: mr
+call run_allocate(pluto%host%get_default_resource())
+call run_allocate(pluto%new_delete_resource())
+call run_allocate(pluto%managed_resource())
+call run_allocate(pluto%pinned_resource())
+call pluto%host%set_default_resource(pluto%managed_resource())
+call run_allocate(pluto%host%get_default_resource())
+call pluto%host%set_default_resource("pluto::pinned_resource")
+call run_allocate(pluto%host%get_default_resource())
 
-type(pluto_memory_resource) :: host_memory_resource
-type(pluto_allocator) :: host_allocator
-type(c_ptr) :: mem
-real(wp), pointer :: array1d(:), array2d(:,:), array3d(:,:,:), array4d(:,:,:,:)
-real(wp) :: real_value
+contains
 
-integer(4), pointer :: iarray1d(:), jarray1d(:)
-
-
-call pluto%host%set_default_resource("pluto::managed_resource")
-call pluto%host%get_default_resource(host_memory_resource)
-call host_memory_resource%allocate(mem, 10*c_sizeof(real_value))
-
-call c_f_pointer(mem, array1d, [10])
-array1d(:) = 5
-call host_memory_resource%deallocate(mem, 10*c_sizeof(real_value))
-
-call pluto%host%get_default_allocator(host_allocator)
-
-call host_allocator%allocate(array1d, [20])
-call host_allocator%deallocate(array1d)
-
-call host_allocator%allocate(array2d, [5,6])
-call host_allocator%deallocate(array2d)
-
-call host_allocator%allocate(array3d, [5,3,6])
-call host_allocator%deallocate(array3d)
-
-call host_allocator%allocate(array4d, [5,2,3,6])
-call host_allocator%deallocate(array4d)
-
-call host_allocator%allocate(iarray1d, shape=[20])
-call host_allocator%allocate(jarray1d, shape=shape(iarray1d))
-call host_allocator%deallocate(iarray1d)
-call host_allocator%deallocate(jarray1d)
+subroutine run_allocate(memory_resource)
+    implicit none
+    integer, parameter :: wp = c_float
+    type(pluto_memory_resource) :: memory_resource
+    type(c_ptr) :: mem
+    real(wp), pointer :: array1d(:)
+    real(wp) :: real_value
+    integer :: N = 10
+    call memory_resource%allocate(mem, N*c_sizeof(real_value))
+    call c_f_pointer(mem, array1d, [N])
+    array1d(:) = 5
+    call memory_resource%deallocate(mem, N*c_sizeof(real_value))
+end subroutine
 
 end program
