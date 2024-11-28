b1b5553f969222d430255587a142e8f356a15166
mo-jonasganderton <jonas.ganderton@metoffice.gov.uk>
2024-11-28 17:43:54 +0000

    Use projection_.xy2lonlat in lonlat

--- src/atlas/grid/detail/grid/CubedSphere2.cc
+++ src/atlas/grid/detail/grid/CubedSphere2.cc
@@ -81,7 +81,7 @@ void CubedSphere2::lonlat(idx_t n, Point2& point) const {
 // Get the lonlat for a given index
 Point2 CubedSphere2::lonlat(idx_t n) const {
     Point2 point;
-    xy(n, point);
+    lonlat(n, point);
     return point;
 }
 
