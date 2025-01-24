0f328ddac4fb70c4c7d02babf2dba2ef7633bf4e
Jonas Ganderton <jonas.ganderton@metoffice.gov.uk>
2025-01-24 12:44:35 +0000

    New simplified cubed sphere grid (#245)
    * Added grid class boilerplate.
    
    * Added simple concrete implementation.
    
    * Minor formatting changes to match atlas style
    
    * Implement methods unrelated to iterators
    
    * Change name to CS-LFR-<N>-2
    
    * Fix hash and bounding box
    
    * Implement CubedSphere2::print()
    
    * Calculate the lonlat for a given point index
    
    * Tidy up size()
    
    * Change lonlat and xy to use Point2
    
    * Implement Iterator
    
    * Add kgo test using iterators
    
    * Remove temporary test
    
    * Swap xy and lonlat methods
    
    * Remove temporary testing method
    
    * Add whitespace
    
    * Remove temporary comments
    
    * Add test case for basic methods
    
    * Improve spec() behaviour
    
    * Add public method N()
    
    * Change nTiles_ to constexpr
    
    * Combine methods to calculate t,i,j into one simpler method
    
    * Change arrays to eckit matrices
    
    * Use eckit to convert cartesian coordinates to lonlat
    
    * Clean up after tij methods change
    
    * Use projection_.xy2lonlat in lonlat
    
    * Improve clarity of generation of xy point
    
    * Add comments to describe methods
    
    * Remove unused rad_to_deg_
    
    * Use grix.xy() for ComputePointXY
    
    * Use Eigen if it's included
    
    * Make function intent clearer
    
    * Change where vector is normalised
    
    * Add const to method parameter
    
    * Improve comparison method
    
    * Add constructor with grid
    
    * Remove intermediate alpha beta point
    
    * Add pre-transposed lfric rotations
    
    * Switch Eigen matrices to arrays
    
    * Re-include eckit/geometry/Sphere.h
    
    ---------
    
    Co-authored-by: odlomax <oliver.lomax@metoffice.gov.uk>

---
+++ src/atlas/grid/CubedSphereGrid2.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "atlas/grid/Grid.h"
+#include "atlas/grid/detail/grid/CubedSphere2.h"
+
+namespace atlas {
+
+class CubedSphereGrid2 : public atlas::Grid {
+public:
+    using grid_t = grid::detail::grid::CubedSphere2;
+
+public:
+    CubedSphereGrid2(idx_t resolution);
+    CubedSphereGrid2(const Grid& grid);
+
+    bool valid() const { return grid_; };
+    operator bool() const { return valid(); }
+
+private:
+    const grid_t* grid_;
+};
+
+}  // namespace atlas
