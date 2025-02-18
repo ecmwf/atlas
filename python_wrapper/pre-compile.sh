8bedb354f84c7cba72ca47191b3477fbfc43f0f5
Vojta Tuma <vojta.tuma@gmail.com>
2025-02-18 11:07:31 +0100

    Python wheel: fix macos build

--- python_wrapper/pre-compile.sh
+++ python_wrapper/pre-compile.sh
@@ -9,12 +9,24 @@
 # additionally, make sure this script is aligned with /buildscripts/compile.sh and /buildscripts/wheel-linux.sh,
 # in particular when it comes to install targets and package data, etc
 
+set -euo pipefail
+
+mkdir -p python_wrapper/src/copying
+
 bash ./tools/install-qhull.sh --prefix /tmp/qhull/target
 # copy the libs, instead of having auditwheel done it later. This is a bit risky because cmake will later write in this
 # very directory... but it works
-cp /tmp/qhull/target/lib/libqhull_r.* /tmp/atlas/target/atlas/lib64/
+if [ "$(uname)" != "Darwin" ] ; then
+    mkdir -p /tmp/atlas/target/atlas/lib64
+    cp /tmp/qhull/target/lib/libqhull_r.* /tmp/atlas/target/atlas/lib64/
+else
+    mkdir -p /tmp/atlas/target/atlas/lib
+    for e in $(brew list qhull | grep '/lib/' | grep dylib) ; do
+        cp $e /tmp/atlas/target/atlas/lib
+    done
+fi
 
-wget https://raw.githubusercontent.com/qhull/qhull/blob/master/COPYING.txt -O python_wrapper/src/copying/libqhull.txt
+wget https://raw.githubusercontent.com/qhull/qhull/master/COPYING.txt -O python_wrapper/src/copying/libqhull.txt
 echo '{"libqhull_r": {"path": "copying/libqhull.txt", "home": "https://github.com/qhull/qhull"}}' > python_wrapper/src/copying/list.json
 
 
