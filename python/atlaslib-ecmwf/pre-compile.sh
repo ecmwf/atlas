c4f919d1a7384f231d0d59ec8570dbef86e63734
tmi <vojta.tuma@gmail.com>
2025-03-12 14:35:27 +0100

    Python wheel: initial setup (#257)
    * Python wheel: initial setup
    
    * Python wheel: use reusable action right away
    
    * Python wheel: Add stripping
    
    * Python wheel: add qhull dep
    
    * Python wheel: fix macos build
    
    * Python wheel: update name to atlaslib-ecmwf
    
    * Python wheel: make post-build.sh executable
    
    * Python wheel: try hotfix linking prob
    
    * Python wheel: undo the hotfix
    
    * Python wheel: reorg
    
    ---------
    
    Co-authored-by: Iain Russell <Iain.Russell@ecmwf.int>

---
+++ python/atlaslib-ecmwf/pre-compile.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# the procedure for adding a new ext dependency to be bundled in here:
+# - add git checkout, compile, etc
+# - ensure the version ends up in python/atlaslib-ecmwf/src/versions.txt
+# - ensure the licence ends up in python/atlaslib-ecmwf/src/copying/, and fname is referenced in copying/list.json
+# - ensure the .so ends up in target/lib64/ with the expected libname
+# - validate that the resulting wheel contains all the above
+# additionally, make sure this script is aligned with /buildscripts/compile.sh and /buildscripts/wheel-linux.sh,
+# in particular when it comes to install targets and package data, etc
+
+set -euo pipefail
+
+mkdir -p python/atlaslib-ecmwf/src/copying
+
+bash ./tools/install-qhull.sh --prefix /tmp/qhull/target
+# copy the libs, instead of having auditwheel done it later. This is a bit risky because cmake will later write in this
+# very directory... but it works
+if [ "$(uname)" != "Darwin" ] ; then
+    mkdir -p /tmp/atlas/target/atlas/lib64
+    cp /tmp/qhull/target/lib/libqhull_r.* /tmp/atlas/target/atlas/lib64/
+else
+    mkdir -p /tmp/atlas/target/atlas/lib
+    for e in $(brew list qhull | grep '/lib/' | grep dylib) ; do
+        cp $e /tmp/atlas/target/atlas/lib
+    done
+fi
+
+wget https://raw.githubusercontent.com/qhull/qhull/master/COPYING.txt -O python/atlaslib-ecmwf/src/copying/libqhull.txt
+echo '{"libqhull_r": {"path": "copying/libqhull.txt", "home": "https://github.com/qhull/qhull"}}' > python/atlaslib-ecmwf/src/copying/list.json
+
+
+
