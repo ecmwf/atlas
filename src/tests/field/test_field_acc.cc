778db759f7e65ffec29dde8521f26a3ddd447b9a
Willem Deconinck <willem.deconinck@ecmwf.int>
2025-03-21 14:10:33 +0100

    Add device offload operators on FieldSet (#268)
    * Add device offload operators on FieldSet in the C++ backend
    
    ---------
    
    Co-authored-by: Slavko Brdar <slavko.brdar@ecmwf.int>

--- src/tests/field/test_field_acc.cc
+++ src/tests/field/test_field_acc.cc
@@ -91,38 +91,111 @@ CASE("test_field_acc") {
 
 
 CASE("test_fieldset_acc") {
-    const std::vector<int> vshape = std::vector<int>({4, -1, 3});
-    const std::vector<std::string> var_names = {"temperature", "pressure", "density"};
-    field::MultiField mfield(array::make_datatype<double>(), vshape, var_names);
-    FieldSet fieldset;
-    fieldset.add(mfield);
-    fieldset.add(Field("mask", make_datatype<int>(), array::make_shape(10)));
 
-    for (auto f : fieldset) {
-        f.allocateDevice();
-    }
+    auto init_field_on_host = [](Field& field, double seed) {
+        auto hview = array::make_host_view<double, 2>(field);
+        for (int i=0; i<field.shape(0); ++i) {
+            for (int j=0; j<field.shape(1); ++j) {
+                hview(i,j) = seed + 100 + 10*i + j;
+            }
+        }
+    };
 
-    auto field = fieldset.field(1);
-    auto view = array::make_view<double, 2>(field);
-    view(3,2) = 1.;
+    auto edit_field_on_device = [](Field& field) {
+        int Ni = field.shape(0);
+        int Nj = field.shape(1);
 
-    field.updateDevice();
-    auto dview = array::make_device_view<double, 2>(field);
-
-#if ! ATLAS_HAVE_GRIDTOOLS_STORAGE
-// TODO: gridtools storage does not implement view.index(...) at the moment
-
-    double* dptr = dview.data();
+#if ATLAS_HAVE_GRIDTOOLS_STORAGE
+        // TODO: gridtools storage does not implement view.index(...) at the moment
+        {
+            auto hview = array::make_host_view<double, 2>(field);
+            for (int i=0; i<field.shape(0); ++i) {
+                for (int j=0; j<field.shape(1); ++j) {
+                    hview(i,j) += 10 + 1;
+                }
+            }
+        }
+#else
+        auto dview = array::make_device_view<double, 2>(field);
+        double* dptr = dview.data();
 #pragma acc parallel deviceptr(dptr)
-    {
-        double t = dptr[dview.index(3,2)];
-        dptr[dview.index(3,2)] = 1. + t;
-    }
-    field.updateHost();
-
-    EXPECT_EQ( view(3,2), 2. );
-
+        {
+            for (int i=0; i<Ni; ++i) {
+                for (int j=0; j<Nj; ++j) {
+                    dptr[dview.index(i,j)] += 10 + 1;
+                }
+            }
+        }
 #endif
+    };
+    auto verify_field_on_host = [](const Field& field, double seed) {
+        auto hview = array::make_host_view<const double, 2>(field);
+        for (int i=0; i<field.shape(0); ++i) {
+            for (int j=0; j<field.shape(1); ++j) {
+                EXPECT_EQ(hview(i,j), seed + 100 + 10 * (i+1) + (j+1));
+            }
+        }
+    };
+
+    auto create_fieldset = [] {
+        const std::vector<int> vshape = std::vector<int>({4, -1, 3});
+        const std::vector<std::string> var_names = {"temperature", "pressure", "density"};
+        field::MultiField mfield(array::make_datatype<double>(), vshape, var_names);
+        FieldSet fieldset;
+        fieldset.add(mfield);
+        fieldset.add(Field("contiguous", make_datatype<double>(), array::make_shape(4,3)));
+        return fieldset;
+    };
+
+    SECTION("all fields in fieldset") {
+        auto fieldset = create_fieldset();
+
+        double seed = 0.;
+        for( auto field : fieldset ) {
+            init_field_on_host(field, seed);
+            seed += 100.;
+        }
+
+        fieldset.allocateDevice();
+        fieldset.updateDevice();
+        for( auto field : fieldset ) {
+            edit_field_on_device(field);
+        }
+        fieldset.updateHost();
+        fieldset.deallocateDevice();
+
+        seed = 0.;
+        for( auto field : fieldset ) {
+            verify_field_on_host(field, seed);
+            seed += 100.;
+        }
+    }
+
+    SECTION("field 'temperature' in fieldset") {
+        auto fieldset = create_fieldset();
+        auto field = fieldset.field("temperature");
+        double seed = 0.;
+        init_field_on_host(field, seed);
+        fieldset.allocateDevice({"temperature"});
+        fieldset.updateDevice({"temperature"});
+        edit_field_on_device(field);
+        fieldset.updateHost({"temperature"});
+        fieldset.deallocateDevice({"temperature"});
+        verify_field_on_host(field, seed);
+    }
+
+    SECTION("field [0] in fieldset") {
+        auto fieldset = create_fieldset();
+        auto field = fieldset.field(0);
+        double seed = 0.;
+        init_field_on_host(field, seed);
+        fieldset.allocateDevice({0});
+        fieldset.updateDevice({0});
+        edit_field_on_device(field);
+        fieldset.updateHost({0});
+        fieldset.deallocateDevice({0});
+        verify_field_on_host(field, seed);
+    }
 }
 //-----------------------------------------------------------------------------
 
