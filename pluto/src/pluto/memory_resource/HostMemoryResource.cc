e91a7803332f87b3f7d9a918e24d34ddec9639aa
Willem Deconinck <willem.deconinck@ecmwf.int>
2025-03-31 11:18:12 +0000

    pluto: Update memory_pool_resource lifetimes (no destruction), add memory_tracking, add tracing

--- pluto/src/pluto/memory_resource/HostMemoryResource.cc
+++ pluto/src/pluto/memory_resource/HostMemoryResource.cc
@@ -13,15 +13,33 @@
 #include <iostream>
 #include <memory>
 
+#include "pluto/alignment.h"
 #include "pluto/pluto_config.h"
 #include "pluto/stream.h"
 #include "pluto/trace.h"
+#include "pluto/memory.h"
 
 #include "MemoryPoolResource.h"
 
 #define LOG PLUTO_DEBUGGING
 
 namespace pluto {
+
+class HostMemoryResource : public async_memory_resource {
+public:
+    HostMemoryResource() = default;
+
+    ~HostMemoryResource() = default;
+
+private:
+    void* do_allocate(std::size_t bytes, std::size_t alignment) override;
+    void do_deallocate(void* ptr, std::size_t bytes, std::size_t alignment) override;
+
+    bool do_is_equal(const memory_resource& other) const noexcept override;
+
+    void* do_allocate_async(std::size_t bytes, std::size_t alignment, stream_view) override;
+    void do_deallocate_async(void* ptr, std::size_t bytes, std::size_t alignment, stream_view) override;
+};
 
 namespace {
 template<typename T>
@@ -31,8 +49,8 @@ struct constant_init {
     };
     constexpr constant_init() : obj() { }
 
-    template<typename U>
-    explicit constexpr constant_init(U arg) : obj(arg) { }
+    template<typename ...Args>
+    explicit constexpr constant_init(Args... args) : obj(args...) { }
 
     ~constant_init() { /* do nothing, union member is not destroyed */ }
 };
@@ -41,12 +59,14 @@ struct constant_init {
 constant_init<HostMemoryResource>  host_res{};
 
 async_memory_resource* host_resource() {
+    // Never destroyed due to constant_init!
     return &host_res.obj;
 }
 
 memory_pool_resource* host_pool_resource() {
-    static MemoryPoolResource resource(host_resource());
-    return &resource;
+    // Never destroyed due to constant_init!
+    static constant_init<MemoryPoolResource> host_pool_res{host_resource(), "pluto::host_pool_resource", &pluto::memory::host_pool};
+    return &host_pool_res.obj;
 }
 
 
@@ -54,14 +74,9 @@ void* HostMemoryResource::do_allocate(std::size_t bytes, std::size_t alignment) 
     alignment = std::max(alignment, default_alignment());
     auto* ptr = new_delete_resource()->allocate(bytes, alignment);
 
+    memory::host.allocate(bytes);
     if (trace::enabled()) {
-        std::string_view label = pluto::get_label();
-        if (label.empty()) {
-            trace::out << "PLUTO_TRACE " << "pluto::host_resource::allocate(bytes="<<trace::format_bytes(bytes)<<", alignment="<<alignment<<") -> ptr=" << ptr << '\n';
-        }
-        else {
-            trace::out << "PLUTO_TRACE " << "pluto::host_resource::allocate(label="<<label<<", bytes="<<trace::format_bytes(bytes)<<", alignment="<<alignment<<") -> ptr=" << ptr << '\n';
-        }
+        trace::log::allocate(get_label(), ptr, bytes, alignment, "pluto::host_resource", &memory::host);
     }
 
     return ptr;
@@ -70,14 +85,9 @@ void* HostMemoryResource::do_allocate(std::size_t bytes, std::size_t alignment) 
 void HostMemoryResource::do_deallocate(void* ptr, std::size_t bytes, std::size_t alignment) {
     alignment = std::max(alignment, default_alignment());
 
+    memory::host.deallocate(bytes);
     if (trace::enabled()) {
-        std::string_view label = pluto::get_label();
-        if (label.empty()) {
-            trace::out << "PLUTO_TRACE " << "pluto::host_resource::deallocate(ptr="<<ptr<<", bytes="<<trace::format_bytes(bytes)<<", alignment="<<alignment<<")" << std::endl;
-        }
-        else {
-            trace::out << "PLUTO_TRACE " << "pluto::host_resource::deallocate(label="<<label<<", ptr="<<ptr<<", bytes="<<trace::format_bytes(bytes)<<", alignment="<<alignment<<")" << std::endl;
-        }
+        trace::log::deallocate(get_label(), ptr, bytes, alignment, "pluto::host_resource", &memory::host);
     }
 
     new_delete_resource()->deallocate(ptr, bytes, alignment);
@@ -85,11 +95,11 @@ void HostMemoryResource::do_deallocate(void* ptr, std::size_t bytes, std::size_t
 
 
 void* HostMemoryResource::do_allocate_async(std::size_t bytes, std::size_t alignment, stream_view) {
-    return new_delete_resource()->allocate(bytes, std::max(alignment, default_alignment()));
+    return do_allocate(bytes, alignment);
 }
 
 void HostMemoryResource::do_deallocate_async(void* ptr, std::size_t bytes, std::size_t alignment, stream_view) {
-    new_delete_resource()->deallocate(ptr, bytes, std::max(alignment, default_alignment()));
+    do_deallocate(ptr, bytes, alignment);
 }
 
 bool HostMemoryResource::do_is_equal(const memory_resource& other) const noexcept {
--- pluto/src/pluto/memory_resource/ManagedMemoryResource.cc
+++ pluto/src/pluto/memory_resource/ManagedMemoryResource.cc
@@ -13,17 +13,32 @@
 #include <iostream>
 
 #include "hic/hic.h"
+
+#include "pluto/alignment.h"
 #include "pluto/pluto_config.h"
 #include "pluto/runtime.h"
 #include "pluto/stream.h"
+#include "pluto/memory.h"
 #include "pluto/trace.h"
 
 #include "MemoryPoolResource.h"
-#include "TraceMemoryResource.h"
 
 #define LOG PLUTO_DEBUGGING
 
 namespace pluto {
+
+class ManagedMemoryResource : public memory_resource {
+public:
+    using alignment_t                      = std::size_t;
+    static constexpr alignment_t alignment = default_alignment();
+
+    ManagedMemoryResource() = default;
+
+protected:
+    void* do_allocate(std::size_t bytes, alignment_t) override;
+    void do_deallocate(void* ptr, std::size_t bytes, alignment_t) override;
+    bool do_is_equal(const memory_resource& other) const noexcept override;
+};
 
 namespace {
 template<typename T>
@@ -33,8 +48,8 @@ struct constant_init {
     };
     constexpr constant_init() : obj() { }
 
-    template<typename U>
-    explicit constexpr constant_init(U arg) : obj(arg) { }
+    template<typename ...Args>
+    explicit constexpr constant_init(Args... args) : obj(args...) { }
 
     ~constant_init() { /* do nothing, union member is not destroyed */ }
 };
@@ -43,12 +58,14 @@ struct constant_init {
 constant_init<ManagedMemoryResource>  managed_res{};
 
 memory_resource* managed_resource() {
+    // Never destroyed due to constant_init!
     return &managed_res.obj;
 }
 
 memory_pool_resource* managed_pool_resource() {
-    static MemoryPoolResource resource(managed_resource());
-    return &resource;
+    // Never destroyed due to constant_init!
+    static constant_init<MemoryPoolResource> managed_pool_res{managed_resource(), "pluto::managed_pool_resource", &pluto::memory::managed_pool};
+    return &managed_pool_res.obj;
 }
 
 // ---------------------------------------------------------------------------------------------------------
@@ -70,28 +87,19 @@ void* ManagedMemoryResource::do_allocate(std::size_t bytes, alignment_t) {
         std::cout << "               + hicMallocManaged(ptr:" << ptr << ", bytes:" << bytes << ")" << std::endl;
     }
 
+    memory::managed.allocate(bytes);
     if (trace::enabled()) {
-        std::string_view label = pluto::get_label();
-        if (label.empty()) {
-            trace::out << "PLUTO_TRACE " << "pluto::managed_resource::allocate(ptr="<<ptr<<", bytes="<<trace::format_bytes(bytes)<<", alignment="<<alignment<<")" << std::endl;
-        }
-        else {
-            trace::out << "PLUTO_TRACE " << "pluto::managed_resource::allocate(label="<<label<<", ptr="<<ptr<<", bytes="<<trace::format_bytes(bytes)<<", alignment="<<alignment<<")" << std::endl;
-        }
+        trace::log::allocate(get_label(), ptr, bytes, alignment, "pluto::managed_resource", &memory::managed);
     }
 
     return ptr;
 }
 
 void ManagedMemoryResource::do_deallocate(void* ptr, std::size_t bytes, alignment_t) {
+
+    memory::managed.deallocate(bytes);
     if (trace::enabled()) {
-        std::string_view label = pluto::get_label();
-        if (label.empty()) {
-            trace::out << "PLUTO_TRACE " << "pluto::managed_resource::deallocate(ptr="<<ptr<<", bytes="<<trace::format_bytes(bytes)<<", alignment="<<alignment<<")" << std::endl;
-        }
-        else {
-            trace::out << "PLUTO_TRACE " << "pluto::managed_resource::deallocate(label="<<label<<", ptr="<<ptr<<", bytes="<<trace::format_bytes(bytes)<<", alignment="<<alignment<<")" << std::endl;
-        }
+        trace::log::deallocate(get_label(), ptr, bytes, alignment, "pluto::managed_resource", &memory::managed);
     }
 
     if constexpr (LOG) {
