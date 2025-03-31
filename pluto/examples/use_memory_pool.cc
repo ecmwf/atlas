63d4bf94b47354da2993343b571fc69b38bfd545
Willem Deconinck <willem.deconinck@ecmwf.int>
2025-03-31 11:09:10 +0000

    pluto: Update examples

--- pluto/examples/use_memory_pool.cc
+++ pluto/examples/use_memory_pool.cc
@@ -22,28 +22,37 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
     std::cout << "BEGIN" << std::endl;
 
     pluto::trace::enable(true);
-    bool pinning = true;
+    // This could also have been done with environment variable:
+    //     export PLUTO_TRACE=1
 
-    std::unique_ptr<pluto::MemoryPoolResource> memory_pool;
-    if (pinning) {
-        memory_pool = std::make_unique<pluto::MemoryPoolResource>(std::make_unique<pluto::TraceMemoryResource>(
-            "pinned_upstream", std::make_unique<pluto::PinnedMemoryResource>()));
-    }
-    else {
-        memory_pool =
-            std::make_unique<pluto::MemoryPoolResource>(std::make_unique<pluto::TraceMemoryResource>("upstream"));
-    }
+    // The default pool options can be changed before using the memory pool for the first time.
+    // If not changed, the default configuration allows for *all* allocations to be handled by a memory pool,
+    // no matter the size.
+    pluto::set_default_pool_options({
+        /* .max_blocks_per_chunk = */ 4,
+        /* .largest_required_pool_block = */ 64*mb
+    });
+    // This modification lets only allocations <= 64M be handled by the memory pool.
+    // When the pool needs to grow, 4 chunks of pool memory are allocated at a time.
+    // This could also have been achieved with environment variables:
+    //     export PLUTO_MAX_BLOCKS_PER_CHUNK=4
+    //     export PLUTO_LARGEST_REQUIRED_POOL_BLOCK=64M
+
+    pluto::host::set_default_resource( pluto::pinned_pool_resource() );
+    // This can also be set with environment variable:
+    //     export PLUTO_HOST_DEFAULT_MEMORY_RESOURCE=pluto::host_pool_resource
 
     auto test = [&](std::size_t size, std::size_t n = 1) {
-        pluto::allocator<std::byte> allocator{memory_pool.get()};
-        std::vector<std::byte*> arrays(n);
+        auto var_name = [](int k) {
+            return std::string("var_") + std::to_string(k);
+        };
+        pluto::host::allocator<std::byte> allocator;
+        std::vector<std::byte*> var_data(n);
         for (std::size_t k = 0; k < n; ++k) {
-            std::cout << "+ allocate array[" << k << "], size = " << size / mb << " mb" << std::endl;
-            arrays[k] = allocator.allocate(size);
+            var_data[k] = allocator.allocate(var_name(k),size);
         }
-        for (std::size_t k = 0; k < n; ++k) {
-            std::cout << "- deallocate array[" << n - 1 - k << "], size = " << size / mb << " mb" << std::endl;
-            allocator.deallocate(arrays[n - 1 - k], size);
+        for (std::size_t k = n; k-- > 0; ) {
+            allocator.deallocate(var_name(k), var_data[k], size);
         }
     };
 
@@ -55,5 +64,11 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
         test(128 * mb);
     }
 
-    std::cout << "END" << std::endl;
+    std::cout << "\nEND\n" << std::endl;
+
+    // Release all memory pools
+    pluto::release();
+
+    std::cout << '\n' << pluto::memory::report() << std::endl;
+
 }
