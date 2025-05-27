bb5af483c0b11810508c75a4e4da251cfe9351ba
Slavko Brdar <slavko.brdar@ecmwf.int>
2025-05-27 16:05:52 +0200

    make coupling exchange a field one way when more than two models share the world communicator

--- src/atlas/coupler/coupler.cc
+++ src/atlas/coupler/coupler.cc
@@ -21,24 +21,67 @@
 #include "atlas/parallel/mpi/mpi.h"
 
 
-
 void setup_oneway_remap(int model_1, int model_2) {
     using namespace atlas;
     using namespace atlas::coupler;
     using Matrix      = atlas::linalg::SparseMatrixStorage;
 
-    bool is_model_1 = (atlas::mpi::comm().name() == coupler_.comm_name(model_1));
-    bool is_model_2 = (atlas::mpi::comm().name() == coupler_.comm_name(model_2));
+    // create the model1 & model2 joint communicator
+    mpi::comm("world").barrier();
+    auto m1_ranks = coupler_.global_ranks(10);
+    auto m2_ranks = coupler_.global_ranks(21);
+    auto in_m1_m2_ranks = [&m1_ranks, &m2_ranks](int rank) {
+        bool in_m1 = (std::find(m1_ranks.begin(), m1_ranks.end(), rank) != m1_ranks.end());
+        if (in_m1) {
+            return true;
+        }
+        bool in_m2 = (std::find(m2_ranks.begin(), m2_ranks.end(), rank) != m2_ranks.end());
+        return in_m2;
+    };
+    auto& comm12 = eckit::mpi::comm("world").split(in_m1_m2_ranks(mpi::comm("world").rank()), "comm12");
+    mpi::comm("world").barrier();
+
+    bool is_model_1 = (mpi::comm().name() == coupler_.comm_name(model_1));
+    bool is_model_2 = (mpi::comm().name() == coupler_.comm_name(model_2));
     if (! is_model_1 && ! is_model_2) {
         return;
     }
 
+    // exchange models
+    std::vector<int> models;
+    models.resize(comm12.size());
+    models[comm12.rank()] = coupler_.this_model();
+    comm12.allGather(coupler_.this_model(), models.begin(), models.end());
+
+    // exchange other procs' comm12-ranks
+    std::unordered_map<int, std::vector<int>> model_comm12_ranks;
+    {
+        std::vector<int> comm12_sizes(comm12.size());
+        int my_size = mpi::comm().size();
+        comm12.allGather(my_size, comm12_sizes.begin(), comm12_sizes.end());
+
+        std::vector<int> my_comm12_ranks(my_size);
+        int my_rank = comm12.rank();
+        mpi::comm().allGather(my_rank, my_comm12_ranks.begin(), my_comm12_ranks.end());
+
+        eckit::mpi::Buffer<int> buf_ranks(comm12.size());
+        comm12.allGatherv(my_comm12_ranks.begin(), my_comm12_ranks.end(), buf_ranks);
+
+        std::vector<int> comm12_models;
+        for (int i = 0; i < comm12.size(); i++) {
+            if (std::find(comm12_models.begin(), comm12_models.end(), models[i]) == comm12_models.end()) {
+                comm12_models.emplace_back(models[i]);
+                model_comm12_ranks[models[i]].reserve(buf_ranks.counts[i]);
+                std::copy_n(&(buf_ranks.buffer[buf_ranks.displs[i]]), buf_ranks.counts[i],
+                        std::back_inserter(model_comm12_ranks[models[i]]));
+            }
+        }
+    }
+
     // everything, except the remote index, is set up on model_2 for remapping
     // global matrix is read in on model_2
-
     atlas::Grid grid1;
     atlas::Grid grid2; // later, no need to recreate grid2, as we are on the tasks which own grid2
-
     Matrix gmatrix;
     Matrix lmatrix;
     auto collect = coupler_.collect_map(model_1, model_2);
@@ -64,33 +107,36 @@ void setup_oneway_remap(int model_1, int model_2) {
             std::size_t nr = fspace_m2.size();
             std::size_t nc = l_gcols.size();
             eckit::linalg::Index index_base = 0;
-            bool is_sorted = true; // !!! check if tripplets are sorted here
+            bool is_sorted = true; // !!! comming from Atlas itself, the tripplets are sorted
             lmatrix = linalg::make_sparse_matrix_storage_from_rows_columns_values(nr, nc, l_rows, l_cols, l_vals, index_base, is_sorted);
         }
 
         grid1 = atlas::Grid(coupler_.grid_name(model_1));
-        grid::Distribution dist_m1(grid1, grid1.partitioner() | util::Config("nb_partitions", coupler_.global_ranks(model_1).size()));
+        grid::Distribution dist_m1(grid1, grid1.partitioner()
+            | util::Config("nb_partitions", coupler_.global_ranks(model_1).size())
+            | util::Config("mpi_comm", "21"));
 
         std::size_t collect_size = l_gcols.size();
         std::vector<gidx_t> collect_gidx(collect_size);
         std::vector<idx_t>  collect_ridx(collect_size);
-        std::vector<int> collect_partition;
+        std::vector<int> collect_partition(collect_size);
         idx_t ridx_base = 0;
 
-        for (size_t i=0; i < collect_gidx.size(); ++i) {
+        for (size_t i = 0; i < collect_gidx.size(); ++i) {
             collect_gidx[i] = l_gcols[i] + 1;
         }
 
-        collect_partition.resize(collect_size);
         util::locate_partition(dist_m1, collect_gidx.size(), collect_gidx.data(), collect_partition.data());
+
         for (int i = 0; i < collect_partition.size(); ++i) {
-            auto part = collect_partition[i]; // local rank
-            collect_partition[i] = coupler_.global_ranks(model_1)[part]; // "world"-rank
+            auto p = collect_partition[i]; // comm1-rank
+            collect_partition[i] = model_comm12_ranks.at(model_1)[p]; // comm12-rank
         }
-        util::locate_remote_index(mpi::comm("world"), 0, nullptr, nullptr,
+
+        util::locate_remote_index(comm12, 0, nullptr, nullptr,
                                     collect_size, collect_gidx.data(), collect_partition.data(), collect_ridx.data(), ridx_base);
-        collect->setup("world", collect_size, collect_partition.data(), collect_ridx.data(), ridx_base);
-        mpi::comm().barrier();
+
+        collect->setup(comm12.name(), collect_size, collect_partition.data(), collect_ridx.data(), ridx_base);
     }
 
     if (is_model_1) {
@@ -98,10 +144,11 @@ void setup_oneway_remap(int model_1, int model_2) {
         functionspace::StructuredColumns fspace_m1(grid1);
         auto glb_idx_v = array::make_view<gidx_t, 1>(fspace_m1.global_index());
         auto ghost_v   = array::make_view<int, 1>(fspace_m1.ghost());
-        util::locate_remote_index(mpi::comm("world"), glb_idx_v.size(), glb_idx_v.data(), ghost_v.data(),
+        util::locate_remote_index(comm12, glb_idx_v.size(), glb_idx_v.data(), ghost_v.data(),
                                     0, nullptr, nullptr, nullptr, 0);
-        collect->setup("world", 0, nullptr, nullptr, 0);
+        collect->setup(comm12.name(), 0, nullptr, nullptr, 0);
     }
+    comm12.barrier();
 }
 
 
