e730fd38813c3bec035c47b5f1e988d79412f711
lewisn-met <lewis.napper@metoffice.gov.uk>
2026-03-04 12:40:03 +0000

    Add config check to redistribution

--- src/atlas/redistribution/Redistribution.cc
+++ src/atlas/redistribution/Redistribution.cc
@@ -20,7 +20,15 @@ Redistribution::Redistribution(): Handle(){};
 Redistribution::Redistribution(const FunctionSpace& sourceFunctionSpace, const FunctionSpace& targetFunctionSpace,
                                const util::Config& config):
     Handle([&]() -> redistribution::detail::RedistributionImpl* {
-        ATLAS_ASSERT(sourceFunctionSpace.type() == targetFunctionSpace.type());
+        // If no heterogeneous_redistribution config is set, or if it is not set to "true", assert that
+        // the source and target function spaces are the same type (default)
+        std::string isHeteroRedistString = "false";
+        config.get("heterogeneous_redistribution", isHeteroRedistString);
+        bool isHeteroRedist = (isHeteroRedistString == "true");
+
+        if (!isHeteroRedist) {
+            ATLAS_ASSERT(sourceFunctionSpace.type() == targetFunctionSpace.type());
+        };
 
         std::string type = redistribution::detail::RedistributeGeneric::static_type();
         config.get("type", type);
