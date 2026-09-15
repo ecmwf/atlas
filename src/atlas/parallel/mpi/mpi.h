5e9f00e55735787f69f5fa00c42ede5ecccf9ee1
Willem Deconinck <willem.deconinck@ecmwf.int>
2026-09-15 13:50:53 +0200

    Fix ambiguity for mpi::Scope (fixes #402) (#405)
    * Fix ambiguity for mpi::Scope
    * Fix ambiguity for mpi::scope::push

--- src/atlas/parallel/mpi/mpi.h
+++ src/atlas/parallel/mpi/mpi.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #include <string_view>
+#include <type_traits>
 
 #include "eckit/mpi/Comm.h"
 #include "atlas/parallel/mpi/Statistics.h"
@@ -46,12 +47,16 @@ void finalize();
 void finalise();
 
 namespace scope {
+    namespace detail {
+        void push(const Comm& comm);
+    }
     /// @brief Push a new scope for MPI communicators. The current default communicator is restored when the scope is destructed using pop()
     void push();
     /// @brief Push a new scope for MPI communicators. The given communicator is set as the default communicator for the duration of the scope. The previous default communicator is restored when the scope is destructed using pop()
     void push(std::string_view name);
     /// @brief Push a new scope for MPI communicators. The given communicator is set as the default communicator for the duration of the scope. The previous default communicator is restored when the scope is destructed using pop()
-    void push(const Comm& comm);
+    template <typename CommT, std::enable_if_t<std::is_same_v<std::decay_t<CommT>, Comm>, int> = 0>
+    void push(CommT&& comm) { detail::push(comm); }
     /// @brief Push a new scope for MPI communicators. The given communicator is set as the default communicator for the duration of the scope. The previous default communicator is restored when the scope is destructed using pop()
     /// If the given communicator is not already registered, it will be registered with a generated name "int.<communicator>" for the duration of the scope, and unregistered when the scope is destructed.
     void push(int communicator);
@@ -63,7 +68,8 @@ namespace scope {
 struct Scope {
     Scope() { scope::push(); }
     Scope(std::string_view name) { scope::push(name); }
-    Scope(const Comm& comm) { scope::push(comm); };
+    template <typename CommT, std::enable_if_t<std::is_same_v<std::decay_t<CommT>, Comm>, int> = 0>
+    Scope(CommT&& comm) { scope::push(comm); };
     /// @brief Constructor using integer value. If the given communicator is not already registered, it will be registered with a generated name "int.<communicator>" for the duration of the scope, and unregistered when the scope is destructed.
     /// @param communicator The integer value of the MPI communicator to use for the scope
     Scope(int communicator) { scope::push(communicator); }
